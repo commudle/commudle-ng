ba5775d68042c2908d6d6c3f0f6942678cc857c0
Ashwin <bsashwin9@gmail.com>
2020-07-09 15:34:56 +0530

    Update the layout of events module

--- projects/commudle-admin/src/app/feature-modules/events/events.module.ts
+++ projects/commudle-admin/src/app/feature-modules/events/events.module.ts
@@ -1,49 +1,51 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EventsRoutingModule } from './events-routing.module';
-import { CreateEventComponent } from './components/create-event/create-event.component';
-import { EditEventComponent } from './components/edit-event/edit-event.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EventsRoutingModule} from './events-routing.module';
+import {CreateEventComponent} from './components/create-event/create-event.component';
+import {EditEventComponent} from './components/edit-event/edit-event.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
-  NbCheckboxModule,
-  NbSelectModule,
-  NbInputModule,
-  NbCardModule,
-  NbButtonModule,
-  NbDatepickerModule,
+  NbAccordionModule,
+  NbActionsModule,
   NbAlertModule,
-  NbListModule,
-  NbTooltipModule,
-  NbWindowModule,
-  NbIconModule,
   NbAutocompleteModule,
-  NbToggleModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule,
+  NbIconModule,
+  NbInputModule,
+  NbListModule,
   NbPopoverModule,
   NbRadioModule,
-  NbAccordionModule
- } from '@nebular/theme';
-import { CollaboratingCommunitiesComponent } from './components/collaborating-communities/collaborating-communities.component';
-import { VolunteersComponent } from './components/volunteers/volunteers.component';
-import { EventFormResponsesComponent } from './components/event-form-responses/event-form-responses.component';
-import { EventCommentsComponent } from './components/event-comments/event-comments.component';
-import { EventStatsComponent } from './components/event-stats/event-stats.component';
-import { EditorModule } from '@tinymce/tinymce-angular';
-import { EventDashboardComponent } from './components/event-dashboard/event-dashboard.component';
-import { FormGroupsComponent } from './components/event-dashboard/form-groups/form-groups.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { UserDetailsCellComponent } from './components/event-form-responses/user-details-cell/user-details-cell.component';
-import { EventLocationsComponent } from './components/event-locations/event-locations.component';
-import { EventLocationTracksComponent } from './components/event-locations/event-location-tracks/event-location-tracks.component';
-import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
-import { EventUpdatesComponent } from './components/event-updates/event-updates.component';
-import { EventStatusComponent } from './components/event-status/event-status.component';
-import { SharedComponentsModule } from 'projects/shared-components/shared-components.module';
-import { EventSimpleRegistrationComponent } from './components/event-simple-registration/event-simple-registration.component';
-import { UserEventRegistrationsComponent } from './components/user-event-registrations/user-event-registrations.component';
-import { UserDetailsComponent } from './components/user-event-registrations/user-details/user-details.component';
-import { EventEmbeddedVideoStreamComponent } from './components/event-embedded-video-stream/event-embedded-video-stream.component';
-import { YouTubePlayerModule } from '@angular/youtube-player';
+  NbSelectModule,
+  NbTabsetModule,
+  NbToggleModule,
+  NbTooltipModule,
+  NbWindowModule
+} from '@nebular/theme';
+import {CollaboratingCommunitiesComponent} from './components/collaborating-communities/collaborating-communities.component';
+import {VolunteersComponent} from './components/volunteers/volunteers.component';
+import {EventFormResponsesComponent} from './components/event-form-responses/event-form-responses.component';
+import {EventCommentsComponent} from './components/event-comments/event-comments.component';
+import {EventStatsComponent} from './components/event-stats/event-stats.component';
+import {EditorModule} from '@tinymce/tinymce-angular';
+import {EventDashboardComponent} from './components/event-dashboard/event-dashboard.component';
+import {FormGroupsComponent} from './components/event-dashboard/form-groups/form-groups.component';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {UserDetailsCellComponent} from './components/event-form-responses/user-details-cell/user-details-cell.component';
+import {EventLocationsComponent} from './components/event-locations/event-locations.component';
+import {EventLocationTracksComponent} from './components/event-locations/event-location-tracks/event-location-tracks.component';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {EventUpdatesComponent} from './components/event-updates/event-updates.component';
+import {EventStatusComponent} from './components/event-status/event-status.component';
+import {SharedComponentsModule} from 'projects/shared-components/shared-components.module';
+import {EventSimpleRegistrationComponent} from './components/event-simple-registration/event-simple-registration.component';
+import {UserEventRegistrationsComponent} from './components/user-event-registrations/user-event-registrations.component';
+import {UserDetailsComponent} from './components/user-event-registrations/user-details/user-details.component';
+import {EventEmbeddedVideoStreamComponent} from './components/event-embedded-video-stream/event-embedded-video-stream.component';
+import {YouTubePlayerModule} from '@angular/youtube-player';
 
 
 @NgModule({
@@ -83,9 +85,7 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
     NgxMaterialTimepickerModule,
 
 
-
-
-    //Nebular
+    // Nebular
     NbButtonModule,
     NbCardModule,
     NbInputModule,
@@ -103,7 +103,10 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
     NbRadioModule,
     NbToggleModule,
     NbAccordionModule,
-    NbTooltipModule
+    NbTooltipModule,
+    NbTabsetModule,
+    NbActionsModule
   ]
 })
-export class EventsModule { }
+export class EventsModule {
+}
