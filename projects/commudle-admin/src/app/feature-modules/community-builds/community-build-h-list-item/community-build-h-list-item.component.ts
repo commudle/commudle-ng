7b3e5307be0f6d2ee337de43a5941884884e6929
arpansac <arpansac@gmail.com>
2020-07-13 10:43:16 +0530

    change community-builds url to builds

---
+++ projects/commudle-admin/src/app/feature-modules/community-builds/community-build-h-list-item/community-build-h-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-build-h-list-item',
+  templateUrl: './community-build-h-list-item.component.html',
+  styleUrls: ['./community-build-h-list-item.component.scss']
+})
+export class CommunityBuildHListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/commudle-admin/src/app/feature-modules/community-builds/community-builds-routing.module.ts
+++ projects/commudle-admin/src/app/feature-modules/community-builds/community-builds-routing.module.ts
@@ -3,23 +3,33 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateCommunityBuildComponent } from './components/create-community-build/create-community-build.component';
 import { MyCommunityBuildsComponent } from './components/my-community-builds/my-community-builds.component';
 import { CommunityBuildDetailsComponent } from './components/community-build-details/community-build-details.component';
+import { CommunityBuildsComponent } from './components/community-builds/community-builds.component';
+import { AuthGuard } from 'projects/shared-services/lib-authwatch.guard';
 
 
 const routes: Routes = [
   {
+    path: '',
+    component: CommunityBuildsComponent,
+  },
+  {
     path: 'create',
-    component: CreateCommunityBuildComponent
+    canActivate: [AuthGuard],
+    component: CreateCommunityBuildComponent,
   },
   {
     path: 'my-builds',
+    canActivate: [AuthGuard],
     component: MyCommunityBuildsComponent
   },
   {
     path: ':community_build_id/edit',
+    canActivate: [AuthGuard],
     component: CreateCommunityBuildComponent
   },
   {
     path: ':community_build_id',
+    canActivate: [AuthGuard],
     component: CommunityBuildDetailsComponent
   },
 ];
