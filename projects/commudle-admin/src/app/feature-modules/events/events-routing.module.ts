62b91338f25b8a82c2645a59c1ab8c2a9c48c932
arpansac <arpansac@gmail.com>
2020-07-16 13:42:02 +0530

    fix event update on editing in the event dashboard

--- projects/commudle-admin/src/app/feature-modules/events/events-routing.module.ts
+++ projects/commudle-admin/src/app/feature-modules/events/events-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
       },
       {
         path: ':event_id',
+        runGuardsAndResolvers: 'always',
         resolve: {
           event: EventDetailsResolver
         },
