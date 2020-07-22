e59f4cb35896ab2e1913f9b6b16981668eccd291
arpansac <arpansac@gmail.com>
2020-07-22 18:25:08 +0530

    add my-labs page and the api's required for it

--- projects/commudle-admin/src/app/feature-modules/labs/components/labs/labs.component.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/components/labs/labs.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, OnDestroy } from '@angular/core';
+import * as moment from 'moment';
+import { ILab, EPublishStatus } from 'projects/shared-models/lab.model';
+import { LabsService } from '../../services/labs.service';
+import { Title } from '@angular/platform-browser';
+import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
+import { LibAuthwatchService } from 'projects/shared-services/lib-authwatch.service';
+import { NbWindowService } from '@nebular/theme';
 
 @Component({
   selector: 'app-labs',
@@ -7,9 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
 
-  ngOnInit() {
+  ) {
   }
 
+  ngOnInit() {
+
+  }
 }
+
+
