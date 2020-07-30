39fd8db4664ba06fee35f465c03581e4f6c70cbb
arpansac <arpansac@gmail.com>
2020-07-30 15:56:33 +0530

    create separate component and channel for user-chat, reason, it contains some edge cases which will mix up the code of discussion-chat

--- projects/shared-services/application-cable-channels.constants.ts
+++ projects/shared-services/application-cable-channels.constants.ts
@@ -4,6 +4,7 @@ export const APPLICATION_CABLE_CHANNELS = {
   USER: 'ApplicationCable::V2::Channels::UserChannel',
   DISCUSSION_QNA: 'ApplicationCable::V2::Channels::DiscussionQnaChannel',
   DISCUSSION_CHAT_CHANNEL: 'ApplicationCable::V2::Channels::DiscussionChatChannel',
+  DISCUSSION_PERSONAL_CHAT_CHANNEL: 'ApplicationCable::V2::Channels::DiscussionPersonalChatChannel',
   POLL_CHANNEL: 'ApplicationCable::V2::Channels::PollChannel',
   VOTE_CHANNEL: 'ApplicationCable::V2::Channels::VoteChannel'
 };
