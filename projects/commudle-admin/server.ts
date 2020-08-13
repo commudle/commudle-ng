de347a2095c090b3e0012c17175534a03c6e8d7c
arpansac <arpansac@gmail.com>
2020-08-13 10:48:30 +0530

    ssr with cloudfunctions

--- projects/commudle-admin/server.ts
+++ projects/commudle-admin/server.ts
@@ -6,6 +6,7 @@ const path = require('path');
 const template = fs.readFileSync(path.join(__dirname, '../../dist', 'commudle-admin', 'index.html')).toString();
 const win = domino.createWindow(template);
 (global as any).WebSocket = require('ws');
+(global as any).XMLHttpRequest = require('xhr2');
 global['window'] = win;
 global['document'] = win.document;
 global['DOMTokenList'] = win.DOMTokenList;
@@ -76,9 +77,9 @@ function run() {
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+  // server.listen(port, () => {
+  //   console.log(`Node Express server listening on http://localhost:${port}`);
+  // });
 }
 
 // Webpack will replace 'require' with '__webpack_require__'
