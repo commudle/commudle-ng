3ee58ab5480f6d8fbaf0b5dabdf7222d53b4985c
arpansac <arpansac@gmail.com>
2020-08-20 17:13:54 +0530

    add event stats to community stats create reusable-components module

---
+++ projects/commudle-admin/src/app/feature-modules/reusable-components/components/community-emails-list/community-emails-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-emails-list',
+  templateUrl: './community-emails-list.component.html',
+  styleUrls: ['./community-emails-list.component.scss']
+})
+export class CommunityEmailsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
