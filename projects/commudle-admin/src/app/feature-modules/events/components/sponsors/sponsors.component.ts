d3b00992cb37cee29d043560210fe709e9759c2f
arpansac <arpansac@gmail.com>
2020-08-31 11:11:17 +0530

    create component for sponsors

---
+++ projects/commudle-admin/src/app/feature-modules/events/components/sponsors/sponsors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsors',
+  templateUrl: './sponsors.component.html',
+  styleUrls: ['./sponsors.component.scss']
+})
+export class SponsorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
