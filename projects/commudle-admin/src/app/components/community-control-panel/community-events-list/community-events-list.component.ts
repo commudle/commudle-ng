83cab888a1bc0e2bb434efd771c8eab87888758a
arpansac <arpansac@gmail.com>
2020-09-03 19:52:28 +0530

    improve the community page

--- projects/commudle-admin/src/app/components/community-control-panel/community-events-list/community-events-list.component.ts
+++ projects/commudle-admin/src/app/components/community-control-panel/community-events-list/community-events-list.component.ts
@@ -1,8 +1,10 @@
+import { CommunityEventsListActionsComponent } from './community-events-list-actions/community-events-list-actions.component';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { EventsService } from '../../../services/events.service';
 import { IEvent } from 'projects/shared-models/event.model';
+import { CommunityEventsListDateComponent } from './community-events-list-date/community-events-list-date.component';
 
 @Component({
   selector: 'app-community-events-list',
@@ -17,15 +19,29 @@ export class CommunityEventsListComponent implements OnInit {
   tableSettings = {
     actions: false,
     pager: {
-      perPage: 5
+      perPage: 10
     },
     columns: {
       name: {
         title: 'Name'
       },
+      date: {
+        title: 'Date',
+        filter: false,
+        sort: false,
+        type: 'custom',
+        renderComponent: CommunityEventsListDateComponent,
+      },
       status: {
         title: 'Status',
         filter: false
+      },
+      actions: {
+        title: 'Actions',
+        filter: false,
+        sort: false,
+        type: 'custom',
+        renderComponent: CommunityEventsListActionsComponent,
       }
     },
 
