6ce74485e4f4c09a8921783d679b93b581403320
arpansac <arpansac@gmail.com>
2020-09-07 01:00:33 +0530

    complete the initial home page

--- projects/commudle-admin/src/app/components/home/communities/communities.component.ts
+++ projects/commudle-admin/src/app/components/home/communities/communities.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommunitiesService } from '../../../services/communities.service';
+import { ICommunity } from 'projects/shared-models/community.model';
 
 @Component({
   selector: 'app-communities',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunitiesComponent implements OnInit {
 
-  constructor() { }
+  communities: ICommunity[] = [];
+
+  constructor(
+    private communitiesService: CommunitiesService
+  ) { }
 
   ngOnInit() {
+    this.getAllCommunities();
   }
+
+
+  getAllCommunities() {
+    this.communitiesService.pGetCommunities().subscribe(
+      data => {
+        this.communities = data.communities;
+      }
+    );
+  }
+
+
 
 }
