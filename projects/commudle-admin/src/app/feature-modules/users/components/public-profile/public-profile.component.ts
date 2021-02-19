0a9f5475e903c165a05e2404424d2d4513437472
Ashwin <bsashwin9@gmail.com>
2021-02-19 09:41:41 +0530

    Basic UI and functionality of user profile sidebar

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/public-profile.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/public-profile.component.ts
@@ -1,9 +1,9 @@
-import { IUser } from 'projects/shared-models/user.model';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AppUsersService } from 'projects/commudle-admin/src/app/services/app-users.service';
-import { ICurrentUser } from 'projects/shared-models/current_user.model';
-import { LibAuthwatchService } from 'projects/shared-services/lib-authwatch.service';
+import {IUser} from 'projects/shared-models/user.model';
+import {ActivatedRoute} from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AppUsersService} from 'projects/commudle-admin/src/app/services/app-users.service';
+import {ICurrentUser} from 'projects/shared-models/current_user.model';
+import {LibAuthwatchService} from 'projects/shared-services/lib-authwatch.service';
 
 @Component({
   selector: 'app-public-profile',
@@ -11,24 +11,24 @@ import { LibAuthwatchService } from 'projects/shared-services/lib-authwatch.serv
   styleUrls: ['./public-profile.component.scss']
 })
 export class PublicProfileComponent implements OnInit, OnDestroy {
+
   user: IUser;
   currentUser: ICurrentUser;
   subscriptions = [];
-
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private authWatchService: LibAuthwatchService,
     private usersService: AppUsersService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.usersService.getProfile(this.activatedRoute.snapshot.params.username).subscribe(
       data => {
         this.user = data;
       }
-    )
-
+    );
 
     this.subscriptions.push(
       this.authWatchService.currentUser$.subscribe(
@@ -44,5 +44,4 @@ export class PublicProfileComponent implements OnInit, OnDestroy {
       subs.unsubscribe();
     }
   }
-
 }
