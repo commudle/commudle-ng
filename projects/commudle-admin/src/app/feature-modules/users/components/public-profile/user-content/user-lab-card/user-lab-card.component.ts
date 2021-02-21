7ef5458f37f161f8571afe7f747e40c0085a9644
Ashwin <bsashwin9@gmail.com>
2021-02-21 14:52:08 +0530

    Fix issue with User lab card API

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-lab-card/user-lab-card.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-lab-card/user-lab-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ILab} from 'projects/shared-models/lab.model';
+import {IUser} from 'projects/shared-models/user.model';
 
 @Component({
   selector: 'app-user-lab-card',
@@ -8,6 +9,7 @@ import {ILab} from 'projects/shared-models/lab.model';
 })
 export class UserLabCardComponent implements OnInit {
 
+  @Input() user: IUser;
   @Input() lab: ILab;
 
   constructor() {
@@ -16,4 +18,12 @@ export class UserLabCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getDesc() {
+    // Decode HTML
+    const txt = document.createElement('textarea');
+    txt.innerHTML = this.lab.description;
+    const htmlContent = txt.value;
+    // Remove HTML tags, take the first 100 characters and add '...'
+    return htmlContent.replace(/<[^>]+>/g, '').substr(0, 100).concat('...');
+  }
 }
