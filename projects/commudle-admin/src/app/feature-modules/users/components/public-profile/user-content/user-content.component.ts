002667bead9b0a68f8b7d7a14eefa331bbe00f0d
Ashwin <bsashwin9@gmail.com>
2021-02-23 08:07:20 +0530

    Add navigation to cards in user content

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-content.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-content.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {IUser} from 'projects/shared-models/user.model';
 import {ILab} from 'projects/shared-models/lab.model';
 import {AppUsersService} from 'projects/commudle-admin/src/app/services/app-users.service';
@@ -10,7 +10,7 @@ import {IUserRolesUser} from 'projects/shared-models/user_roles_user.model';
   templateUrl: './user-content.component.html',
   styleUrls: ['./user-content.component.scss']
 })
-export class UserContentComponent implements OnInit {
+export class UserContentComponent implements OnInit, AfterViewChecked {
 
   @Input() user: IUser;
 
@@ -18,8 +18,15 @@ export class UserContentComponent implements OnInit {
   communities: IUserRolesUser[] = [];
   builds: ICommunityBuild[] = [];
 
+  communityNavButtonsHeight: number;
+  labNavButtonsHeight: number;
+
+  @ViewChild('communityCards') communityCards: ElementRef;
+  @ViewChild('labCards') labCards: ElementRef;
+
   constructor(
-    private appUsersService: AppUsersService
+    private appUsersService: AppUsersService,
+    private cdr: ChangeDetectorRef
   ) {
   }
 
@@ -30,6 +37,36 @@ export class UserContentComponent implements OnInit {
     this.appUsersService.communities(this.user.username).subscribe(value => this.communities = value.user_roles_users);
     // Get the user's builds
     this.appUsersService.communityBuilds(this.user.username).subscribe(value => this.builds = value.community_builds);
+  }
+
+  ngAfterViewChecked(): void {
+    // Set height of community navigation buttons
+    this.communityNavButtonsHeight = this.communityCards.nativeElement.offsetHeight / 2 + 23;
+    // Set height of lab navigation buttons
+    this.labNavButtonsHeight = this.labCards.nativeElement.offsetHeight / 2 + 23;
+    // Manually trigger change detection for the current component because we need to set the height of navigation buttons dynamically
+    this.cdr.detectChanges();
+  }
+
+  // Check if the given element is scrollable
+  isScrollable(element: HTMLDivElement) {
+    return element.offsetWidth < element.scrollWidth;
+  }
+
+  scrollLeft(element: HTMLDivElement) {
+    // Scroll left by two cards when width > 1000px else scroll left by one card
+    element.scrollTo({
+      left: (element.scrollLeft - 315 * (window.innerWidth > 1000 ? 2 : 1)),
+      behavior: 'smooth'
+    });
+  }
+
+  scrollRight(element: HTMLDivElement) {
+    // Scroll right by two cards when width > 1000px else scroll left by one card
+    element.scrollTo({
+      left: (element.scrollLeft + 315 * (window.innerWidth > 1000 ? 2 : 1)),
+      behavior: 'smooth'
+    });
   }
 
 }
