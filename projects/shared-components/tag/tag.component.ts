f2ba61446f940e850a1a0845835ea16a1199b0d7
Ashwin <bsashwin9@gmail.com>
2021-02-28 20:56:33 +0530

    Create tags component

---
+++ projects/shared-components/tag/tag.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss']
+})
+export class TagComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
