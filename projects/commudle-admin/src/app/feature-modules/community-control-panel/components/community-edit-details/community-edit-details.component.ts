6f3f6d535d1bc46669b9c7da617d035d150933e7
violet360 <mazedk2k@gmail.com>
2021-03-05 00:43:22 +0530

    community-form/tags

--- projects/commudle-admin/src/app/feature-modules/community-control-panel/components/community-edit-details/community-edit-details.component.ts
+++ projects/commudle-admin/src/app/feature-modules/community-control-panel/components/community-edit-details/community-edit-details.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ICommunity } from 'projects/shared-models/community.model';
-import { ActivatedRoute } from '@angular/router';
-import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
-import { DOCUMENT } from '@angular/common';
-import { CommunitiesService } from 'projects/commudle-admin/src/app/services/communities.service';
+import { Component, OnInit, Output, EventEmitter, Inject } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ICommunity } from "projects/shared-models/community.model";
+import { ActivatedRoute } from "@angular/router";
+import { LibToastLogService } from "projects/shared-services/lib-toastlog.service";
+import { DOCUMENT } from "@angular/common";
+import { CommunitiesService } from "projects/commudle-admin/src/app/services/communities.service";
 
 @Component({
-  selector: 'app-community-edit-details',
-  templateUrl: './community-edit-details.component.html',
-  styleUrls: ['./community-edit-details.component.scss']
+  selector: "app-community-edit-details",
+  templateUrl: "./community-edit-details.component.html",
+  styleUrls: ["./community-edit-details.component.scss"],
 })
 export class CommunityEditDetailsComponent implements OnInit {
   community: ICommunity;
   uploadedLogo: any;
   uploadedLogoFile: File;
+  tags: string[] = [];
 
   @Output() updateCommunity = new EventEmitter();
 
   communityForm = this.fb.group({
     community: this.fb.group({
-      id: [''],
-      name: ['', Validators.required],
-      about: [''],
-      mini_description: ['', Validators.required],
-      contact_email: ['', Validators.required],
-      facebook: [''],
-      twitter: [''],
-      github: [''],
-      website: [''],
-      linkedin: [''],
-    })
+      id: [""],
+      name: ["", Validators.required],
+      about: [""],
+      mini_description: ["", Validators.required],
+      contact_email: ["", Validators.required],
+      facebook: [""],
+      twitter: [""],
+      github: [""],
+      website: [""],
+      linkedin: [""],
+    }),
   });
 
   constructor(
@@ -38,27 +39,25 @@ export class CommunityEditDetailsComponent implements OnInit {
     private fb: FormBuilder,
     private communitiesService: CommunitiesService,
     private toastLogService: LibToastLogService,
-    @Inject(DOCUMENT) private document: Document,
-
-  ) {
-  }
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.parent.params.subscribe(params => {
+    this.activatedRoute.parent.params.subscribe((params) => {
       this.getCommunityDetails(params.community_id);
     });
   }
 
-
   getCommunityDetails(communityId) {
-    this.communitiesService.getCommunityDetails(communityId).subscribe((data) => {
-      this.community = data;
-      this.updateCommunity.emit(this.community);
-      this.communityForm.get('community').patchValue(this.community);
-      this.uploadedLogo = this.community.logo_path;
-    });
+    this.communitiesService
+      .getCommunityDetails(communityId)
+      .subscribe((data) => {
+        this.community = data;
+        this.updateCommunity.emit(this.community);
+        this.communityForm.get("community").patchValue(this.community);
+        this.uploadedLogo = this.community.logo_path;
+      });
   }
-
 
   displaySelectedLogo(event: any) {
     if (event.target.files && event.target.files[0]) {
@@ -73,22 +72,36 @@ export class CommunityEditDetailsComponent implements OnInit {
     }
   }
 
-
   updateCommunityDetails() {
     const formData: any = new FormData();
-    const communityFormData = this.communityForm.get('community').value;
-    Object.keys(communityFormData).forEach(
-      key => (!(communityFormData[key] == null) ? formData.append(`community[${key}]`, communityFormData[key]) : '')
-      );
+    const communityFormData = this.communityForm.get("community").value;
+    Object.keys(communityFormData).forEach((key) =>
+      !(communityFormData[key] == null)
+        ? formData.append(`community[${key}]`, communityFormData[key])
+        : ""
+    );
 
     if (this.uploadedLogoFile != null) {
-      formData.append('community[logo_image]', this.uploadedLogoFile);
+      formData.append("community[logo_image]", this.uploadedLogoFile);
     }
-    this.communitiesService.updateCommunity(formData, this.community.id).subscribe((community) => {
-      this.toastLogService.successDialog('Updated! Reloading the app for changes to apply...');
-      this.community = community;
-      this.document.location.reload();
-    });
+    this.communitiesService
+      .updateCommunity(formData, this.community.id)
+      .subscribe((community) => {
+        this.toastLogService.successDialog(
+          "Updated! Reloading the app for changes to apply..."
+        );
+        this.community = community;
+        this.document.location.reload();
+      });
   }
 
+  onTagAdd(value: string) {
+    if (!this.tags.includes(value)) {
+      this.tags.push(value);
+    }
+  }
+
+  onTagDelete(value: string) {
+    this.tags = this.tags.filter((tag) => tag !== value);
+  }
 }
