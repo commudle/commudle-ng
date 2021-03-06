529991c52a37398c8dd4544e62f3540a03bf1e6f
Ashwin <bsashwin9@gmail.com>
2021-03-06 08:22:06 +0530

    Create tag search UI and functionality

--- projects/commudle-admin/src/app/feature-modules/labs/components/labs/search-bar/search-bar.component.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/components/labs/search-bar/search-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {LabsService} from 'projects/commudle-admin/src/app/feature-modules/labs/services/labs.service';
+import {LabsHomeService} from 'projects/commudle-admin/src/app/feature-modules/labs/services/labs-home.service';
+import {ITag} from 'projects/shared-models/tag.model';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  filterLabItems = [
+    {title: 'Popular Labs'},
+    {title: 'New Labs'},
+    {title: 'Labs by Experts'}
+  ];
 
-  ngOnInit(): void {
+  tagSearchParam = '';
+  tagSearchResults: ITag[] = [];
+  // tagSearchResults: Set<ITag> = new Set<ITag>();
+
+  @ViewChild('searchBar') searchBar: ElementRef<HTMLInputElement>;
+  @ViewChild('searchParamsDiv') searchParamsDiv: ElementRef<HTMLDivElement>;
+  @ViewChild('searchResultsDiv') searchResultsDiv: ElementRef<HTMLDivElement>;
+
+  constructor(
+    private labsService: LabsService,
+    private labsHomeService: LabsHomeService
+  ) {
+    document.addEventListener('click', this.clickHandler.bind(this));
   }
 
+  ngOnInit(): void {
+    // Subscribe to tag search results
+    this.labsHomeService.tagSearch$.subscribe(data => this.tagSearchResults = data);
+  }
+
+  getTagSearchResults() {
+    if (this.tagSearchParam !== '') {
+      this.labsHomeService.getTagSearchResults(Array(this.tagSearchParam));
+    } else {
+      this.tagSearchResults = [];
+    }
+    console.log(this.tagSearchParam);
+  }
+
+  clickHandler(event: any) {
+    const elements: ElementRef[] = [this.searchBar, this.searchParamsDiv, this.searchResultsDiv];
+    this.searchResultsDiv.nativeElement.style.display = elements.some(element => element.nativeElement.contains(event.target)) ? 'flex' : 'none';
+  }
+
+  // onTagAdd(eventTarget: EventTarget) {
+  //   const value = (eventTarget as HTMLInputElement).value;
+  //   if (!this.searchParams.includes(value)) {
+  //     this.searchParams.push(value);
+  //   }
+  //   this.searchBar.nativeElement.value = '';
+  // }
+  //
+  // onTagDelete(value: string) {
+  //   this.searchParams = this.searchParams.filter(tag => tag !== value);
+  // }
 }
--- projects/commudle-admin/src/app/feature-modules/labs/services/labs-home.service.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/services/labs-home.service.ts
@@ -1,26 +1,29 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {ApiRoutesService} from 'projects/shared-services/api-routes.service';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {LabsService} from 'projects/commudle-admin/src/app/feature-modules/labs/services/labs.service';
+import {ITag} from 'projects/shared-models/tag.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LabsHomeService {
-  // behaviour subject for list of searched tagnames --> subscribed to by search bar (initialize by empty array of ITag)
+
+  // behaviour subject for list of searched tag names --> subscribed to by search bar (initialize by empty array of ITag)
+  private tagSearch: BehaviorSubject<ITag[]> = new BehaviorSubject<ITag[]>([]);
+  public tagSearch$: Observable<ITag[]> = this.tagSearch.asObservable();
+
   // behaviour subject for list of searched labs --> subscribed to by labs component (initialize by empty array of ILab)
 
-
   constructor(
-    private http: HttpClient,
-    private apiRoutesService: ApiRoutesService
+    private labsService: LabsService
   ) {
   }
 
   addSearchTag(tagName) {
-
     // update the list of searched labs through this
-
   }
-  
+
+  getTagSearchResults(value: string[]) {
+    this.labsService.searchTags(value).subscribe(data => this.tagSearch.next(data.tags))
+  }
 }
