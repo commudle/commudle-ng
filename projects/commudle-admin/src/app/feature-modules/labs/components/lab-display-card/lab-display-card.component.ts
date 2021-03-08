385c78e83ac4a7fe3787c0329150ee1704048e5d
arpansac <arpansac@gmail.com>
2021-03-08 11:28:12 +0530

    fix lab card design

--- projects/commudle-admin/src/app/feature-modules/labs/components/lab-display-card/lab-display-card.component.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/components/lab-display-card/lab-display-card.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { ILab } from "projects/shared-models/lab.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { ILab } from 'projects/shared-models/lab.model';
 
 @Component({
-  selector: "app-lab-display-card",
-  templateUrl: "./lab-display-card.component.html",
-  styleUrls: ["./lab-display-card.component.scss"],
+  selector: 'app-lab-display-card',
+  templateUrl: './lab-display-card.component.html',
+  styleUrls: ['./lab-display-card.component.scss'],
 })
 export class LabDisplayCardComponent implements OnInit {
   @Input() lab: ILab;
--- projects/commudle-admin/src/app/feature-modules/labs/components/labs/labs.component.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/components/labs/labs.component.ts
@@ -1,30 +1,21 @@
-import {
-  Component,
-  OnInit,
-  TemplateRef,
-  ViewChild,
-  OnDestroy,
-} from "@angular/core";
-import * as moment from "moment";
-import { ILab, EPublishStatus } from "projects/shared-models/lab.model";
-import { LabsService } from "../../services/labs.service";
-import { Title, Meta } from "@angular/platform-browser";
-import { LibToastLogService } from "projects/shared-services/lib-toastlog.service";
-import { LibAuthwatchService } from "projects/shared-services/lib-authwatch.service";
-import { NbWindowService } from "@nebular/theme";
-import { faFlask } from "@fortawesome/free-solid-svg-icons";
-import { ITag } from "projects/shared-models/tag.model";
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { ILab, EPublishStatus } from 'projects/shared-models/lab.model';
+import { LabsService } from '../../services/labs.service';
+import { Title, Meta } from '@angular/platform-browser';
+import { faFlask } from '@fortawesome/free-solid-svg-icons';
+import { ITag } from 'projects/shared-models/tag.model';
 
 @Component({
-  selector: "app-labs",
-  templateUrl: "./labs.component.html",
-  styleUrls: ["./labs.component.scss"],
+  selector: 'app-labs',
+  templateUrl: './labs.component.html',
+  styleUrls: ['./labs.component.scss'],
 })
 export class LabsComponent implements OnInit {
   faFlask = faFlask;
 
   popularTags: ITag[];
-  Labs: ILab[];
+  labs: ILab[];
   selectedTag;
 
   constructor(
@@ -42,43 +33,43 @@ export class LabsComponent implements OnInit {
   }
 
   setMeta() {
-    this.title.setTitle("Labs | Learn Something New!");
+    this.title.setTitle('Labs | Learn Something New!');
     this.meta.updateTag({
-      name: "description",
+      name: 'description',
       content:
-        "The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!",
+        'The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!',
     });
     this.meta.updateTag({
-      name: "og:image",
+      name: 'og:image',
       content: `https://commudle.com/assets/images/commudle-logo192.png`,
     });
     this.meta.updateTag({
-      name: "og:image:secure_url",
+      name: 'og:image:secure_url',
       content: `https://commudle.com/assets/images/commudle-logo192.png`,
     });
     this.meta.updateTag({
-      name: "og:title",
-      content: "Labs | Learn Something New!",
+      name: 'og:title',
+      content: 'Labs | Learn Something New!',
     });
     this.meta.updateTag({
-      name: "og:description",
+      name: 'og:description',
       content:
-        "The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!",
+        'The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!',
     });
-    this.meta.updateTag({ name: "og:type", content: "website" });
+    this.meta.updateTag({ name: 'og:type', content: 'website' });
 
     this.meta.updateTag({
-      name: "twitter:image",
+      name: 'twitter:image',
       content: `https://commudle.com/assets/images/commudle-logo192.png`,
     });
     this.meta.updateTag({
-      name: "twitter:title",
-      content: "Labs | Learn Something New!",
+      name: 'twitter:title',
+      content: 'Labs | Learn Something New!',
     });
     this.meta.updateTag({
-      name: "twitter:description",
+      name: 'twitter:description',
       content:
-        "The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!",
+        'The best way to learn, is step by step. We introduce Labs, a place where you will find tutorials created by everyone who learnt something new and wants to make it easy for others to learn too!',
     });
   }
 
@@ -91,7 +82,7 @@ export class LabsComponent implements OnInit {
   getLabs(tag) {
     this.selectedTag = tag;
     this.labsService.pIndex(tag).subscribe((data) => {
-      this.Labs = data.labs;
+      this.labs = data.labs;
     });
   }
 }
