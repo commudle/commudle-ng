983df570cc226c088b5937333697446cea6ff625
Ashwin <bsashwin9@gmail.com>
2021-03-15 17:53:47 +0530

    Fix bugs

--- projects/commudle-admin/src/environments/environment.ts
+++ projects/commudle-admin/src/environments/environment.ts
@@ -5,18 +5,18 @@
 export const environment = {
   production: false,
   app_url: 'http://localhost:4200',
-  base_url: 'http://localhost:3000',
-  action_cable_url: 'http://localhost:3000/live',
-  auth_cookie_name: 'dev_commudle_user_auth',
-  zoom_call_server_url: 'http://localhost:4000',
+  // base_url: 'http://localhost:3000',
+  // action_cable_url: 'http://localhost:3000/live',
+  // auth_cookie_name: 'dev_commudle_user_auth',
+  // zoom_call_server_url: 'http://localhost:4000',
 
   session_cookie_name: 'cmdle_sess',
 
   // app_url: 'https://commudle.com',
-  // base_url: 'https://json.commudle.com',
-  // action_cable_url: 'https://json.commudle.com/live',
-  // auth_cookie_name: 'commudle_user_auth',
-  // zoom_call_server_url: 'https://zoom.commudle.com',
+  base_url: 'https://json.commudle.com',
+  action_cable_url: 'https://json.commudle.com/live',
+  auth_cookie_name: 'commudle_user_auth',
+  zoom_call_server_url: 'https://zoom.commudle.com',
 };
 
 /*
