7632e4307d8b3855509d8b728fea70c08b7d321d
Ashwin <bsashwin9@gmail.com>
2021-03-20 07:05:35 +0530

    Refactoring and CSS fixes

--- projects/commudle-admin/src/app/app.module.ts
+++ projects/commudle-admin/src/app/app.module.ts
@@ -72,7 +72,7 @@ import {CommunityChannelsModule} from './feature-modules/community-channels/comm
 import {UserChatsModule} from './feature-modules/user-chats/user-chats.module';
 import {HomeEventsComponent} from './components/home/components/home-events/home-events.component';
 import {HomeEventsCardComponent} from './components/home/components/home-events/home-events-card/home-events-card.component';
-import {HomeLabsComponent} from './components/home/home-labs/home-labs.component';
+import {HomeLabsComponent} from './components/home/components/home-labs/home-labs.component';
 import {SkeletonScreensModule} from './feature-modules/skeleton-screens/skeleton-screens.module';
 
 
