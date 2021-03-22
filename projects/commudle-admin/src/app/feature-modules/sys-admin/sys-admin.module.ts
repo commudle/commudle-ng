6977f6f95bf5f2939ea81f63dfa532fc794c74f9
arpansac <arpansac@gmail.com>
2021-03-22 12:46:22 +0530

    divide admin labs into tabs based on status

--- projects/commudle-admin/src/app/feature-modules/sys-admin/sys-admin.module.ts
+++ projects/commudle-admin/src/app/feature-modules/sys-admin/sys-admin.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SysAdminRoutingModule } from './sys-admin-routing.module';
 import { SysAdminComponent } from './sys-admin.component';
 import { AdminSurveysComponent } from './components/admin-surveys/admin-surveys.component';
-import { NbCardModule, NbWindowModule, NbIconModule, NbButtonModule, NbListModule, NbSelectModule, NbSpinnerModule } from '@nebular/theme';
+import { NbCardModule, NbWindowModule, NbIconModule, NbButtonModule, NbListModule, NbSelectModule, NbSpinnerModule, NbTabsetModule } from '@nebular/theme';
 import { SharedComponentsModule } from 'projects/shared-components/shared-components.module';
 import { CommunityBuildsComponent } from './components/community-builds/community-builds.component';
 import { LabsComponent } from './components/labs/labs.component';
@@ -30,7 +30,8 @@ import { LabsComponent } from './components/labs/labs.component';
     NbIconModule,
     NbButtonModule,
     NbListModule,
-    NbSelectModule
+    NbSelectModule,
+    NbTabsetModule
   ]
 })
 export class SysAdminModule { }
