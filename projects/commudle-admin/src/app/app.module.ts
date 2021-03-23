5024e81776a2137203b7c44462ddc854e834d4ad
Ashwin <bsashwin9@gmail.com>
2021-03-23 18:35:58 +0530

    Create home builds section

--- projects/commudle-admin/src/app/app.module.ts
+++ projects/commudle-admin/src/app/app.module.ts
@@ -74,6 +74,8 @@ import {HomeEventsComponent} from './components/home/components/home-events/home
 import {HomeEventsCardComponent} from './components/home/components/home-events/home-events-card/home-events-card.component';
 import {HomeLabsComponent} from './components/home/components/home-labs/home-labs.component';
 import {HomeCommunitiesComponent} from './components/home/components/home-communities/home-communities.component';
+import {HomeBuildsComponent} from './components/home/components/home-builds/home-builds.component';
+import {HomeBuildsCardComponent} from './components/home/components/home-builds/home-builds-card/home-builds-card.component';
 import {SkeletonScreensModule} from './feature-modules/skeleton-screens/skeleton-screens.module';
 
 
@@ -103,7 +105,9 @@ export function initApp(appInitService: AppInitService) {
     HomeEventsComponent,
     HomeEventsCardComponent,
     HomeLabsComponent,
-    HomeCommunitiesComponent
+    HomeCommunitiesComponent,
+    HomeBuildsComponent,
+    HomeBuildsCardComponent
   ],
   imports: [
     AppRoutingModule,
