9af9a497365fb4ab319a84a9ebf7312383f795a0
arpansac <arpansac@gmail.com>
2021-03-23 20:07:30 +0530

    set meta tags

--- projects/commudle-admin/src/app/feature-modules/pricing/components/pricing/pricing.component.ts
+++ projects/commudle-admin/src/app/feature-modules/pricing/components/pricing/pricing.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.css']
+  styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) { }
 
   ngOnInit(): void {
+    this.setMeta();
+  }
+
+  setMeta() {
+    this.title.setTitle('Pricing & Features');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Plans for all Developer Communities, student, individual, non-profite and enterprise!'
+    });
+    this.meta.updateTag({name: 'og:image', content: 'https://commudle.com/assets/images/commudle-logo192.png'});
+    this.meta.updateTag({name: 'og:image:secure_url', content: 'https://commudle.com/assets/images/commudle-logo192.png'});
+    this.meta.updateTag({name: 'og:title', content: 'Pricing & Features'});
+    this.meta.updateTag({
+      name: 'og:description',
+      content: 'Plans for all Developer Communities, student, individual, non-profite and enterprise!'
+    });
+    this.meta.updateTag({name: 'og:type', content: 'website'});
+
+    this.meta.updateTag({name: 'twitter:image', content: 'https://commudle.com/assets/images/commudle-logo192.png'});
+    this.meta.updateTag({name: 'twitter:title', content: 'Pricing & Features'});
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: 'Plans for all Developer Communities, student, individual, non-profite and enterprise!'
+    });
   }
 
 }
