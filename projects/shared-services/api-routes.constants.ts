35f5204b1b0a94eada7382c332303027c43a376e
arpansac <arpansac@gmail.com>
2021-04-01 20:25:26 +0530

    speaker resources API

--- projects/shared-services/api-routes.constants.ts
+++ projects/shared-services/api-routes.constants.ts
@@ -298,6 +298,7 @@ export const API_ROUTES = {
     LABS: 'api/v2/users/labs',
     COMMUNITY_BUILDS: 'api/v2/users/community_builds',
     BADGES: 'api/v2/users/badges',
+    SPEAKER_RESOURCES: 'api/v2/users/speaker_resources',
     SOCIAL_RESOURCES: 'api/v2/users/social_resources',
     TOGGLE_FOLLOW: 'api/v2/users/toggle_follow',
     CHECK_FOLLOWEE: 'api/v2/users/check_followee',
