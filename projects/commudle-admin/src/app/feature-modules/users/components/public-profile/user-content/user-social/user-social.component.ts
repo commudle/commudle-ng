e4f2fa4f1919810c85c2b3b9f09cbd3aea40d103
arpansac <arpansac@gmail.com>
2021-04-07 15:40:11 +0530

    add header banner to the mobile home page

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-social/user-social.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-social/user-social.component.ts
@@ -42,12 +42,10 @@ export class UserSocialComponent implements OnInit, OnDestroy {
     })
   });
   tags: string[] = [];
-  urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  urlPattern = new RegExp(
+    '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
+    );
+
 
   @ViewChild('addLinkDialog') addLinkDialog: TemplateRef<any>;
   addLinkDialogRef: NbDialogRef<any>;
