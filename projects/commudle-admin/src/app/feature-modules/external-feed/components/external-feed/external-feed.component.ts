88d3730be91ab2dbca91bf8994f76d7fe8fe7fc3
Akshyta Katyal <akshyta17216@iiitd.ac.in>
2021-04-14 19:41:47 +0530

    changes in external-feed component for fetching list of posts

--- projects/commudle-admin/src/app/feature-modules/external-feed/components/external-feed/external-feed.component.ts
+++ projects/commudle-admin/src/app/feature-modules/external-feed/components/external-feed/external-feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ExternalFeedService } from 'projects/commudle-admin/src/app/services/external-feed.service';
+import { Title, Meta } from '@angular/platform-browser';
+import { ISingleExternalFeed } from 'projects/shared-models/single-external-feed.model';
 
 @Component({
   selector: 'app-external-feed',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./external-feed.component.scss']
 })
 export class ExternalFeedComponent implements OnInit {
+  externalPosts: ISingleExternalFeed[] = [];
+  page = 1;
+  count = 10;
+  total;
+  isLoading = false;
+  canLoadMore = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private externalFeedService: ExternalFeedService,
+    private title: Title,
+    private meta: Meta
+  ) {
+    title.setTitle('Feed Posts');
   }
 
+  ngOnInit() {
+    this.getFeedPosts();
+  }
+
+  getFeedPosts() {
+    if (!this.isLoading && (!this.total || this.externalPosts.length < this.total)) {
+      this.externalFeedService.pGetAll(this.page, this.count).subscribe(
+        data => {
+          this.externalPosts = this.externalPosts.concat(data.feed_posts);
+          this.page += 1;
+          this.total = data.total;
+          this.isLoading = false;
+          if (this.externalPosts.length >= this.total) {
+            this.canLoadMore = false;
+          }
+        }
+      );
+    }
+  }
 }
