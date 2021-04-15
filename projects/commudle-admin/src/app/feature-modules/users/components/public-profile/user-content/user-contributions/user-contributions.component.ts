ef6e01217b646fe366396eff38f85e1afdf62722
Ashwin <bsashwin9@gmail.com>
2021-04-15 08:55:32 +0530

    User profile sidewise navigation

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-contributions/user-contributions.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-content/user-contributions/user-contributions.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {AfterViewChecked, Component, Input, OnDestroy, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {AppUsersService} from 'projects/commudle-admin/src/app/services/app-users.service';
 import {IUser} from 'projects/shared-models/user.model';
@@ -12,7 +12,7 @@ import {ISpeakerResource} from 'projects/shared-models/speaker_resource.model';
   templateUrl: './user-contributions.component.html',
   styleUrls: ['./user-contributions.component.scss']
 })
-export class UserContributionsComponent implements OnInit, OnChanges, OnDestroy {
+export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   @Input() user: IUser;
 
@@ -23,49 +23,63 @@ export class UserContributionsComponent implements OnInit, OnChanges, OnDestroy 
 
   subscriptions: Subscription[] = [];
 
+  @ViewChildren('content') sections: QueryList<any>;
+  @ViewChildren('navigation') navigations: QueryList<any>;
+
   constructor(
-    private appUsersService: AppUsersService
+    private appUsersService: AppUsersService,
   ) {
   }
 
   ngOnInit(): void {
     // Get the user's past events
-
-  }
-
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.user) {
-      this.ngOnDestroy()
-      this.subscriptions.push(this.appUsersService.speakerResources(this.user.username).subscribe(value => {
-        this.pastEvents = value.speaker_resources;
-      }));
-
-      // Get the user's communities
-      this.subscriptions.push(this.appUsersService.communities(this.user.username).subscribe(value => {
-        this.communities = value.user_roles_users;
-        // TODO: If some community is undefined then remove it, is it required?
-        this.communities.forEach(community => {
-          if (!community.community) {
-            this.communities.splice(this.communities.indexOf(community), 1);
-          }
-        })
-      }));
-
-      // Get the user's labs
-      this.subscriptions.push(this.appUsersService.labs(this.user.username).subscribe(value => {
-        this.labs = value.labs;
-      }));
-
-      // Get the user's builds
-      this.subscriptions.push(this.appUsersService.communityBuilds(this.user.username).subscribe(value => {
-        this.builds = value.community_builds;
-      }));
-    }
+    this.subscriptions.push(this.appUsersService.speakerResources(this.user.username).subscribe(value => {
+      this.pastEvents = value.speaker_resources;
+    }));
+    // Get the user's communities
+    this.subscriptions.push(this.appUsersService.communities(this.user.username).subscribe(value => {
+      this.communities = value.user_roles_users;
+      // TODO: If some community is undefined then remove it, is it required?
+      this.communities.forEach(community => {
+        if (!community.community) {
+          this.communities.splice(this.communities.indexOf(community), 1);
+        }
+      })
+    }));
+    // Get the user's labs
+    this.subscriptions.push(this.appUsersService.labs(this.user.username).subscribe(value => {
+      this.labs = value.labs;
+    }));
+    // Get the user's builds
+    this.subscriptions.push(this.appUsersService.communityBuilds(this.user.username).subscribe(value => {
+      this.builds = value.community_builds;
+    }));
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(value => value.unsubscribe());
+  }
+
+  ngAfterViewChecked(): void {
+    this.sections.forEach((section, idx) => {
+      if (!this.isScrollable(section.nativeElement)) {
+        this.navigations.toArray()[idx].nativeElement.classList.remove('active');
+      }
+    });
+  }
+
+  // Check if the given element is scrollable
+  isScrollable(element: HTMLDivElement) {
+    return element.offsetWidth < element.scrollWidth;
+  }
+
+  scrollElement(event: MouseEvent, direction: number) {
+    // @ts-ignore
+    const element = event.srcElement.parentElement.previousSibling;
+    element.scrollTo({
+      left: (element.scrollLeft + direction * 294),
+      behavior: 'smooth'
+    });
   }
 
 }
