a4026019099e0f5efe84d5bbb826bcc92f1c5858
harshita2216 <harshita2216@gmail.com>
2021-04-17 02:28:45 +0530

    speaker resources and discussion component

--- projects/commudle-admin/src/app/feature-modules/speaker-resources/components/speaker-resource/speaker-resource.component.ts
+++ projects/commudle-admin/src/app/feature-modules/speaker-resources/components/speaker-resource/speaker-resource.component.ts
@@ -1,50 +1,54 @@
-import { DiscussionsService } from 'projects/commudle-admin/src/app/services/discussions.service';
-import { IDiscussion } from './../../../../../../../shared-models/discussion.model';
-import { ISpeakerResource } from 'projects/shared-models/speaker_resource.model';
-import * as moment from 'moment';
-import { Component, OnInit } from '@angular/core';
-import { SpeakerResourcesService } from 'projects/commudle-admin/src/app/services/speaker-resources.service';
-import { ActivatedRoute } from '@angular/router';
-import { ICommunity } from 'projects/shared-models/community.model';
-
+import { DiscussionsService } from "projects/commudle-admin/src/app/services/discussions.service";
+import { IDiscussion } from "./../../../../../../../shared-models/discussion.model";
+import { ISpeakerResource } from "projects/shared-models/speaker_resource.model";
+import * as moment from "moment";
+import { Component, OnInit } from "@angular/core";
+import { SpeakerResourcesService } from "projects/commudle-admin/src/app/services/speaker-resources.service";
+import { ActivatedRoute } from "@angular/router";
+import { ICommunity } from "projects/shared-models/community.model";
 
 @Component({
-  selector: 'app-speaker-resource',
-  templateUrl: './speaker-resource.component.html',
-  styleUrls: ['./speaker-resource.component.scss']
+  selector: "app-speaker-resource",
+  templateUrl: "./speaker-resource.component.html",
+  styleUrls: ["./speaker-resource.component.scss"],
 })
 export class SpeakerResourceComponent implements OnInit {
-
-
-  speaker :ISpeakerResource;
+  speaker: ISpeakerResource;
   speaker_id;
   moment = moment;
   community: ICommunity;
 
-  discussion : IDiscussion;
-
-
+  discussion: IDiscussion;
+  discussionChat: IDiscussion;
+  messagesCount: number;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private speakerResourcesService: SpeakerResourcesService,
-    private discussionService : DiscussionsService,
-
-  ) { }
+    private discussionService: DiscussionsService
+  ) {}
 
   ngOnInit(): void {
-
-    this.speaker_id = this.activatedRoute.snapshot.params['speaker_resource_id'];
-    this.activatedRoute.parent.data.subscribe(data => {
-        this.getSpeaker(this.speaker_id);
-      }
-    )
+    this.speaker_id = this.activatedRoute.snapshot.params[
+      "speaker_resource_id"
+    ];
+    this.activatedRoute.parent.data.subscribe((data) => {
+      this.getSpeaker(this.speaker_id);
+      this.getDiscussionChat();
+    });
   }
-  getSpeaker(speaker_id){
-    this.speakerResourcesService.getDetails(speaker_id).subscribe(
-      data => {
-        this.speaker = data;
-      }
-    );
+  getSpeaker(speaker_id) {
+    this.speakerResourcesService.getDetails(speaker_id).subscribe((data) => {
+      this.speaker = data;
+      console.log(this.speaker);
+    });
+  }
+  getMessagesCount(count: number) {
+    this.messagesCount = count;
+  }
+  getDiscussionChat() {
+    this.discussionService
+      .pGetOrCreateForSpeakerResourceChat(this.speaker_id)
+      .subscribe((data) => (this.discussionChat = data));
   }
 }
