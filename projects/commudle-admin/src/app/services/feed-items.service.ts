eabede9797627d7f10f1bc74372f1cf972b11ecc
Anushika <37685708+anushika99@users.noreply.github.com>
2021-05-04 23:57:31 +0530

    Update feed APIs

--- projects/commudle-admin/src/app/services/feed-items.service.ts
+++ projects/commudle-admin/src/app/services/feed-items.service.ts
@@ -1,62 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { ApiRoutesService } from 'projects/shared-services/api-routes.service';
-import { API_ROUTES } from 'projects/shared-services/api-routes.constants';
-import { IFeedItems } from 'projects/shared-models/feed-items.model';
-import { IFeedItem } from 'projects/shared-models/feed-item.model';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { ApiRoutesService } from "projects/shared-services/api-routes.service";
+import { API_ROUTES } from "projects/shared-services/api-routes.constants";
+import { IFeedItems } from "projects/shared-models/feed-items.model";
+import { IFeedItem } from "projects/shared-models/feed-item.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FeedItemService {
-
-  private feed_api = 'http://15.207.110.193/feed/v2/latest-feed/'
-  private post_api = 'http://15.207.110.193/feed/v2/post/'
-  private tag_based_feed_api = 'http://15.207.110.193/feed/v2/tag-based-feed/'
-  private popular_tags_api = 'http://15.207.110.193/feed/v2/popular-tags/'
-  private popular_feed_api = 'http://15.207.110.193/feed/v2/popular-feed/'
+  private popular_tags_api = "http://15.207.110.193/feed/v2/popular-tags/";
 
   constructor(
     private http: HttpClient,
     private apiRoutesService: ApiRoutesService
-  ) { }
+  ) {}
 
-  pGetAll(): Observable<IFeedItems>{
+  pGetAll(page): Observable<IFeedItems> {
+    const params = new HttpParams().set("page", page);
     return this.http.get<IFeedItems>(
-        this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.INDEX));
+      this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.INDEX),
+      { params }
+    );
   }
 
-  pShow(id): Observable<IFeedItem>{
-    const params = new HttpParams().set('feed_item_id', id);
-    return this.http.get<IFeedItem>(
-        this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.SHOW), {params});
-  }
-
-  pGetAllv2(page){
-    const params = new HttpParams().set('page', page);
-    return this.http.get(this.feed_api, {params});
-  }
-
-  pGetPopularFeed(page){
-    const params = new HttpParams().set('page', page);
-    return this.http.get(this.popular_feed_api, {params});
-  }
-
-  pGetTagBasedFeed(tags, page){
+  pGetTagBasedFeed(tags, page, sort): Observable<IFeedItems> {
     let params = new HttpParams();
-    params = params.append('tags', tags.join(','));
-    params = params.append('page', page);
-    return this.http.get(this.tag_based_feed_api, {params});
+    for (let index in tags) {
+      params = params.append("tags[]", tags[index]);
+    }
+    params = params.append("page", page);
+    params = params.append("sort", sort);
+    return this.http.get<IFeedItems>(
+      this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.TAGS_POST),
+      { params }
+    );
+  }
+
+  pGetPopularFeed(page): Observable<IFeedItems> {
+    const params = new HttpParams().set("page", page);
+    return this.http.get<IFeedItems>(
+      this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.POPULAR_POST),
+      { params }
+    );
+  }
+
+  pShow(id): Observable<IFeedItem> {
+    const params = new HttpParams().set("feed_item_id", id);
+    return this.http.get<IFeedItem>(
+      this.apiRoutesService.getRoute(API_ROUTES.EXTERNAL_FEEDS.SHOW),
+      { params }
+    );
   }
 
   pGetPopularTags() {
-   return this.http.get(this.popular_tags_api); 
+    return this.http.get(this.popular_tags_api);
   }
-
-  pShowv2(id){
-    const params = new HttpParams().set('id', id);
-    return this.http.get(this.post_api, {params});
-  }
-
 }
