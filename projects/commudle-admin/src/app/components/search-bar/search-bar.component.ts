2883f3250d29d3857202b837a8672a126cabde1b
Kaushik Sriram <kaushik162000@gmail.com>
2021-05-28 14:37:55 +0530

    Changed border radius of search field to 8px

--- projects/commudle-admin/src/app/components/search-bar/search-bar.component.ts
+++ projects/commudle-admin/src/app/components/search-bar/search-bar.component.ts
@@ -5,14 +5,13 @@ import {debounceTime} from 'rxjs/operators';
 import {HomeService} from 'projects/commudle-admin/src/app/services/home.service';
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  selector: "app-search-bar",
+  templateUrl: "./search-bar.component.html",
+  styleUrls: ["./search-bar.component.scss"],
 })
 export class SearchBarComponent implements OnInit, OnDestroy {
-
   // Search variables
-  searchQuery = '';
+  searchQuery = "";
   searchQueryChanged: Subject<string> = new Subject<string>();
   searchQueryChangedSubscription: Subscription;
   searchResults: IHomeSearch = {
@@ -20,29 +19,26 @@ export class SearchBarComponent implements OnInit, OnDestroy {
     communities: [],
     events: [],
     labs: [],
-    users: []
+    users: [],
   };
   showSearchResults = false;
   showPlaceholder = true;
   showLoading = false;
   // Filters
-  filterBy = ['builds', 'communities', 'events', 'labs', 'users'];
+  filterBy = ["builds", "communities", "events", "labs", "users"];
   filters: string[] = [];
 
-  constructor(
-    private homeService: HomeService
-  ) {
-  }
+  constructor(private homeService: HomeService) {}
 
   ngOnInit(): void {
     // Subscribe to search
-    this.searchQueryChangedSubscription = this.searchQueryChanged.pipe(
-      debounceTime(800)
-    ).subscribe(value => {
-      this.showLoading = true;
-      this.searchQuery = value;
-      this.getSearchResults(value);
-    });
+    this.searchQueryChangedSubscription = this.searchQueryChanged
+      .pipe(debounceTime(800))
+      .subscribe((value) => {
+        this.showLoading = true;
+        this.searchQuery = value;
+        this.getSearchResults(value);
+      });
   }
 
   ngOnDestroy(): void {
@@ -57,33 +53,52 @@ export class SearchBarComponent implements OnInit, OnDestroy {
   toggleSearchSuffix(value: boolean, event?: Event) {
     if (event) {
       const clickingInSelect = (event as any).path.some((e: HTMLElement) => {
-        return e.classList && e.classList.contains('cdk-overlay-pane');
+        return e.classList && e.classList.contains("cdk-overlay-pane");
       });
 
       if (clickingInSelect) return;
     }
 
     this.showSearchResults = value;
+    this.borderChange(this.showSearchResults);
+  }
+
+  borderChange(showSearchResults) {
+    var searchInput = document.getElementById("search-input");
+    var searchSelect = document.getElementsByTagName("button")[0];
+
+    if (showSearchResults) {
+      searchInput.style.borderBottomLeftRadius = "0";
+      searchSelect.style.borderBottomRightRadius = "0";
+    } else {
+      setTimeout(() => {
+        searchInput.style.borderBottomLeftRadius = "8px";
+      }, 300);
+      setTimeout(() => {
+        searchSelect.style.borderBottomRightRadius = "8px";
+      }, 150);
+    }
   }
 
   getSearchResults(query: string) {
-    if (query !== '') {
+    if (query !== "") {
       this.showPlaceholder = false;
-      this.homeService.searchEverything(query, this.filters).subscribe(value => {
-        this.showLoading = false;
-        this.searchResults = value;
-      });
+      this.homeService
+        .searchEverything(query, this.filters)
+        .subscribe((value) => {
+          this.showLoading = false;
+          this.searchResults = value;
+        });
     } else {
       this.searchResults = {
         builds: [],
         communities: [],
         events: [],
         labs: [],
-        users: []
+        users: [],
       };
       this.showPlaceholder = true;
       this.showLoading = false;
     }
   }
-
 }
