aa380fe9e133139243d113c8de85623ac6b245c4
Ashwin <bsashwin9@gmail.com>
2021-06-06 12:02:42 +0530

    Create component for qna in session page

--- projects/commudle-admin/src/app/feature-modules/public-events/components/session-page/session-page-video/session-page-qna/session-page-qna.component.ts
+++ projects/commudle-admin/src/app/feature-modules/public-events/components/session-page/session-page-video/session-page-qna/session-page-qna.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IDiscussion } from 'projects/shared-models/discussion.model';
 
 @Component({
   selector: 'app-session-page-qna',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionPageQnaComponent implements OnInit {
 
+  @Input() qna: IDiscussion;
+  @Output() newQna: EventEmitter<any> = new EventEmitter<any>();
+
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  newQnaNotification(): void {
+    this.newQna.emit();
   }
 
 }
