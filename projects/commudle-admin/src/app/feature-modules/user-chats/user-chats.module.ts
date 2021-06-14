b098ba06b73afec9f005034412fb896e18202116
Ashwin <bsashwin9@gmail.com>
2021-06-14 14:28:44 +0530

    Change the UI of chats to accommodate the session page

--- projects/commudle-admin/src/app/feature-modules/user-chats/user-chats.module.ts
+++ projects/commudle-admin/src/app/feature-modules/user-chats/user-chats.module.ts
@@ -1,25 +1,32 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ChatsContainerComponent} from './components/chats-container/chats-container.component';
-import {ChatsListComponent} from './components/chats-list/chats-list.component';
-import {SharedComponentsModule} from '../../../../../shared-components/shared-components.module';
-import {NbButtonModule, NbCardModule, NbIconModule, NbListModule, NbUserModule} from '@nebular/theme';
-import {ChatsWindowComponent} from './components/chats-window/chats-window.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NbButtonModule, NbCardModule, NbIconModule, NbListModule, NbTooltipModule, NbUserModule } from '@nebular/theme';
+import { SharedComponentsModule } from 'projects/shared-components/shared-components.module';
+import { ChatsContainerComponent } from './components/chats-container/chats-container.component';
+import { ChatsListComponent } from './components/chats-list/chats-list.component';
+import { ChatsWindowComponent } from './components/chats-window/chats-window.component';
 
 
 @NgModule({
-  declarations: [ChatsContainerComponent, ChatsListComponent, ChatsWindowComponent],
+  declarations: [
+    ChatsContainerComponent,
+    ChatsListComponent,
+    ChatsWindowComponent
+  ],
   exports: [
     ChatsContainerComponent
   ],
   imports: [
     CommonModule,
     SharedComponentsModule,
+
+    // Nebular
     NbCardModule,
     NbListModule,
     NbUserModule,
     NbButtonModule,
     NbIconModule,
+    NbTooltipModule
   ]
 })
 export class UserChatsModule {
