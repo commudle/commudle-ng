d18ee54e31b5a7031003000827bac0f393de7215
Kaushik Sriram <kaushik162000@gmail.com>
2021-06-15 22:19:50 +0530

    Make contributions independent of input

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-extra-details/user-content/user-contributions/user-contributions.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-extra-details/user-content/user-contributions/user-contributions.component.ts
@@ -1,11 +1,11 @@
 import {AfterViewChecked, Component, Input, OnDestroy, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {AppUsersService} from 'projects/commudle-admin/src/app/services/app-users.service';
-import {IUser} from 'projects/shared-models/user.model';
 import {ILab} from 'projects/shared-models/lab.model';
 import {ICommunityBuild} from 'projects/shared-models/community-build.model';
 import {IUserRolesUser} from 'projects/shared-models/user_roles_user.model';
 import {ISpeakerResource} from 'projects/shared-models/speaker_resource.model';
+import { ActivatedRoute,Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-contributions',
@@ -14,7 +14,7 @@ import {ISpeakerResource} from 'projects/shared-models/speaker_resource.model';
 })
 export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
-  @Input() user: IUser;
+  username = "";
 
   labs: ILab[];
   communities: IUserRolesUser[];
@@ -28,16 +28,20 @@ export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewC
 
   constructor(
     private appUsersService: AppUsersService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
   }
 
   ngOnInit(): void {
+    //Get username from router link
+    this.username = this.router.url.split('/')[2];
     // Get the user's past events
-    this.subscriptions.push(this.appUsersService.speakerResources(this.user.username).subscribe(value => {
+    this.subscriptions.push(this.appUsersService.speakerResources(this.username).subscribe(value => {
       this.pastEvents = value.speaker_resources;
     }));
     // Get the user's communities
-    this.subscriptions.push(this.appUsersService.communities(this.user.username).subscribe(value => {
+    this.subscriptions.push(this.appUsersService.communities(this.username).subscribe(value => {
       this.communities = value.user_roles_users;
       // TODO: If some community is undefined then remove it, is it required?
       this.communities.forEach(community => {
@@ -47,11 +51,11 @@ export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewC
       })
     }));
     // Get the user's labs
-    this.subscriptions.push(this.appUsersService.labs(this.user.username).subscribe(value => {
+    this.subscriptions.push(this.appUsersService.labs(this.username).subscribe(value => {
       this.labs = value.labs;
     }));
     // Get the user's builds
-    this.subscriptions.push(this.appUsersService.communityBuilds(this.user.username).subscribe(value => {
+    this.subscriptions.push(this.appUsersService.communityBuilds(this.username).subscribe(value => {
       this.builds = value.community_builds;
     }));
   }
