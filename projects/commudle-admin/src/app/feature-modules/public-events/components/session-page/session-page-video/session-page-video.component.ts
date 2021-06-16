4c195dc0daf024067d548ab0cf45af6950e6683e
Ashwin <bsashwin9@gmail.com>
2021-06-16 07:36:58 +0530

    Bug fixes and CSS improvements in session page

--- projects/commudle-admin/src/app/feature-modules/public-events/components/session-page/session-page-video/session-page-video.component.ts
+++ projects/commudle-admin/src/app/feature-modules/public-events/components/session-page/session-page-video/session-page-video.component.ts
@@ -1,18 +1,21 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, ElementRef, Inject, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
 import * as moment from 'moment';
+import { ICommunity } from 'projects/shared-models/community.model';
 import { ICurrentUser } from 'projects/shared-models/current_user.model';
 import { IDiscussion } from 'projects/shared-models/discussion.model';
 import { IEmbeddedVideoStream } from 'projects/shared-models/embedded_video_stream.model';
 import { EUserRoles } from 'projects/shared-models/enums/user_roles.enum';
 import { IEvent } from 'projects/shared-models/event.model';
+import { ITrackSlot } from 'projects/shared-models/track-slot.model';
+import { IUser } from 'projects/shared-models/user.model';
 
 @Component({
   selector: 'app-session-page-video',
   templateUrl: './session-page-video.component.html',
   styleUrls: ['./session-page-video.component.scss']
 })
-export class SessionPageVideoComponent implements OnInit {
+export class SessionPageVideoComponent implements OnInit, AfterViewInit {
 
   @Input() event: IEvent;
   @Input() userRoles: any[];
@@ -24,11 +27,16 @@ export class SessionPageVideoComponent implements OnInit {
   @Input() qna: IDiscussion;
   @Input() pollableId: number;
   @Input() pollableType: string;
+  @Input() community: ICommunity;
+  @Input() trackSlot: ITrackSlot;
+  @Input() speaker: IUser;
+  @Input() userVisitData;
 
   moment = moment;
   EUserRoles = EUserRoles;
 
   isFullScreen = false;
+  compressVideoStream = false;
 
   // For live notifications
   userCount = 0;
@@ -36,14 +44,22 @@ export class SessionPageVideoComponent implements OnInit {
   newQna = false;
   newPoll = false;
 
-  @ViewChildren('window') windows: QueryList<ElementRef>;
+  @ViewChildren('interactionWindow') interactionWindows: QueryList<ElementRef>;
 
   constructor(
-    @Inject(DOCUMENT) private document: any
+    @Inject(DOCUMENT) private document: any,
+    private cdr: ChangeDetectorRef
   ) {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    if (['commudle', 'youtube'].includes(this.embeddedVideoStream.source)) {
+      this.toggleWindow(0);
+      this.cdr.detectChanges();
+    }
   }
 
   toggleFullScreen(): void {
@@ -56,20 +72,25 @@ export class SessionPageVideoComponent implements OnInit {
   }
 
   toggleWindow(windowNum: number): void {
-    this.windows.forEach((item, index) => {
+    this.interactionWindows.forEach((item, index) => {
       item.nativeElement.style.display = windowNum === index ? item.nativeElement.style.display === 'block' ? 'none' : 'block' : 'none';
     });
+    this.compressVideoStream = this.interactionWindows.some(value => value.nativeElement.style.display === 'block');
     switch (windowNum) {
-      case 1:
+      case 2:
         this.newMessage = false;
         break;
-      case 2:
+      case 3:
         this.newQna = false;
         break;
-      case 3:
+      case 4:
         this.newPoll = false;
         break;
     }
+  }
+
+  getStatusOfInteractionWindow(windowNum: number): boolean {
+    return this.interactionWindows.toArray()[windowNum].nativeElement.style.display === 'none';
   }
 
 }
