df3952d596d99ec9c93e3b7400f80a4ecc4f53e3
Ashwin <bsashwin9@gmail.com>
2021-06-17 16:46:53 +0530

    Revamp community about component

--- projects/commudle-admin/src/app/feature-modules/public-community/public-community-routing.module.ts
+++ projects/commudle-admin/src/app/feature-modules/public-community/public-community-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeCommunityComponent } from './components/home-community/home-community.component';
-import { CommunityDetailsResolver } from '../../resolvers/community-details.resolver';
+import { RouterModule, Routes } from '@angular/router';
+import { CommunityDetailsResolver } from 'projects/commudle-admin/src/app/resolvers/community-details.resolver';
 import { AboutComponent } from './components/about/about.component';
-import { EventsComponent } from './components/events/events.component';
-import { EventResourcesComponent } from './components/event-resources/event-resources.component';
-import { MembersComponent } from './components/members/members.component';
 import { CommunityChannelsListComponent } from './components/community-channels-list/community-channels-list.component';
+import { EventsComponent } from './components/events/events.component';
+import { HomeCommunityComponent } from './components/home-community/home-community.component';
+import { MembersComponent } from './components/members/members.component';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 
 
@@ -26,10 +25,10 @@ const routes: Routes = [
         path: 'events',
         component: EventsComponent
       },
-      {
-        path: 'resources',
-        component: EventResourcesComponent
-      },
+      // {
+      //   path: 'resources',
+      //   component: EventResourcesComponent
+      // },
       {
         path: 'members',
         component: MembersComponent
@@ -50,4 +49,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PublicCommunityRoutingModule { }
+export class PublicCommunityRoutingModule {
+}
