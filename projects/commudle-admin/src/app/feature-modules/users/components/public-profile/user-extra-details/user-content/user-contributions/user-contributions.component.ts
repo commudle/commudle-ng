b5a2c35a379b3a4f8f84807ce8690261224e248f
Ashwin <bsashwin9@gmail.com>
2021-06-18 21:29:40 +0530

    Fix profile not updating on route change

--- projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-extra-details/user-content/user-contributions/user-contributions.component.ts
+++ projects/commudle-admin/src/app/feature-modules/users/components/public-profile/user-extra-details/user-content/user-contributions/user-contributions.component.ts
@@ -1,25 +1,25 @@
-import {AfterViewChecked, Component, Input, OnDestroy, OnInit, QueryList, ViewChildren} from '@angular/core';
-import {Subscription} from 'rxjs';
-import {AppUsersService} from 'projects/commudle-admin/src/app/services/app-users.service';
-import {IUser} from 'projects/shared-models/user.model';
-import {ILab} from 'projects/shared-models/lab.model';
-import {ICommunityBuild} from 'projects/shared-models/community-build.model';
-import {IUserRolesUser} from 'projects/shared-models/user_roles_user.model';
-import {ISpeakerResource} from 'projects/shared-models/speaker_resource.model';
+import { AfterViewChecked, Component, Input, OnChanges, OnDestroy, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
+import { AppUsersService } from 'projects/commudle-admin/src/app/services/app-users.service';
+import { ICommunityBuild } from 'projects/shared-models/community-build.model';
+import { ILab } from 'projects/shared-models/lab.model';
+import { ISpeakerResource } from 'projects/shared-models/speaker_resource.model';
+import { IUser } from 'projects/shared-models/user.model';
+import { IUserRolesUser } from 'projects/shared-models/user_roles_user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-contributions',
   templateUrl: './user-contributions.component.html',
   styleUrls: ['./user-contributions.component.scss']
 })
-export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewChecked {
+export class UserContributionsComponent implements OnInit, OnChanges, OnDestroy, AfterViewChecked {
 
   @Input() user: IUser;
 
-  labs: ILab[];
-  communities: IUserRolesUser[];
-  builds: ICommunityBuild[];
-  pastEvents: ISpeakerResource[];
+  labs: ILab[] = [];
+  communities: IUserRolesUser[] = [];
+  builds: ICommunityBuild[] = [];
+  pastEvents: ISpeakerResource[] = [];
 
   subscriptions: Subscription[] = [];
 
@@ -27,33 +27,20 @@ export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewC
   @ViewChildren('navigation') navigations: QueryList<any>;
 
   constructor(
-    private appUsersService: AppUsersService,
+    private appUsersService: AppUsersService
   ) {
   }
 
   ngOnInit(): void {
-    // Get the user's past events
-    this.subscriptions.push(this.appUsersService.speakerResources(this.user.username).subscribe(value => {
-      this.pastEvents = value.speaker_resources;
-    }));
-    // Get the user's communities
-    this.subscriptions.push(this.appUsersService.communities(this.user.username).subscribe(value => {
-      this.communities = value.user_roles_users;
-      // TODO: If some community is undefined then remove it, is it required?
-      this.communities.forEach(community => {
-        if (!community.community) {
-          this.communities.splice(this.communities.indexOf(community), 1);
-        }
-      })
-    }));
-    // Get the user's labs
-    this.subscriptions.push(this.appUsersService.labs(this.user.username).subscribe(value => {
-      this.labs = value.labs;
-    }));
-    // Get the user's builds
-    this.subscriptions.push(this.appUsersService.communityBuilds(this.user.username).subscribe(value => {
-      this.builds = value.community_builds;
-    }));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.user) {
+      this.getPastEvents();
+      this.getCommunities();
+      this.getLabs();
+      this.getBuilds();
+    }
   }
 
   ngOnDestroy(): void {
@@ -66,6 +53,40 @@ export class UserContributionsComponent implements OnInit, OnDestroy, AfterViewC
         this.navigations.toArray()[idx].nativeElement.classList.remove('active');
       }
     });
+  }
+
+  // Get the user's past events
+  getPastEvents(): void {
+    this.subscriptions.push(this.appUsersService.speakerResources(this.user.username).subscribe(value => {
+      this.pastEvents = value.speaker_resources;
+    }));
+  }
+
+  // Get the user's communities
+  getCommunities(): void {
+    this.subscriptions.push(this.appUsersService.communities(this.user.username).subscribe(value => {
+      this.communities = value.user_roles_users;
+      // TODO: If some community is undefined then remove it, is that required?
+      this.communities.forEach(community => {
+        if (!community.community) {
+          this.communities.splice(this.communities.indexOf(community), 1);
+        }
+      });
+    }));
+  }
+
+  // Get the user's labs
+  getLabs(): void {
+    this.subscriptions.push(this.appUsersService.labs(this.user.username).subscribe(value => {
+      this.labs = value.labs;
+    }));
+  }
+
+  // Get the user's builds
+  getBuilds(): void {
+    this.subscriptions.push(this.appUsersService.communityBuilds(this.user.username).subscribe(value => {
+      this.builds = value.community_builds;
+    }));
   }
 
   // Check if the given element is scrollable
