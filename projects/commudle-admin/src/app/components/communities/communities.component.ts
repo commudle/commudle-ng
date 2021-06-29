a75198df6bbba558a76e6d116dfaf8ac23c02519
Ashwin <bsashwin9@gmail.com>
2021-06-29 15:31:43 +0530

    Improve the CSS in communities landing page

--- projects/commudle-admin/src/app/components/communities/communities.component.ts
+++ projects/commudle-admin/src/app/components/communities/communities.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-communities',
@@ -7,89 +8,38 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CommunitiesComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.setMeta();
   }
 
-  // page = 1;
-  // count = 5;
-  // total = 0;
-  // activeCommunities: ICommunity[] = [];
-  // communities: ICommunity[] = [];
-  // isLoading = false;
-  //
-  // searchForm = this.fb.group({
-  //   name: ['']
-  // });
-  //
-  // constructor(
-  //   private communitiesService: CommunitiesService,
-  //   private fb: FormBuilder,
-  //   private title: Title,
-  //   private meta: Meta
-  // ) { }
-  //
-  // ngOnInit() {
-  //   this.setMeta();
-  //   this.getCommunities();
-  //   this.search();
-  // }
-  //
-  //
-  //
-  // getCommunities() {
-  //   this.isLoading = true;
-  //   this.communitiesService.pGetCommunities(this.page, this.count, this.searchForm.get('name').value).subscribe(
-  //     data => {
-  //       this.communities = this.communities.concat(data.communities);
-  //       this.isLoading = false;
-  //       this.page = (+data.page);
-  //       this.total = data.total;
-  //
-  //       if (this.communities.length < this.total) {
-  //         this.page += 1;
-  //         this.getCommunities();
-  //       }
-  //     }
-  //   );
-  // }
-  //
-  //
-  // search() {
-  //   this.searchForm.valueChanges.pipe(
-  //     debounceTime(800),
-  //     switchMap(() => {
-  //       this.page = 1;
-  //       this.isLoading = true;
-  //       return this.communitiesService.pGetCommunities(this.page, this.count, this.searchForm.get('name').value);
-  //     })
-  //   ).subscribe(data => {
-  //     this.communities = data.communities;
-  //     this.isLoading = false;
-  //     this.page = (+data.page) + 1;
-  //     this.total = data.total;
-  //     this.getCommunities();
-  //   });
-  // }
-  //
-  // setMeta() {
-  //   this.title.setTitle('All Communities');
-  //   this.meta.updateTag({ name: 'description', content: `Over 90 Communities and 20,000 Users are using Commudle.`});
-  //
-  //
-  //   this.meta.updateTag({ name: 'og:image', content: 'https://commudle.com/assets/images/commudle-logo192.png' });
-  //   this.meta.updateTag({ name: 'og:image:secure_url', content: 'https://commudle.com/assets/images/commudle-logo192.png' });
-  //   this.meta.updateTag({ name: 'og:title', content: `All Communities` });
-  //   this.meta.updateTag({ name: 'og:description', content: `Over 90 Communities and 20,000 Users are using Commudle.`});
-  //   this.meta.updateTag( { name: 'og:type', content: 'website'});
-  //
-  //   this.meta.updateTag({ name: 'twitter:image', content: 'https://commudle.com/assets/images/commudle-logo192.png' });
-  //   this.meta.updateTag({ name: 'twitter:title', content: `All Communities` });
-  //   this.meta.updateTag({ name: 'twitter:description', content: `Over 90 Communities and 20,000 Users are using Commudle.`});
-  // }
-  //
-  //
-  //
+  setMeta() {
+    this.title.setTitle('All Communities');
+    this.meta.updateTag({ name: 'description', content: `Over 90 Communities and 20,000 Users are using Commudle.` });
+
+    this.meta.updateTag({ name: 'og:image', content: 'https://commudle.com/assets/images/commudle-logo192.png' });
+    this.meta.updateTag({
+      name: 'og:image:secure_url',
+      content: 'https://commudle.com/assets/images/commudle-logo192.png'
+    });
+    this.meta.updateTag({ name: 'og:title', content: `All Communities` });
+    this.meta.updateTag({
+      name: 'og:description',
+      content: `Over 90 Communities and 20,000 Users are using Commudle.`
+    });
+    this.meta.updateTag({ name: 'og:type', content: 'website' });
+
+    this.meta.updateTag({ name: 'twitter:image', content: 'https://commudle.com/assets/images/commudle-logo192.png' });
+    this.meta.updateTag({ name: 'twitter:title', content: `All Communities` });
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: `Over 90 Communities and 20,000 Users are using Commudle.`
+    });
+  }
+
 }
