33afc229ceaf4fb3da3e3a5d848ecb7637e66a4d
Kaushik Sriram <kaushik162000@gmail.com>
2021-07-10 22:35:16 +0530

    fix: remove footer from channels page

--- projects/commudle-admin/src/app/feature-modules/community-channels/components/community-channel/community-channel.component.ts
+++ projects/commudle-admin/src/app/feature-modules/community-channels/components/community-channel/community-channel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DiscussionsService } from 'projects/commudle-admin/src/app/services/discussions.service';
+import { FooterService } from 'projects/commudle-admin/src/app/services/footer.service';
 import { ICommunityChannel } from 'projects/shared-models/community-channel.model';
 import { IDiscussion } from 'projects/shared-models/discussion.model';
 import { CommunityChannelManagerService } from '../../services/community-channel-manager.service';
@@ -26,9 +27,11 @@ export class CommunityChannelComponent implements OnInit, OnDestroy {
     private discussionsService: DiscussionsService,
     private activatedRoute: ActivatedRoute,
     private communityChannelNotificationsChannel: CommunityChannelNotificationsChannel,
+    private footerService: FooterService
   ) {}
 
   ngOnInit() {
+    this.footerService.changeFooterStatus(false);
     this.subscriptions.push(
       this.communityChannelManagerService.communityChannels$.subscribe((data) => {
         if (data && !this.initialized) {
