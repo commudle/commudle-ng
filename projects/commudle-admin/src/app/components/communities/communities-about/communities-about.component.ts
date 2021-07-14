fb88722a1d243269d0304bdb287cc00ad181074f
Ashwin <bsashwin9@gmail.com>
2021-07-14 07:38:37 +0530

    feat: create featured communities and complete community landing page design

--- projects/commudle-admin/src/app/components/communities/communities-about/communities-about.component.ts
+++ projects/commudle-admin/src/app/components/communities/communities-about/communities-about.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-communities-about',
   templateUrl: './communities-about.component.html',
-  styleUrls: ['./communities-about.component.scss']
+  styleUrls: ['./communities-about.component.scss'],
 })
 export class CommunitiesAboutComponent implements OnInit {
-
   constructor() {
+    // do nothing
   }
 
   ngOnInit(): void {
+    // do nothing
   }
 
+  // Function to scroll to communities list section
+  scrollToCommunitiesList(): void {
+    const element = document.getElementById('communities-list');
+    window.scroll({ top: element.offsetTop - 56, behavior: 'smooth' });
+  }
 }
--- projects/commudle-admin/src/app/services/communities.service.ts
+++ projects/commudle-admin/src/app/services/communities.service.ts
@@ -8,33 +8,28 @@ import { ApiRoutesService } from 'projects/shared-services/api-routes.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommunitiesService {
+  constructor(private http: HttpClient, private apiRoutesService: ApiRoutesService) {}
 
-  constructor(
-    private http: HttpClient,
-    private apiRoutesService: ApiRoutesService
-  ) {
-  }
-
-  create(communityData, communityGroupId = null): Observable<ICommunity> {
+  create(communityData: any, communityGroupId = null): Observable<ICommunity> {
     let params;
     if (communityGroupId) {
       params = new HttpParams().set('community_group_id', communityGroupId);
     }
     return this.http.post<ICommunity>(
       this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.CREATE),
-      {community: communityData},
-      {params}
+      { community: communityData },
+      { params },
     );
   }
 
-  getRoleCommunities(role): Observable<ICommunities> {
+  getRoleCommunities(role: string): Observable<ICommunities> {
     const params = new HttpParams().set('role', role);
-    return this.http.get<ICommunities>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.USER_ROLE_COMMUNITIES), {params}
-    );
+    return this.http.get<ICommunities>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.USER_ROLE_COMMUNITIES), {
+      params,
+    });
     // .pipe(
     //   tap((data: ICommunities) => {
     //     this.organizerCommunities.next(data.communities);
@@ -43,63 +38,54 @@ export class CommunitiesService {
   }
 
   // get the details of a community
-  getCommunityDetails(communityId): Observable<ICommunity> {
-    const params = new HttpParams().set('community_id', communityId);
-    return this.http.get<ICommunity>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.DETAILS), {params}
-    );
+  getCommunityDetails(communityId: number): Observable<ICommunity> {
+    const params = new HttpParams().set('community_id', String(communityId));
+    return this.http.get<ICommunity>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.DETAILS), { params });
   }
 
   // update community details
-  updateCommunity(communityFormData, communityId): Observable<ICommunity> {
-    const params = new HttpParams().set('community_id', communityId);
-    params.append('community_id', 'gdg-new-delhi');
-    return this.http.put<ICommunity>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.UPDATE),
-      communityFormData,
-      {params}
-    );
+  updateCommunity(communityFormData: any, communityId: number): Observable<ICommunity> {
+    const params = new HttpParams().set('community_id', String(communityId));
+    return this.http.put<ICommunity>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.UPDATE), communityFormData, {
+      params,
+    });
   }
 
   // search a community by name
-  searchByName(query): Observable<ICommunity[]> {
+  searchByName(query: string): Observable<ICommunity[]> {
     const params = new HttpParams().set('query', query);
-    return this.http.get<ICommunity[]>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SEARCH_BY_NAME), {params}
-    );
+    return this.http.get<ICommunity[]>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SEARCH_BY_NAME), {
+      params,
+    });
   }
 
   // search a community by name
   searchByTag(query: string): Observable<ICommunity[]> {
     const params = new HttpParams().set('query', query);
-    return this.http.get<ICommunity[]>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SEARCH_BY_TAG), {params}
-    );
+    return this.http.get<ICommunity[]>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SEARCH_BY_TAG), {
+      params,
+    });
+  }
+
+  getPopularTags(): Observable<string[]> {
+    return this.http.get<string[]>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.POPULAR_TAGS));
   }
 
   // get all the speakers
-  speakers(communityId): Observable<IUsers> {
-    // communityId is the slug here
-    const params = new HttpParams().set('community_id', communityId);
-    return this.http.get<IUsers>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SPEAKERS), {params}
-    );
+  speakers(communityId: number): Observable<IUsers> {
+    const params = new HttpParams().set('community_id', String(communityId));
+    return this.http.get<IUsers>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.SPEAKERS), { params });
   }
 
   // TODO deprecate once we have the new API's for list of communities
   // Public api communication
-  pGetCommunities(page, count, query): Observable<ICommunities> {
-    const params = new HttpParams().set('page', page).set('count', count).set('query', query);
-    return this.http.get<ICommunities>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.PUBLIC_INDEX), {params}
-    );
+  pGetCommunities(page: number, count: number, query: string): Observable<ICommunities> {
+    const params = new HttpParams().set('page', String(page)).set('count', String(count)).set('query', query);
+    return this.http.get<ICommunities>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.PUBLIC_INDEX), { params });
   }
 
-  pGetCommunityDetails(communityId): Observable<ICommunity> {
-    const params = new HttpParams().set('community_id', communityId);
-    return this.http.get<ICommunity>(
-      this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.PUBLIC_DETAILS), {params}
-    );
+  pGetCommunityDetails(communityId: number): Observable<ICommunity> {
+    const params = new HttpParams().set('community_id', String(communityId));
+    return this.http.get<ICommunity>(this.apiRoutesService.getRoute(API_ROUTES.COMMUNITIES.PUBLIC_DETAILS), { params });
   }
-
 }
