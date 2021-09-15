4434efcadc157f04b8f16ca9ce7c1c8377b83435
Ashwin <bsashwin9@gmail.com>
2021-09-15 15:24:01 +0530

    fix: bug in notifications service

--- projects/shared-services/notifications/tab-title-notifications.service.ts
+++ projects/shared-services/notifications/tab-title-notifications.service.ts
@@ -1,33 +1,42 @@
-import { Injectable, OnDestroy, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TabTitleNotificationsService{
+export class TabTitleNotificationsService {
   private defaultTitle: string;
   private interval: NodeJS.Timeout;
+  private newTitle: string = '';
 
   constructor(private title: Title) {}
 
   blinkTitle(value: string): void {
-    window.onblur = () => this.enableNotification(value);
+    if (document.hasFocus()) {
+      return;
+    }
+
+    this.newTitle = value;
+    window.onblur = () => this.enableNotification();
     window.onfocus = () => this.disableNotification();
   }
 
-  enableNotification(text: string, interval: number = 3000): void {
-    if (!this.interval) {
-      this.defaultTitle = this.title.getTitle();
-      this.interval = setInterval(() => {
-        this.title.setTitle(this.defaultTitle === this.title.getTitle() ? text : this.defaultTitle);
-      }, interval);
+  enableNotification(interval: number = 3000): void {
+    if (this.interval || this.newTitle === '') {
+      return;
     }
+
+    this.defaultTitle = this.title.getTitle();
+    this.interval = setInterval(() => {
+      this.title.setTitle(this.defaultTitle === this.title.getTitle() ? this.newTitle : this.defaultTitle);
+    }, interval);
   }
 
   disableNotification(): void {
     if (this.interval) {
       clearInterval(this.interval);
       this.interval = null;
+      this.newTitle = '';
       this.title.setTitle(this.defaultTitle);
     }
   }
