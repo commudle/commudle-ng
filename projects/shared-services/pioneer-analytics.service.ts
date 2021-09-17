687c81ad0e9de38e3296db9f2c7f33bfcedfddde
Ashwin <bsashwin9@gmail.com>
2021-09-17 19:12:08 +0530

    feat: enable pioneer analytics

---
+++ projects/shared-services/pioneer-analytics.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PioneerAnalyticsService {
+  pioneerAnalytics: any;
+
+  constructor(private router: Router) {
+    // @ts-ignore
+    this.pioneerAnalytics = window.pioneerAnalytics;
+  }
+
+  // Call the active() method whenever the current user performs an action that makes them an active user
+  activeUser(): void {
+    this.pioneerAnalytics.active();
+  }
+
+  startAnalytics(userId: number): void {
+    this.identifyUser(userId);
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.trackPage();
+      }
+    });
+  }
+
+  // Make an identify() call on every page load for logged-in users. identify() tells us who the current user is
+  private identifyUser(userId: number): void {
+    this.pioneerAnalytics.identify(userId);
+  }
+
+  // Correctly tracking page views in single-page applications (SPAs)
+  private trackPage(): void {
+    this.pioneerAnalytics.page();
+  }
+}
