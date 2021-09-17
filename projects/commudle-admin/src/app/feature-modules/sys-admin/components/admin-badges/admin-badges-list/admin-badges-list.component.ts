768a88222462f67f25dac5a8aa3c2269170fe789
Ajay Singh <ajaysinghrr007@gmail.com>
2021-09-17 11:30:40 +0530

    adding edit and delete functionality

--- projects/commudle-admin/src/app/feature-modules/sys-admin/components/admin-badges/admin-badges-list/admin-badges-list.component.ts
+++ projects/commudle-admin/src/app/feature-modules/sys-admin/components/admin-badges/admin-badges-list/admin-badges-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { SysAdminBadgesService } from 'projects/commudle-admin/src/app/feature-modules/sys-admin/services/sys-admin-badges.service';
 import { IBadge } from 'projects/shared-models/badge.model';
+import { NbDialogService } from '@nebular/theme';
+import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
 
 @Component({
   selector: 'app-admin-badges-list',
@@ -12,11 +14,15 @@ export class AdminBadgesListComponent implements OnInit {
   currentBadgeId;
   badges: IBadge[] = [];
   page = 1;
-  count = 5;
+  count = 15;
   total = -1;
 
+  @ViewChild('confirmDeleteBadge') confirmDeleteBadgeDialogue: TemplateRef<any>;
+
   constructor(
-    private sysAdminBadgesService : SysAdminBadgesService
+    private sysAdminBadgesService : SysAdminBadgesService,
+    private dialogService: NbDialogService,
+    private toastLogService: LibToastLogService,
   ) { }
 
   ngOnInit(): void {
@@ -24,14 +30,37 @@ export class AdminBadgesListComponent implements OnInit {
   }
 
   getBadges(): void {
+    console.log(this.total)
+    console.log(this.badges.length)
     if (this.badges.length !== this.total) {
        this.sysAdminBadgesService.getAllBadges(this.page, this.count).subscribe((value) => {
-         console.log(value)
-        // this.badges = this.badges.concat(value.badges);
-        // this.page = +value.page;
-        // this.total = +value.total;
-        // this.page += 1;
+        //console.log(value)
+        this.badges = this.badges.concat(value.badges);
+        this.page = +value.page;
+        this.total = +value.total;
+        this.page += 1;
+        console.log(this.badges)
+        console.log(this.total)
+        console.log(this.badges.length)
       });
+    }
+  }
+
+  confirmDeleteBadgeOpen(badgeId: number): void {
+    this.currentBadgeId = badgeId;
+    this.dialogService.open(this.confirmDeleteBadgeDialogue);
+  }
+
+  deleteBadge(): void {
+    if(this.currentBadgeId){
+      this.sysAdminBadgesService.deleteBadge(this.currentBadgeId).subscribe((data) => {
+        if(data){
+          this.toastLogService.successDialog('Successfully deleted badge!');
+          //console.log(this.badges)
+          this.getBadges();
+          //console.log(this.badges)
+        }
+      })
     }
   }
 
