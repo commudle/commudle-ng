0c1a4ade63489906c7a0a40365ac5ea6648ece42
Ashwin <bsashwin9@gmail.com>
2021-09-21 15:37:37 +0530

    fix: bugs

--- projects/commudle-admin/src/app/feature-modules/labs/components/lab/lab-details/lab-details.component.ts
+++ projects/commudle-admin/src/app/feature-modules/labs/components/lab/lab-details/lab-details.component.ts
@@ -1,14 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ILab} from 'projects/shared-models/lab.model';
-import {IDiscussion} from 'projects/shared-models/discussion.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ILab } from 'projects/shared-models/lab.model';
+import { IDiscussion } from 'projects/shared-models/discussion.model';
 
 @Component({
   selector: 'app-lab-details',
   templateUrl: './lab-details.component.html',
-  styleUrls: ['./lab-details.component.scss']
+  styleUrls: ['./lab-details.component.scss'],
 })
 export class LabDetailsComponent implements OnInit {
-
   @Input() lab: ILab;
   @Input() similarLabs: ILab[];
   @Input() selectedLabStep: number;
@@ -23,17 +22,15 @@ export class LabDetailsComponent implements OnInit {
   @Output() setStep: EventEmitter<number> = new EventEmitter<number>();
   @Output() scrollToChat: EventEmitter<any> = new EventEmitter<any>();
 
-  showContinue:boolean = true;
+  showContinue = true;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSetStep(value: number) {
     this.setStep.emit(value);
-    if( this.lab.lab_steps[value].id === this.lab.last_visited_step_id ){
+    if (this.lab.lab_steps[value]?.id === this.lab.last_visited_step_id) {
       this.showContinue = false;
     }
   }
