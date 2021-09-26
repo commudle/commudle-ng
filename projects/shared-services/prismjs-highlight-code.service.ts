3405e58b3e2bd2b2f40fa3ad6f67bacb63e5483b
Ashwin <bsashwin9@gmail.com>
2021-09-26 18:34:23 +0530

    fix: bugs

--- projects/shared-services/prismjs-highlight-code.service.ts
+++ projects/shared-services/prismjs-highlight-code.service.ts
@@ -11,8 +11,8 @@ import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-sass';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-typescript';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 
 declare var Prism: any;
 
