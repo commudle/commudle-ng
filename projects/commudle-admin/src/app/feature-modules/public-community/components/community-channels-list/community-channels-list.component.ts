55ee1898b69adc2942d75710f5271fba98407928
Ajay Singh <ajaysinghrr007@gmail.com>
2021-10-23 21:00:15 +0530

    feat: displayed channels on public community page and channels page

--- projects/commudle-admin/src/app/feature-modules/public-community/components/community-channels-list/community-channels-list.component.ts
+++ projects/commudle-admin/src/app/feature-modules/public-community/components/community-channels-list/community-channels-list.component.ts
@@ -2,25 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ICommunity } from 'projects/shared-models/community.model';
+import { CommunityChannelsService } from 'projects/commudle-admin/src/app/feature-modules/community-channels/services/community-channels.service';
+import { ICommunityChannel } from 'projects/shared-models/community-channel.model';
 
 @Component({
   selector: 'app-community-channels-list',
   templateUrl: './community-channels-list.component.html',
-  styleUrls: ['./community-channels-list.component.scss']
+  styleUrls: ['./community-channels-list.component.scss'],
 })
 export class CommunityChannelsListComponent implements OnInit {
   community: ICommunity;
+  channels: ICommunityChannel[] = [];
+  communityLogo;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private meta: Meta,
-    private title: Title
-  ) { }
+    private title: Title,
+    private communityChannelsService: CommunityChannelsService,
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.parent.data.subscribe(data => {
+    this.activatedRoute.parent.data.subscribe((data) => {
       this.community = data.community;
+      this.communityLogo = this.community.logo_path;
+      this.getChannels();
       this.setMeta();
+    });
+  }
+
+  getChannels() {
+    this.communityChannelsService.index(this.community.id).subscribe((data) => {
+      this.channels = data.community_channels;
     });
   }
 
@@ -29,5 +42,4 @@ export class CommunityChannelsListComponent implements OnInit {
     this.meta.updateTag({ name: 'og:title', content: `Public Channels | ${this.community.name}` });
     this.meta.updateTag({ name: 'twitter:title', content: `Public Channels | ${this.community.name}` });
   }
-
 }
