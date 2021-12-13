0bcc2bdf0060e70d035b951d34484e1a63b8b77b
Ashwin <bsashwin9@gmail.com>
2021-12-13 18:32:30 +0530

    feat: recommendations for builds

--- projects/commudle-admin/src/app/feature-modules/recommendations/recommendations.module.ts
+++ projects/commudle-admin/src/app/feature-modules/recommendations/recommendations.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NbCardModule } from '@nebular/theme';
+import { RecommendedBuildsComponent } from './components/recommended-builds/recommended-builds.component';
 import { RecommendedLabsComponent } from './components/recommended-labs/recommended-labs.component';
 
 @NgModule({
-  declarations: [RecommendedLabsComponent],
+  declarations: [RecommendedLabsComponent, RecommendedBuildsComponent],
   imports: [CommonModule, NbCardModule, RouterModule],
   exports: [RecommendedLabsComponent],
 })
--- projects/commudle-admin/src/app/feature-modules/recommendations/services/recommendation.service.ts
+++ projects/commudle-admin/src/app/feature-modules/recommendations/services/recommendation.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICommunityBuild } from 'projects/shared-models/community-build.model';
 import { ILab } from 'projects/shared-models/lab.model';
 import { API_ROUTES } from 'projects/shared-services/api-routes.constants';
 import { ApiRoutesService } from 'projects/shared-services/api-routes.service';
@@ -13,5 +14,11 @@ export class RecommendationService {
 
   getRecommendedLabs(): Observable<ILab[]> {
     return this.http.get<ILab[]>(this.apiRoutesService.getRoute(API_ROUTES.RECOMMENDATIONS.LABS));
+  }
+
+  getRecommendedCommunityBuilds(): Observable<ICommunityBuild[]> {
+    return this.http.get<ICommunityBuild[]>(
+      this.apiRoutesService.getRoute(API_ROUTES.RECOMMENDATIONS.COMMUNITY_BUILDS),
+    );
   }
 }
