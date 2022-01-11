ea2399b0ca3d8d607884ee68e932d092f54e0d3b
Ashwin <bsashwin9@gmail.com>
2022-01-11 09:02:37 +0530

    feat: create a central seo service

--- projects/commudle-admin/src/app/components/home/home.component.ts
+++ projects/commudle-admin/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
+import { SeoService } from 'projects/shared-services/seo.service';
 
 @Component({
   selector: 'app-home',
@@ -7,53 +7,13 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private meta: Meta, private title: Title) {}
+  constructor(private seoService: SeoService) {}
 
   ngOnInit() {
-    this.setMeta();
-  }
-
-  setMeta() {
-    this.title.setTitle('Commudle - Connect & Learn With Software Developers');
-
-    this.meta.updateTag({
-      name: 'description',
-      content:
-        'A community platform for software developers to connect over online events, channels, share knowledge & find jobs. Join now to build your "Developer Network".',
-    });
-    this.meta.updateTag({
-      name: 'og:image',
-      content: 'https://commudle.com/assets/images/commudle-logo192.png',
-    });
-    this.meta.updateTag({
-      name: 'og:image:secure_url',
-      content: 'https://commudle.com/assets/images/commudle-logo192.png',
-    });
-    this.meta.updateTag({
-      name: 'og:title',
-      content: 'Commudle - Connect & Learn With Software Developers',
-    });
-    this.meta.updateTag({
-      name: 'og:description',
-      content:
-        'A community platform for software developers to connect over online events, channels, share knowledge & find jobs. Join now to build your "Developer Network".',
-    });
-    this.meta.updateTag({
-      name: 'og:type',
-      content: 'website',
-    });
-    this.meta.updateTag({
-      name: 'twitter:image',
-      content: 'https://commudle.com/assets/images/commudle-logo192.png',
-    });
-    this.meta.updateTag({
-      name: 'twitter:title',
-      content: 'Commudle - Connect & Learn With Software Developers',
-    });
-    this.meta.updateTag({
-      name: 'twitter:description',
-      content:
-        'A community platform for software developers to connect over online events, channels, share knowledge & find jobs. Join now to build your "Developer Network".',
-    });
+    this.seoService.setTags(
+      'Commudle - Connect & Learn With Software Developers',
+      'A community platform for software developers to connect over online events, channels, share knowledge & find jobs. Join now to build your "Developer Network".',
+      'https://commudle.com/assets/images/commudle-logo192.png',
+    );
   }
 }
