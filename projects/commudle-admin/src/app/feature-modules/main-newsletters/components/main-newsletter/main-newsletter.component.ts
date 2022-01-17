a28c720caf0dde61ef59c35004593e2784c4a084
Ashwin <bsashwin9@gmail.com>
2022-01-17 09:11:57 +0530

    feat: migrate public newsletter page to grid system

--- projects/commudle-admin/src/app/feature-modules/main-newsletters/components/main-newsletter/main-newsletter.component.ts
+++ projects/commudle-admin/src/app/feature-modules/main-newsletters/components/main-newsletter/main-newsletter.component.ts
@@ -1,55 +1,51 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { DomSanitizer, Meta, Title } from '@angular/platform-browser';
+import { DomSanitizer, Meta, SafeHtml, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { MainNewslettersService } from 'projects/commudle-admin/src/app/feature-modules/main-newsletters/services/main-newsletters.service';
 import { IMainNewsletter } from 'projects/shared-models/main-newsletter.model';
-import { MainNewslettersService } from '../../services/main-newsletters.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-newsletter',
   templateUrl: './main-newsletter.component.html',
-  styleUrls: ['./main-newsletter.component.scss']
+  styleUrls: ['./main-newsletter.component.scss'],
 })
 export class MainNewsletterComponent implements OnInit, OnDestroy {
-  subscriptions = [];
   mainNewsletter: IMainNewsletter;
-  sanitizedContent;
+  sanitizedContent: SafeHtml;
+
+  subscriptions: Subscription[] = [];
 
   constructor(
     private mainNewslettersService: MainNewslettersService,
     private sanitizer: DomSanitizer,
     private activatedRoute: ActivatedRoute,
     private meta: Meta,
-    private title: Title
-  ) { }
+    private title: Title,
+  ) {}
 
   ngOnInit(): void {
     this.subscriptions.push(
-      this.activatedRoute.params.subscribe(
-        data => {
-          if (data.main_newsletter_id) {
-            this.getMainNewsletter(data.main_newsletter_id);
-          }
+      this.activatedRoute.params.subscribe((data) => {
+        if (data.main_newsletter_id) {
+          this.getMainNewsletter(data.main_newsletter_id);
         }
-      )
-    )
+      }),
+    );
   }
 
   ngOnDestroy() {
-    for (let subs of this.subscriptions) {
-      subs.unsubscribe();
-    }
+    this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
   }
 
   getMainNewsletter(id) {
-    this.mainNewslettersService.show(id).subscribe(
-      data => {
-        this.mainNewsletter = data;
-        let newsletterContent = data.content.replace(/utm_medium=email/g, 'utm_medium=webapp');
-        newsletterContent = newsletterContent.replace(/utm_source=email/g, 'utm_medium=webapp');
-        this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(newsletterContent);
-        this.setMeta();
-      }
-    )
+    this.mainNewslettersService.show(id).subscribe((data) => {
+      this.mainNewsletter = data;
+      let newsletterContent = data.content.replace(/utm_medium=email/g, 'utm_medium=webapp');
+      newsletterContent = newsletterContent.replace(/utm_source=email/g, 'utm_medium=webapp');
+      this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(newsletterContent);
+      this.setMeta();
+    });
   }
 
   setMeta(): void {
@@ -75,5 +71,4 @@ export class MainNewsletterComponent implements OnInit, OnDestroy {
       content: `${this.mainNewsletter.email_subject}`,
     });
   }
-
 }
