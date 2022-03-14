cec2ba4da555678da27b9601b0b7d32e6fa0ade0
Ashwin <bsashwin9@gmail.com>
2022-03-14 10:26:01 +0530

    feat: create boilerplate for homepage testimonial

---
+++ projects/commudle-admin/src/app/feature-modules/homepage/components/homepage-testimonials/homepage-testimonials.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-testimonials',
+  templateUrl: './homepage-testimonials.component.html',
+  styleUrls: ['./homepage-testimonials.component.scss'],
+})
+export class HomepageTestimonialsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
