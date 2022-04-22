9d1acd85ca08d5b13111e60df22f8b19203ffd5c
Ashwin <bsashwin9@gmail.com>
2022-04-22 11:08:46 +0000

    feat: add homepage actions from sanity

--- projects/commudle-admin/src/app/feature-modules/homepage/components/homepage/homepage.component.ts
+++ projects/commudle-admin/src/app/feature-modules/homepage/components/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IHomepageAction } from 'projects/commudle-admin/src/app/feature-modules/homepage/models/homepage-action.model';
 import { SearchStatusService } from 'projects/commudle-admin/src/app/feature-modules/search/services/search-status.service';
+import { CmsService } from 'projects/shared-services/cms.service';
 import { SeoService } from 'projects/shared-services/seo.service';
 
 @Component({
@@ -8,15 +10,29 @@ import { SeoService } from 'projects/shared-services/seo.service';
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit, OnDestroy {
-  constructor(private seoService: SeoService, private searchStatusService: SearchStatusService) {}
+  homepageActions: IHomepageAction[] = [];
+
+  constructor(
+    private seoService: SeoService,
+    private searchStatusService: SearchStatusService,
+    private cmsService: CmsService,
+  ) {}
 
   ngOnInit(): void {
     this.seoService.noIndex(true);
     this.searchStatusService.setSearchStatus(false);
+
+    this.getHomepageActions();
   }
 
   ngOnDestroy(): void {
     this.seoService.noIndex(false);
     this.searchStatusService.setSearchStatus(true);
+  }
+
+  getHomepageActions() {
+    this.cmsService.getDataByType('homepageActions').subscribe((value: IHomepageAction[]) => {
+      this.homepageActions = value;
+    });
   }
 }
