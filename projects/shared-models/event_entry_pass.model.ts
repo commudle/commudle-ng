68eb4f7b0fc3f163ef18a135de4705bf248e032c
Ashwin <bsashwin9@gmail.com>
2022-06-09 07:20:11 +0530

    fix: bugs in entry pass scan page

--- projects/shared-models/event_entry_pass.model.ts
+++ projects/shared-models/event_entry_pass.model.ts
@@ -1,6 +1,9 @@
+import { IUser } from 'projects/shared-models/user.model';
+
 export interface IEventEntryPass {
   id: number;
   unique_code: string;
   attendance: boolean;
   uninvited: boolean;
+  user: IUser;
 }
