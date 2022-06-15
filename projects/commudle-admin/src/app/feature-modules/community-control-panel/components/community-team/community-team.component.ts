bf7d6363814ac8c99ac8faa9dab6c41e35d34dfb
Arshdeep Singh <arsh199820@gmail.com>
2022-06-15 20:55:45 +0530

    fix: depricate

--- projects/commudle-admin/src/app/feature-modules/community-control-panel/components/community-team/community-team.component.ts
+++ projects/commudle-admin/src/app/feature-modules/community-control-panel/components/community-team/community-team.component.ts
@@ -8,7 +8,7 @@ import { UserRolesUsersService } from 'projects/commudle-admin/src/app/services/
 @Component({
   selector: 'app-community-team',
   templateUrl: './community-team.component.html',
-  styleUrls: ['./community-team.component.scss']
+  styleUrls: ['./community-team.component.scss'],
 })
 export class CommunityTeamComponent implements OnInit, OnChanges {
   communityId;
@@ -18,89 +18,65 @@ export class CommunityTeamComponent implements OnInit, OnChanges {
   organizers: IUserRolesUser[] = [];
   eventOrganizers: IUserRolesUser[] = [];
 
-
   userRolesUserForm = this.fb.group({
     email: ['', Validators.required],
-    user_role_name: ['', Validators.required]
+    user_role_name: [''],
   });
 
   constructor(
     private userRolesUsersService: UserRolesUsersService,
     private fb: FormBuilder,
     private toastLogService: LibToastLogService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe((params) => {
       this.communityId = this.activatedRoute.parent.snapshot.params['community_id'];
       this.getRoles();
     });
   }
 
-  ngOnChanges() {
-  }
+  ngOnChanges() {}
 
   getRoles() {
     if (this.communityId) {
-      this.userRolesUsersService.getCommunityUsersByRole(
-        this.communityId, EUserRoles.ORGANIZER).subscribe(
-          data => this.organizers = data.user_roles_users
-        );
+      this.userRolesUsersService
+        .getCommunityUsersByRole(this.communityId, EUserRoles.ORGANIZER)
+        .subscribe((data) => (this.organizers = data.user_roles_users));
 
-      this.userRolesUsersService.getCommunityUsersByRole(
-        this.communityId, EUserRoles.EVENT_ORGANIZER).subscribe(
-          data => this.eventOrganizers = data.user_roles_users
-        );
+      this.userRolesUsersService
+        .getCommunityUsersByRole(this.communityId, EUserRoles.EVENT_ORGANIZER)
+        .subscribe((data) => (this.eventOrganizers = data.user_roles_users));
     }
-
   }
 
-
   resendInvitationMail(userRolesUser) {
-    this.userRolesUsersService.resendInvitation(userRolesUser.id).subscribe(
-      (data) => {
-        this.toastLogService.successDialog("Invite sent again!");
-      }
-    );
+    this.userRolesUsersService.resendInvitation(userRolesUser.id).subscribe((data) => {
+      this.toastLogService.successDialog('Invite sent again!');
+    });
   }
 
   remove(userRolesUser, arrayType, index) {
-    this.userRolesUsersService.removeUserRolesUser(userRolesUser.id).subscribe(
-      (data) => {
+    this.userRolesUsersService.removeUserRolesUser(userRolesUser.id).subscribe((data) => {
+      this[arrayType].splice(index, 1);
 
-        this[arrayType].splice(index, 1);
-
-        this.toastLogService.successDialog("Removed and informed by email!", 3000);
-      }
-    );
+      this.toastLogService.successDialog('Removed and informed by email!', 3000);
+    });
   }
-
 
   createUserRolesUser() {
     let newUserRolesUser = this.userRolesUserForm.value;
 
     newUserRolesUser.parent_type = 'Kommunity';
     newUserRolesUser.parent_id = this.communityId;
-    this.userRolesUsersService.createUserRolesUser(newUserRolesUser).subscribe(
-      data => {
-        switch (data.user_role.name) {
-          case EUserRoles.ORGANIZER: {
-            this.organizers.push(data);
-            break;
-          }
-          case EUserRoles.EVENT_ORGANIZER: {
-            this.eventOrganizers.push(data);
-            break;
-          }
-          default:
-            console.log('no valid role found');
-        }
-        this.userRolesUserForm.reset();
-        this.toastLogService.successDialog("Invitation Sent!");
-      }
-    );
+    // let role = data.user_role.name;
+    this.userRolesUsersService.createUserRolesUser(newUserRolesUser).subscribe((data) => {
+      console.log(data.user_role.name);
+      this.organizers.push(data);
+
+      this.userRolesUserForm.reset();
+      this.toastLogService.successDialog('Invitation Sent!');
+    });
   }
-
-
 }
