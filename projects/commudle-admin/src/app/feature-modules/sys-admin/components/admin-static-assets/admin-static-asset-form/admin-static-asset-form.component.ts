04bd3d872363af9b53d5eb210ff08b8a864be055
Arshdeep Singh <arsh199820@gmail.com>
2022-06-15 18:47:09 +0530

    build: asset upload

--- projects/commudle-admin/src/app/feature-modules/sys-admin/components/admin-static-assets/admin-static-asset-form/admin-static-asset-form.component.ts
+++ projects/commudle-admin/src/app/feature-modules/sys-admin/components/admin-static-assets/admin-static-asset-form/admin-static-asset-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminStaticAssetsService } from '../../../services/admin-static-assets.service';
 import { IAttachedFile } from 'projects/shared-models/attached-file.model';
-import { assets } from 'projects/shared-models/assets.model';
+import { static_assets } from 'projects/shared-models/assets.model';
 import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
 import { Subscription } from 'rxjs';
 
@@ -13,15 +13,17 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./admin-static-asset-form.component.scss'],
 })
 export class AdminStaticAssetFormComponent implements OnInit {
-  asset: assets;
+  asset: static_assets;
   assetForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     asset: ['', Validators.required],
   });
-  uploadFile: IAttachedFile;
-  fileUploaded = false;
+  uploadedImage: IAttachedFile;
+  imageUploaded = false;
   subscriptions: Subscription[] = [];
-  fileSrc: string;
+  imageSrc: string;
+
+  @ViewChild('inputImage') inputImage: ElementRef;
 
   constructor(
     private adminStaticAssetsService: AdminStaticAssetsService,
@@ -31,7 +33,71 @@ export class AdminStaticAssetFormComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(' works ');
+    this.subscriptions.push(
+      this.activatedRoute.queryParams.subscribe((params) => {
+        this.getAssets(+params.AssetId || 0);
+      }),
+    );
+  }
+
+  getAssets(AssetId: number): void {
+    if (AssetId >= 0) {
+      this.subscriptions.push(
+        this.adminStaticAssetsService.getAssetById(AssetId).subscribe((value) => {
+          // console.log(value, 'prefile works under value');
+          this.asset = value;
+          this.prefillAsset();
+        }),
+      );
+    }
+  }
+
+  prefillAsset(): void {
+    // console.log(' prefill works');
+    // console.log(this.asset.name);
+
+    this.assetForm.patchValue({
+      name: this.asset.name,
+    });
+    // console.log(this.asset.name);
+
+    if (this.asset.file) {
+      this.uploadedImage = this.asset.file;
+      this.imageSrc = this.asset.file.url;
+      this.imageUploaded = true;
+    }
+  }
+
+  addImage(event: Event): void {
+    const fileList: FileList = (event.target as HTMLInputElement).files;
+    const inputImage: File = fileList.length ? fileList[0] : null;
+
+    if (inputImage?.size > 2425190) {
+      this.libToastLogService.warningDialog('Files should be less than 2MB', 3000);
+    } else {
+      if (inputImage) {
+        const iAttachedFile: IAttachedFile = {
+          id: null,
+          file: inputImage,
+          url: null,
+          name: null,
+          type: null,
+        };
+
+        this.uploadedImage = iAttachedFile;
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.imageSrc = reader.result as string;
+          // console.log(this.imageSrc, 'onload');
+        };
+        reader.readAsDataURL(inputImage);
+        this.imageUploaded = true;
+        // console.log(this.imageUploaded, 'readAsDataURL');
+      }
+    }
+  }
 
   submitForm(): void {
     this.createAsset();
@@ -39,9 +105,10 @@ export class AdminStaticAssetFormComponent implements OnInit {
   }
 
   createAsset(): void {
+    console.log('create works');
     this.subscriptions.push(
       this.adminStaticAssetsService.createAsset(this.buildFormData()).subscribe(() => {
-        this.router.navigate(['/sys-admin', 'static-asset']).then(
+        this.router.navigate(['/sys-admin', 'static-assets']).then(
           () => {
             this.libToastLogService.successDialog('Asset Upload Successfully!');
           },
@@ -50,20 +117,26 @@ export class AdminStaticAssetFormComponent implements OnInit {
       }),
     );
   }
-  buildFormData(): FormData {
-    const formData: FormData = new FormData();
-    const AssetFormValue = this.assetForm.value;
 
-    Object.keys(AssetFormValue).forEach((key) => {
-      if (AssetFormValue[key] != null) {
-        formData.append(`asset[${key}]`, AssetFormValue[key]);
+  buildFormData(): FormData {
+    console.log('build works');
+
+    const formData: FormData = new FormData();
+    const assetFormValue = this.assetForm.value;
+
+    Object.keys(assetFormValue).forEach((key) => {
+      // console.log(assetFormValue);
+
+      if (assetFormValue[key] != null) {
+        formData.append(`static_asset[${key}]`, assetFormValue[key]);
+        // console.log(assetFormValue);
       }
     });
 
-    Object.keys(this.uploadFile).forEach((value) => {
-      formData.append(`asset[file][${value}]`, this.uploadFile[value]);
+    Object.keys(this.uploadedImage).forEach((value) => {
+      console.log(value);
+      formData.append(`static_asset[file][${value}]`, this.uploadedImage[value]);
     });
-
     return formData;
   }
 }
--- projects/commudle-admin/src/app/feature-modules/sys-admin/services/admin-static-assets.service.ts
+++ projects/commudle-admin/src/app/feature-modules/sys-admin/services/admin-static-assets.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { assets } from 'projects/shared-models/assets.model';
+import { static_assets } from 'projects/shared-models/assets.model';
 import { API_ROUTES } from 'projects/shared-services/api-routes.constants';
 import { ApiRoutesService } from 'projects/shared-services/api-routes.service';
 import { Observable } from 'rxjs';
@@ -17,5 +18,10 @@ export class AdminStaticAssetsService {
   }
   createAsset(formData): Observable<assets> {
     return this.http.post<assets>(this.apiRoutesService.getRoute(API_ROUTES.STATIC_ASSETS.CREATE), formData);
+  }
+  getAssetById(AssetId: number): Observable<static_assets> {
+    const params = new HttpParams().set('asset_id', String(AssetId));
+
+    return this.http.get<static_assets>(this.apiRoutesService.getRoute(API_ROUTES.STATIC_ASSETS.SHOW), { params });
   }
 }
