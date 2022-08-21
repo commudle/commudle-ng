2e805893af2fe0d6c36346c26e4fd5cda911e73e
arshdeepgrover <arsh199820@gmail.com>
2022-08-22 01:34:26 +0530

    feat: add share button in blogs

--- projects/commudle-admin/src/app/feature-modules/public-blogs/components/blogs-list/blogs-list.component.ts
+++ projects/commudle-admin/src/app/feature-modules/public-blogs/components/blogs-list/blogs-list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { IBlog } from 'projects/commudle-admin/src/app/feature-modules/public-blogs/models/blogs.model';
 import { CmsService } from 'projects/shared-services/cms.service';
 import { SeoService } from 'projects/shared-services/seo.service';
+import { environment } from 'projects/commudle-admin/src/environments/environment';
+import { NavigatorShareService } from 'projects/shared-services/navigator-share.service';
+import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-blogs',
@@ -9,12 +13,20 @@ import { SeoService } from 'projects/shared-services/seo.service';
   styleUrls: ['./blogs-list.component.scss'],
 })
 export class BlogsListComponent implements OnInit {
-  constructor(private cmsService: CmsService, private seoService: SeoService) {}
+  constructor(
+    private cmsService: CmsService,
+    private seoService: SeoService,
+    private navigatorShareService: NavigatorShareService,
+    private libToastLogService: LibToastLogService,
+    private clipboard: Clipboard,
+  ) {}
 
   blogs: IBlog;
   richText: any;
 
   isLoading = false;
+
+  environment = environment;
 
   imageUrl(source: any) {
     return this.cmsService.getImageUrl(source);
@@ -39,5 +51,23 @@ export class BlogsListComponent implements OnInit {
       'Blogs in the form of experiences and knowledge, authored by Developers, Designers, Community Managers and DevRels',
       'https://commudle.com/assets/images/commudle-logo192.png',
     );
+  }
+
+  copyTextToClipboard(title, slug): void {
+    if (!this.navigatorShareService.canShare()) {
+      if (this.clipboard.copy(`${environment.app_url}/blogs/${slug}`)) {
+        this.libToastLogService.successDialog('Copied the message successfully!');
+      }
+      return;
+    }
+    this.navigatorShareService
+      .share({
+        title: `${title}`,
+        text: `${title}`,
+        url: `${environment.app_url}/blogs/${slug}`,
+      })
+      .then(() => {
+        this.libToastLogService.successDialog('Shared successfully!');
+      });
   }
 }
