0b1e4098d0d0238055adcba684e7c6ff8eb10123
arshdeepgrover <arsh199820@gmail.com>
2022-09-13 18:03:36 +0530

    refactor: adding custom variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        /* Adding custom colors to the tailwind config. by www.color-name.com */
         'Spiro-Disco': '#1ac7e340',
         'Brilliant-Azure': '#2aa5ff',
         'Caribbean-Green': '#00d68f',
-        gunmetal: '#2f2e41',
+        'gunmetal': '#2f2e41',
         'Silver-Sand': '#c4c4c4',
+        'Azureish-White': '#dae0ff',
+        'Very-Light-Blue': '#536eff',
+        'Sonic-Silver': '#777777'
       },
       lineClamp: {
         14: '14',
@@ -24,9 +28,15 @@ module.exports = {
         'span-22': 'span 22 / span 22',
       },
       spacing: {
+       /* Adding custom spacing to the tailwind config. */
         '5px': '5px',
+        '6px': '6px',
         '20px': '20px',
+        '30px': '30px',
         '72px': '72px',
+        '140px': '140px',
+        '152px': '152px',
+        '156px': '156px',
         '212px': '212px',
         '260px': '260px',
         '296px': '296px',
@@ -36,7 +46,13 @@ module.exports = {
         '95vw': '95vw',
       },
       borderWidth: {
+        /* Setting the default border width to 1px. */
         DEFAULT: '1px',
+      },
+      lineHeight: {
+        /* Adding custom line height to the tailwind config. */
+        '14px': '14px',
+        '30px': '30px',
       },
     },
     container: {
