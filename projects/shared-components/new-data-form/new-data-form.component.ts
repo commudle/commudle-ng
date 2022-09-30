fdcb65c16823919c434c39a2fb10b11ffae9d1e6
arshdeepgrover <arsh199820@gmail.com>
2022-09-30 17:01:35 +0530

    fix: drag drop css in create form

--- projects/shared-components/new-data-form/new-data-form.component.ts
+++ projects/shared-components/new-data-form/new-data-form.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { DataFormsService } from 'projects/commudle-admin/src/app/services/data_forms.service';
 import { IDataForm } from 'projects/shared-models/data_form.model';
-import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, Validators, FormArray, FormGroup, Form } from '@angular/forms';
 import { IQuestionType } from 'projects/shared-models/question_type.model';
-import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
-import { Title } from '@angular/platform-browser';
 import { QuestionTypesService } from 'projects/shared-components/services/question-types.service';
-import { NbWindowRef } from '@nebular/theme';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 enum EFormPurposes {
@@ -100,8 +95,8 @@ export class NewDataFormComponent implements OnInit {
     });
   }
 
-  addQuestionButtonClick() {
-    (this.createDataForm.get('data_form').get('questions') as FormArray).push(this.initQuestion());
+  addQuestionButtonClick(index: number) {
+    (this.createDataForm.get('data_form').get('questions') as FormArray).insert(index, this.initQuestion());
     this.updateQuestionsCount();
   }
 
@@ -189,7 +184,7 @@ export class NewDataFormComponent implements OnInit {
 
     if (this.minQuestionCount && this.minQuestionCount > 0) {
       for (let i = 0; i < this.minQuestionCount; i++) {
-        this.addQuestionButtonClick();
+        this.addQuestionButtonClick(i);
       }
     }
   }
