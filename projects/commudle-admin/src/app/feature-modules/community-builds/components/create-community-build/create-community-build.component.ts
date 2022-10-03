569240358b31495076a5536833ae412a5fff469f
arshdeepgrover <arsh199820@gmail.com>
2022-10-03 14:11:11 +0530

    refactor: move tinyMCE to ts

--- projects/commudle-admin/src/app/feature-modules/community-builds/components/create-community-build/create-community-build.component.ts
+++ projects/commudle-admin/src/app/feature-modules/community-builds/components/create-community-build/create-community-build.component.ts
@@ -51,6 +51,18 @@ export class CreateCommunityBuildComponent implements OnInit, OnDestroy {
     team: this.fb.array([]),
   });
 
+  tinyMCE = {
+    height: 500,
+    menubar: false,
+    convert_urls: false,
+    plugins:
+      'advlist lists autolink link charmap preview anchor visualblocks code table charmap insertdatetime table paste code help wordcount autoresize',
+    toolbar:
+      'formatselect | bold italic backcolor | link | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | charmap | removeformat | help',
+    default_link_target: '_blank',
+    branding: false,
+  };
+
   subscriptions: Subscription[] = [];
 
   constructor(
