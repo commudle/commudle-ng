1dbf336d14166e7c69c012eba71f866147325b97
arshdeepgrover <arsh199820@gmail.com>
2022-10-08 13:32:48 +0530

    style: add styling

--- projects/commudle-admin/src/app/feature-modules/jobs/components/jobs-lists/jobs-lists.component.ts
+++ projects/commudle-admin/src/app/feature-modules/jobs/components/jobs-lists/jobs-lists.component.ts
@@ -11,14 +11,24 @@ import { LabelType, Options } from '@angular-slider/ngx-slider';
   styleUrls: ['./jobs-lists.component.scss'],
 })
 export class JobsListsComponent implements OnInit {
-  minSalary: number = 10000;
-  maxSalary: number = 40000;
-  options: Options = {
+  minSalary: number = 20000;
+  maxSalary: number = 60000;
+  salary_options: Options = {
     floor: 0,
     ceil: 100000,
     step: 10000,
     noSwitching: true,
   };
+  // experience: number = 2;
+  // exp_options: Options = {
+  //   floor: 0,
+  //   ceil: 10,
+  //   step: 1,
+  // };
+
+  page = 1;
+  count = 10;
+
   Jobs: IJob[] = [];
 
   subscriptions: Subscription[] = [];
@@ -31,6 +41,9 @@ export class JobsListsComponent implements OnInit {
   onValueChange(value) {
     this.minSalary = value;
   }
+  onExpChange(value) {
+    console.log(value);
+  }
 
   onHighValueChange(value) {
     this.maxSalary = value;
@@ -38,12 +51,22 @@ export class JobsListsComponent implements OnInit {
 
   getJobsList() {
     this.subscriptions.push(
-      this.jobsService.getJobs(1, 10).subscribe((data) => {
+      this.jobsService.getJobs(this.page, this.count).subscribe((data) => {
         this.Jobs = data.jobs;
       }),
     );
   }
+
   formSubmit(form) {
+    if (form.value.category == null) {
+      form.value.category = '';
+    }
+    if (form.value.location_type == null) {
+      form.value.location_type = '';
+    }
+    if (form.value.job_type == null) {
+      form.value.job_type = '';
+    }
     this.subscriptions.push(
       this.jobsService
         .getJobsByFilter(1, 10, form.value.category, form.value.location_type, form.value.job_type)
@@ -51,12 +74,13 @@ export class JobsListsComponent implements OnInit {
           this.Jobs = data.jobs;
         }),
     );
-  }
-  freset(form) {
     form.reset();
-    form.value.category = ' ';
-    form.value.job_type = ' ';
-    form.value.location_type = ' ';
+  }
+
+  filterReset(form) {
+    form.reset();
+    this.minSalary = 20000;
+    this.maxSalary = 60000;
     this.getJobsList();
   }
 }
