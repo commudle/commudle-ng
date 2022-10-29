716dbf792834ca53f7baa5db8e79444801ed0884
arshdeepgrover <arsh199820@gmail.com>
2022-10-29 11:08:20 +0530

    refactor: add meta description in communityGroup

--- projects/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-home/community-group-home.component.ts
+++ projects/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-home/community-group-home.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ICommunityGroup } from 'projects/shared-models/community-group.model';
 import { CommunityGroupsService } from 'projects/commudle-admin/src/app/services/community-groups.service';
 import { FooterService } from 'projects/commudle-admin/src/app/services/footer.service';
+import { SeoService } from 'projects/shared-services/seo.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-community-group-home',
   templateUrl: './community-group-home.component.html',
-  styleUrls: ['./community-group-home.component.scss']
+  styleUrls: ['./community-group-home.component.scss'],
 })
 export class CommunityGroupHomeComponent implements OnInit, OnDestroy {
   communityGroup: ICommunityGroup;
@@ -16,15 +18,17 @@ export class CommunityGroupHomeComponent implements OnInit, OnDestroy {
   constructor(
     private activatedRoute: ActivatedRoute,
     private footerService: FooterService,
-  ) { }
+    private seoService: SeoService,
+  ) {}
 
   ngOnInit() {
     this.footerService.changeFooterStatus(false);
-    this.subscriptions.push(this.activatedRoute.data.subscribe(
-      data => {
+    this.subscriptions.push(
+      this.activatedRoute.data.subscribe((data) => {
         this.communityGroup = data.community_group;
-      }
-    ));
+        this.setMeta();
+      }),
+    );
   }
 
   ngOnDestroy() {
@@ -35,8 +39,11 @@ export class CommunityGroupHomeComponent implements OnInit, OnDestroy {
     }
   }
 
-
-
-
-
+  setMeta(): void {
+    this.seoService.setTags(
+      `${this.communityGroup.name} - commudle`,
+      this.communityGroup.description.replace(/<[^>]*>/g, ''),
+      this.communityGroup.logo.url,
+    );
+  }
 }
