fbf243afcc9c852d0891204645082bac8fe64307
Arshdeep Singh <arsh199820@gmail.com>
2022-12-09 13:20:31 +0530

    fix: share button URL (communities)

--- projects/commudle-admin/src/app/feature-modules/homepage/components/homepage-featured-communities/homepage-featured-communities.component.ts
+++ projects/commudle-admin/src/app/feature-modules/homepage/components/homepage-featured-communities/homepage-featured-communities.component.ts
@@ -1,11 +1,8 @@
-import { Clipboard } from '@angular/cdk/clipboard';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 // import getImagePalette from 'image-palette-core';
 import { FeaturedCommunitiesService } from 'projects/commudle-admin/src/app/services/featured-communities.service';
 import { environment } from 'projects/commudle-admin/src/environments/environment';
 import { IFeaturedCommunity } from 'projects/shared-models/featured-community.model';
-import { LibToastLogService } from 'projects/shared-services/lib-toastlog.service';
-import { NavigatorShareService } from 'projects/shared-services/navigator-share.service';
 
 @Component({
   selector: 'app-homepage-featured-communities',
@@ -15,12 +12,10 @@ import { NavigatorShareService } from 'projects/shared-services/navigator-share.
 })
 export class HomepageFeaturedCommunitiesComponent implements OnInit {
   featuredCommunities: IFeaturedCommunity[] = [];
+  environment = environment;
 
   constructor(
     private featuredCommunitiesService: FeaturedCommunitiesService,
-    private navigatorShareService: NavigatorShareService,
-    private libToastLogService: LibToastLogService,
-    private clipboard: Clipboard,
     private changeDetectorRef: ChangeDetectorRef,
   ) {}
 
@@ -42,28 +37,5 @@ export class HomepageFeaturedCommunitiesComponent implements OnInit {
     //   img.src = community.kommunity.logo_image.url;
     //   img.onload = () => (community.palette = getImagePalette(img));
     // });
-  }
-
-  copyTextToClipboard(featuredCommunity: IFeaturedCommunity): void {
-    if (!this.navigatorShareService.canShare()) {
-      if (
-        this.clipboard.copy(
-          `${featuredCommunity.kommunity.name} | ${featuredCommunity.reason} | ${environment.app_url}/communities/${featuredCommunity.kommunity.slug}`,
-        )
-      ) {
-        this.libToastLogService.successDialog('Copied the message successfully!');
-      }
-      return;
-    }
-
-    this.navigatorShareService
-      .share({
-        title: `${featuredCommunity.kommunity.name}`,
-        text: `${featuredCommunity.reason}`,
-        url: `${environment.app_url}/communities/${featuredCommunity.kommunity.slug}`,
-      })
-      .then(() => {
-        this.libToastLogService.successDialog('Shared successfully!');
-      });
   }
 }
