5c1dba4343105131084139392a9056692679c34b
Arshdeep Singh <arsh199820@gmail.com>
2022-12-23 12:35:58 +0530

    refactor: changes

--- apps/commudle-admin/src/app/feature-modules/jobs/components/job-list/job-list.component.ts
+++ apps/commudle-admin/src/app/feature-modules/jobs/components/job-list/job-list.component.ts
@@ -34,6 +34,7 @@ export class JobListComponent implements OnInit, OnDestroy {
   selectedFormValue;
   UrlParamsFromFilters = {};
   heading = '';
+  total: number;
 
   filterForm;
 
@@ -76,16 +77,10 @@ export class JobListComponent implements OnInit, OnDestroy {
       location_type: [''],
       job_type: [''],
       status: [''],
-      experience: this.fb.group({
-        min: [''],
-        max: [''],
-      }),
+      experience: [''],
       min_experience: [''],
       max_experience: [''],
-      salary_range: this.fb.group({
-        min: [''],
-        max: [''],
-      }),
+      salary_range: [''],
       min_salary: [''],
       max_salary: [''],
       tags: [],
@@ -195,6 +190,7 @@ export class JobListComponent implements OnInit, OnDestroy {
             this.jobs = [];
           }
           this.jobs = this.jobs.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
+          this.total = data.total;
           this.page_info = data.page_info;
           this.isLoading = false;
           this.isFilterLoading = false;
