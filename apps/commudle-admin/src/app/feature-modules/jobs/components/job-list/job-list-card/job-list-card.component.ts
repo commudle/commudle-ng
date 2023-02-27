4f30850657e0e50ed38e4cc082343a3cf46f4188
Prerna <rprerna2000@gmail.com>
2023-02-27 12:42:47 +0530

    refactor: added two components

--- apps/commudle-admin/src/app/feature-modules/jobs/components/job-list/job-list-card/job-list-card.component.ts
+++ apps/commudle-admin/src/app/feature-modules/jobs/components/job-list/job-list-card/job-list-card.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
 import { ICurrentUser } from 'apps/shared-models/current_user.model';
-import { EJobStatus, IJob } from 'apps/shared-models/job.model';
+import { EJobLocationType, EJobStatus, IJob } from 'apps/shared-models/job.model';
 import { IUserResume } from 'apps/shared-models/user_resume.model';
 import { LibAuthwatchService } from 'apps/shared-services/lib-authwatch.service';
 import { Subscription } from 'rxjs';
@@ -11,7 +11,20 @@ import {
   faIdCard,
   faClock,
   faSuitcase,
+  faPencil,
+  faShareAlt,
+  faUsers,
+  faInfoCircle,
+  faEyeSlash,
+  faEye,
+  faTrash,
 } from '@fortawesome/free-solid-svg-icons';
+import { NbDialogService, NbToastrService } from '@commudle/theme';
+import { NavigatorShareService } from 'apps/shared-services/navigator-share.service';
+import { environment } from 'apps/commudle-admin/src/environments/environment';
+import { JobService } from 'apps/commudle-admin/src/app/services/job.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { IUser } from 'apps/shared-models/user.model';
 
 @Component({
   selector: 'app-job-list-card',
@@ -20,10 +33,22 @@ import {
 })
 export class JobListCardComponent implements OnInit {
   @Input() job: IJob;
+  @Input() applyButton = true;
+  @Input() applicationsCount = true;
+  @Input() userProfile = true;
+  @Input() user: IUser;
+  @Input() showAdminButtons = false;
+
+  @Output() updateJob: EventEmitter<any> = new EventEmitter<any>();
+  @Output() deleteJob: EventEmitter<any> = new EventEmitter<any>();
+  @Output() toggleJobStatus: EventEmitter<any> = new EventEmitter<any>();
 
   currentUser: ICurrentUser;
   userResumes: IUserResume[] = [];
   subscriptions: Subscription[] = [];
+  jobLink: string;
+
+  EJobLocationType = EJobLocationType;
 
   jobStatus = EJobStatus;
 
@@ -34,14 +59,68 @@ export class JobListCardComponent implements OnInit {
   faIdCard = faIdCard;
   faClock = faClock;
   faSuitcase = faSuitcase;
+  faPencil = faPencil;
+  faShareAlt = faShareAlt;
+  faUsers = faUsers;
+  faInfoCircle = faInfoCircle;
+  faEyeSlash = faEyeSlash;
+  faEye = faEye;
+  faTrash = faTrash;
 
-  constructor(private authWatchService: LibAuthwatchService) {}
+  constructor(
+    private authWatchService: LibAuthwatchService,
+    private nbToastrService: NbToastrService,
+    private nbDialogService: NbDialogService,
+    private navigatorShareService: NavigatorShareService,
+    private clipboard: Clipboard,
+    private jobService: JobService,
+  ) {}
 
   ngOnInit(): void {
     this.subscriptions.push(this.authWatchService.currentUser$.subscribe((data) => (this.currentUser = data)));
+    this.jobLink = `${environment.app_url}/jobs/${this.job.id}`;
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  copyTextToClipboard(id): void {
+    if (!this.navigatorShareService.canShare()) {
+      if (this.clipboard.copy(this.jobLink)) {
+        this.nbToastrService.success('Copied job link to clipboard!', 'Success');
+      }
+      return;
+    }
+
+    this.navigatorShareService
+      .share({ title: 'Hey, check out this job!', url: this.jobLink })
+      .then(() => this.nbToastrService.success('Shared job link!', 'Success'));
+  }
+
+  updateJobStatus(): void {
+    this.subscriptions.push(
+      this.jobService.toggleStatus(this.job.id).subscribe((data: IJob) => {
+        if (data) {
+          this.nbToastrService.success('Job updated successfully', 'Success');
+          this.job = data;
+        }
+      }),
+    );
+  }
+
+  onDeleteJob(): void {
+    this.subscriptions.push(
+      this.jobService.deleteJob(this.job.id).subscribe((value) => {
+        if (value) {
+          this.nbToastrService.success('Job deleted successfully', 'Success');
+          this.deleteJob.emit(this.job.id);
+        }
+      }),
+    );
+  }
+
+  onDialogOpen(templateRef: TemplateRef<any>) {
+    this.nbDialogService.open(templateRef, { closeOnEsc: false, closeOnBackdropClick: false });
   }
 }
