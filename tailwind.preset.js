0801b83a3ae2ceda93d7393e502234a839a43742
Prerna <rprerna2000@gmail.com>
2023-03-21 09:46:16 +0530

    style: event-page

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -36,6 +36,8 @@ module.exports = {
         'Black-Coral': '#595867',
         AuroMetalSaurus: '#667085',
         Charcoal: '#344054',
+        'Anti-Flash-White': '#f2f2f2',
+        'Yankees-Blue': '##222B45',
       },
 
       lineClamp: {
