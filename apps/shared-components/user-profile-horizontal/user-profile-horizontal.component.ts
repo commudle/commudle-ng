b6381713aa218ef5cc1b311cb0cf60ca300f4c22
Prerna <rprerna2000@gmail.com>
2023-03-24 11:23:25 +0530

    style: ux-page

--- apps/shared-components/user-profile-horizontal/user-profile-horizontal.component.ts
+++ apps/shared-components/user-profile-horizontal/user-profile-horizontal.component.ts
@@ -1,29 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {IUser} from 'apps/shared-models/user.model';
-import {UserChatsService} from '../../commudle-admin/src/app/feature-modules/user-chats/services/user-chats.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { IUser } from 'apps/shared-models/user.model';
+import { UserChatsService } from '../../commudle-admin/src/app/feature-modules/user-chats/services/user-chats.service';
 
 @Component({
   selector: 'app-user-profile-horizontal',
   templateUrl: './user-profile-horizontal.component.html',
-  styleUrls: ['./user-profile-horizontal.component.scss']
+  styleUrls: ['./user-profile-horizontal.component.scss'],
 })
 export class UserProfileHorizontalComponent implements OnInit {
   @Input() user: IUser;
   @Input() size: string;
   @Input() aboutMe: boolean;
+  @Input() designation = true;
   @Input() socialMediaLinks: boolean;
   @Input() showLiveStatus: boolean;
 
-  constructor(
-    private userChatsService: UserChatsService
-  ) {
-  }
+  constructor(private userChatsService: UserChatsService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   openChatWithUser() {
     this.userChatsService.changeFollowerId(this.user.id);
   }
-
 }
