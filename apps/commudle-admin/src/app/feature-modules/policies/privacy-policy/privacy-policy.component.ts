224d68a50c86d37318b0835c6054310336548025
ArshdeepGrover <arsh199820@gmail.comssh-keygen -t rsa -C YOUR@EMAIL.com>
2023-03-29 12:27:20 +0530

    feat: fetch T&C and policy  from sanity

--- apps/commudle-admin/src/app/feature-modules/policies/privacy-policy/privacy-policy.component.ts
+++ apps/commudle-admin/src/app/feature-modules/policies/privacy-policy/privacy-policy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SeoService } from 'apps/shared-services/seo.service';
 import { FooterService } from 'apps/commudle-admin/src/app/services/footer.service';
+import { CmsService } from 'apps/shared-services/cms.service';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -8,11 +9,18 @@ import { FooterService } from 'apps/commudle-admin/src/app/services/footer.servi
   styleUrls: ['./privacy-policy.component.scss'],
 })
 export class PrivacyPolicyComponent implements OnInit, OnDestroy {
-  constructor(private seoService: SeoService, private footerService: FooterService) {}
+  privacyPolicy;
+  content;
+  constructor(private seoService: SeoService, private footerService: FooterService, private cmsService: CmsService) {}
 
   ngOnInit() {
     this.footerService.changeFooterStatus(true);
     this.seoService.setTitle('Privacy Policy');
+
+    this.cmsService.getDataBySlug('privacy-policy-for-commudle').subscribe((value) => {
+      this.privacyPolicy = value;
+      this.content = this.cmsService.getHtmlFromBlock(value);
+    });
   }
 
   ngOnDestroy(): void {
