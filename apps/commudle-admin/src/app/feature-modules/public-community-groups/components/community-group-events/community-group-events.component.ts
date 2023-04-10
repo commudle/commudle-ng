c805795f4ef86513be4abf95baef119804e53eae
ArshdeepGrover <arsh199820@gmail.comssh-keygen -t rsa -C YOUR@EMAIL.com>
2023-04-10 20:30:56 +0530

    fix: ui/ux

--- apps/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-events/community-group-events.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-events/community-group-events.component.ts
@@ -16,7 +16,9 @@ export class CommunityGroupEventsComponent implements OnInit {
 
   isLoading = true;
   limit = 6;
-  page_info: IPageInfo;
+  pastPageInfo: IPageInfo;
+  upcomingPageInfo: IPageInfo;
+
   community_group_id: number;
 
   subscriptions: Subscription[] = [];
@@ -27,19 +29,19 @@ export class CommunityGroupEventsComponent implements OnInit {
     this.subscriptions.push(
       this.activatedRoute.parent.params.subscribe((data) => {
         this.community_group_id = data.community_group_id;
-        this.getEvents();
+        this.getPastEvents();
         this.getUpcomingEvents();
       }),
     );
   }
 
-  getEvents() {
+  getPastEvents() {
     this.subscriptions.push(
       this.communityGroupsService
-        .pEvents(this.community_group_id, this.limit, this.page_info?.end_cursor, this.page_info?.end_cursor, 'past')
+        .pEvents(this.community_group_id, this.limit, '', this.pastPageInfo?.end_cursor, 'past')
         .subscribe((data) => {
           this.events = this.events.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
-          this.page_info = data.page_info;
+          this.pastPageInfo = data.page_info;
           this.isLoading = false;
         }),
     );
@@ -47,11 +49,13 @@ export class CommunityGroupEventsComponent implements OnInit {
 
   getUpcomingEvents() {
     this.subscriptions.push(
-      this.communityGroupsService.pEvents(this.community_group_id, this.limit, '', '', 'future').subscribe((data) => {
-        this.UpcomingEvents = this.UpcomingEvents.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
-        this.page_info = data.page_info;
-        // this.isLoading = false;
-      }),
+      this.communityGroupsService
+        .pEvents(this.community_group_id, this.limit, '', this.upcomingPageInfo?.end_cursor, 'future')
+        .subscribe((data) => {
+          this.UpcomingEvents = this.UpcomingEvents.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
+          this.upcomingPageInfo = data.page_info;
+          this.isLoading = false;
+        }),
     );
   }
 }
