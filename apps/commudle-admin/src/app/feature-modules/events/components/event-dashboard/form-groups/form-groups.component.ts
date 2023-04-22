5fffc87beaae526331f831757188a567ea59a035
Prerna <rprerna2000@gmail.com>
2023-04-22 12:27:00 +0530

    style: removed comment

--- apps/commudle-admin/src/app/feature-modules/events/components/event-dashboard/form-groups/form-groups.component.ts
+++ apps/commudle-admin/src/app/feature-modules/events/components/event-dashboard/form-groups/form-groups.component.ts
@@ -63,7 +63,6 @@ export class FormGroupsComponent implements OnInit {
       data_form_entity_group: this.fb.group({
         name: ['', Validators.required],
         registration_type_id: ['', Validators.required],
-        // data_form_id: [0, Validators.required],
         data_form_id: ['', Validators.required],
       }),
     });
