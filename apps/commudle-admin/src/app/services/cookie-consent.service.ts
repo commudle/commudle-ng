34e91ad64354a7a58122d84c3c5a045c8009f8c9
Prerna <rprerna2000@gmail.com>
2023-04-25 18:15:03 +0530

    style: removed comments

--- apps/commudle-admin/src/app/services/cookie-consent.service.ts
+++ apps/commudle-admin/src/app/services/cookie-consent.service.ts
@@ -16,123 +16,20 @@ export class CookieConsentService {
     private gtm: GoogleTagManagerService,
   ) {}
 
-  // acceptCookieConsent() {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     this.cookieService.set(this.cookieConsentKey, this.acceptConsentValue, 60, '/');
-  //   }
-  // }
-
-  // acceptedCookieConsent(cookieValue) {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     this.cookieService.set('com_cookiepref_analytics', cookieValue, 60, '/');
-  //     this.cookieService.set('com_cookiepref_marketing', cookieValue, 60, '/');
-  //     this.cookieService.set(this.cookieConsentKey, this.acceptConsentValue, 60, '/');
-  //     // sessionStorage.setItem('cookie-options', cookieValue);
-  //     this.acceptedSelected = true;
-  //   }
-  // }
-
-  acceptedCookieConsent(analytics, marketing, acceptAll) {
+  acceptedCookieConsent(analytics, marketing) {
     if (isPlatformBrowser(this.platformId)) {
-      // const options = JSON.parse(cookieValue);
       this.cookieService.set('com_cookiepref_analytics', analytics, 60, '/');
       this.cookieService.set('com_cookiepref_marketing', marketing, 60, '/');
-      this.cookieService.set(this.cookieConsentKey, acceptAll, 60, '/');
+      this.cookieService.set(this.cookieConsentKey, this.acceptConsentValue, 60, '/');
       this.gtm.dataLayerPushEvent('consent', {
         analytics: analytics,
         marketing: marketing,
-        acceptAll: acceptAll,
       });
-      console.log(window.dataLayer);
-      // sessionStorage.setItem('cookie-options', cookieValue);
     }
   }
-
-  // acceptedCookieConsent(options) {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     // const options = JSON.parse(cookieValue);
-  //     this.cookieService.set('com_cookiepref_analytics', options.analytics, 60, '/');
-  //     this.cookieService.set('com_cookiepref_marketing', options.marketing, 60, '/');
-  //     this.cookieService.set(this.cookieConsentKey, options.acceptAll, 60, '/');
-  //     this.gtm.dataLayerPushEvent('consent', { cookiepref: options, acceptAll: this.acceptConsentValue });
-  //     console.log(window.dataLayer);
-  //     // sessionStorage.setItem('cookie-options', cookieValue);
-  //     this.acceptedSelected = true;
-  //   }
-  // }
-
-  // acceptedCookieConsent(cookieValue) {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     this.cookieService.set(this.cookieConsentKey, cookieValue, 60, '/');
-  //     sessionStorage.setItem('cookie-options', cookieValue);
-  //     this.acceptedSelected = true;
-  //   }
-  // }
 
   isCookieConsentAccepted(): boolean {
     const consentValue = isPlatformBrowser(this.platformId) ? this.cookieService.get(this.cookieConsentKey) : 'false';
     return consentValue === this.acceptConsentValue;
   }
 }
-
-// if (this.acceptedSelected) {
-//   return true;
-// } else {
-//   const consentValue = isPlatformBrowser(this.platformId) ? this.cookieService.get(this.cookieConsentKey) : 'false';
-//   // const consentValue = this.cookieService.get(this.cookieConsentKey);
-//   return consentValue === this.acceptConsentValue;
-// }
-
-// return consentValue === this.acceptConsentValue ;
-
-// getSessionCookieOptions() {
-//   const options = sessionStorage.getItem('cookie-options');
-//   return options ? JSON.parse(options) : null;
-// }
-
-// acceptCookieConsent(option: string) {
-//   if (isPlatformBrowser(this.platformId)) {
-//     this.cookieService.set(this.cookieConsentKey, option, 60, '/');
-//   }
-// }
-
-// acceptCookieConsent() {
-//   if (isPlatformBrowser(this.platformId)) {
-//     this.cookieService.set(this.cookieConsentKey, option, 60, '/');
-// const preferencesJson = JSON.stringify(preferences);
-// this.cookieService.set(this.cookieConsentKey, preferencesJson, 60, '/');
-// sessionStorage.setItem(this.cookieConsentKey, preferencesJson);
-//   }
-// }
-
-// isCookieConsentAccepted(): boolean {
-//   const consentValue = isPlatformBrowser(this.platformId) ? this.cookieService.get(this.cookieConsentKey) : 'false';
-//   return consentValue === this.acceptConsentValue;
-// }
-
-// }
-
-// cookie-consent-necessary
-
-// acceptCookieConsent(value: string) {
-//   if (isPlatformBrowser(this.platformId)) {
-//     const options = JSON.parse(value);
-//     this.cookieService.set('this.cookieConsentKey', options.necessary, 60, '/');
-//     this.cookieService.set('this.cookieConsentKey', options.analytics, 60, '/');
-//     this.cookieService.set('this.cookieConsentKey', options.marketing, 60, '/');
-//   }
-// }
-
-// isCookieConsentAccepted(option: string): boolean {
-//   const consentValue = isPlatformBrowser(this.platformId)
-//     ? this.cookieService.get(`cookie-consent-${option}`)
-//     : 'false';
-//   return consentValue === 'true';
-// }
-
-// getCookieValue(): string {
-//   if (isPlatformBrowser(this.platformId)) {
-//     return this.cookieService.get(this.cookieConsentKey);
-//   }
-//   return null;
-// }
--- apps/shared-components/cookie-consent/cookie-consent.component.ts
+++ apps/shared-components/cookie-consent/cookie-consent.component.ts
@@ -29,7 +29,7 @@ export class CookieConsentComponent implements OnInit {
   ) {
     this.isBrowser = this.isBrowserService.isBrowser();
     this.preferencesForm = this.fb.group({
-      necessary: [true],
+      necessary: [{ value: true, disabled: true }],
       analytics: [true],
       marketing: [true],
     });
@@ -47,99 +47,15 @@ export class CookieConsentComponent implements OnInit {
     }
   }
 
-  // acceptManagedCookies() {
-  //   const options = {
-  //     necessary: 'true',
-  //     analytics: this.allowAnalytics ? 'true' : 'false',
-  //     marketing: this.allowMarketing ? 'true' : 'false',
-  //   };
-  //   const cookieValue = JSON.stringify(options);
-  //   this.cookieConsentService.acceptedCookieConsent(cookieValue);
-  //   this.cookieConstent = false;
-  // }
-
-  // acceptManagedCookies(type) {
-  //     const options = {
-  //       necessary: 'true',
-  //       analytics: this.preferencesForm.value.analytics ? 'true' : 'false',
-  //       marketing: this.preferencesForm.value.marketing ? 'true' : 'false',
-  //     };
-  //   // const cookieValue = JSON.stringify(options);
-  //   this.cookieConsentService.acceptedCookieConsent(options);
-  //   this.cookieConstent = false;
-  // }
-
-  // acceptManagedCookies() {
-  //   const options = {
-  //     necessary: 'true',
-  //     analytics: this.preferencesForm.value.analytics ? 'true' : 'false',
-  //     marketing: this.preferencesForm.value.marketing ? 'true' : 'false',
-  //     acceptAll: this.preferencesForm.value.marketing && this.preferencesForm.value.analytics ? 'true' : 'false',
-  //   };
-  //   // const cookieValue = JSON.stringify(options);
-  //   this.cookieConsentService.acceptedCookieConsent(options);
-  //   this.cookieConstent = false;
-  // }
-
   acceptManagedCookies() {
-    const analytics = this.preferencesForm.value.analytics ? 'true' : 'false';
-    const marketing = this.preferencesForm.value.marketing ? 'true' : 'false';
-    const acceptAll = this.preferencesForm.value.marketing && this.preferencesForm.value.analytics ? 'true' : 'false';
-
-    this.cookieConsentService.acceptedCookieConsent(analytics, marketing, acceptAll);
+    this.cookieConsentService.acceptedCookieConsent(
+      this.preferencesForm.value.analytics,
+      this.preferencesForm.value.marketing,
+    );
     this.cookieConstent = false;
   }
-
-  // acceptCookieConsent() {
-  //   this.cookieConsentService.acceptCookieConsent();
-  //   this.cookieConstent = false;
-  // }
 
   Consent() {
     this.showPreferncesButton = true;
   }
-
-  // analytics() {
-  //   this.allowAnalytics = !this.allowAnalytics;
-  // }
-
-  // marketing() {
-  //   this.allowMarketing = !this.allowMarketing;
-  // }
-
-  // disagreeCookieConsent() {
-  //   this.cookieService.deleteAll();
-  //   window.location.href = 'about:blank';
-  // }
 }
-
-// Consent() {
-//   const managePreferencesBtn = document.getElementById('managePreferences') as HTMLButtonElement;
-//   const preferenceButtonsDiv = document.getElementById('preferenceButtons') as HTMLDivElement;
-//   managePreferencesBtn.addEventListener('click', () => {
-//     preferenceButtonsDiv.style.display = 'block';
-//   });
-// }
-
-// acceptNecessary() {
-//   sessionStorage.setItem('cookieConsentSelectedOption', 'necessary');
-//   this.allowNecessary = true;
-//   this.allowAnalytics = false;
-//   this.allowMarketing = false;
-// }
-
-// acceptAnalytics() {
-//   // this.allowAnalytics = !this.allowAnalytics;
-//   // sessionStorage.setItem('allowAnalytics', this.allowAnalytics ? 'true' : 'false');
-//   sessionStorage.setItem('cookieConsentSelectedOption', 'analytics');
-//   this.allowNecessary = false;
-//   this.allowAnalytics = true;
-//   this.allowMarketing = false;
-// }
-
-// acceptMarketing() {
-//   sessionStorage.setItem('cookieConsentSelectedOption', 'marketing');
-//   this.allowNecessary = false;
-//   this.allowAnalytics = false;
-//   this.allowMarketing = true;
-// }
