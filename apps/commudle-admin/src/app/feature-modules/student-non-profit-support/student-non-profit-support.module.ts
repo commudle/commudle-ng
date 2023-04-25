c6f97a91a89a8b07b71f44ac8a01294b80af8f85
Prerna <rprerna2000@gmail.com>
2023-04-25 17:30:54 +0530

    style: nebular button

--- apps/commudle-admin/src/app/feature-modules/student-non-profit-support/student-non-profit-support.module.ts
+++ apps/commudle-admin/src/app/feature-modules/student-non-profit-support/student-non-profit-support.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentNonProfitSupportRoutingModule } from './student-non-profit-support-routing.module';
 import { StudentNonProfitCommunitySupportComponent } from './components/student-non-profit-community-support/student-non-profit-community-support.component';
+import { NbButtonModule } from '@commudle/theme';
 
 @NgModule({
   declarations: [StudentNonProfitCommunitySupportComponent],
-  imports: [CommonModule, StudentNonProfitSupportRoutingModule],
+  imports: [CommonModule, StudentNonProfitSupportRoutingModule, NbButtonModule],
 })
 export class StudentNonProfitSupportModule {}
