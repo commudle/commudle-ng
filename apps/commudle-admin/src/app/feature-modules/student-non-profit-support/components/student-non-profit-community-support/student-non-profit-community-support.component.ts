36e1612581481ad055dc561976488e1b975526cc
Prerna <rprerna2000@gmail.com>
2023-04-26 15:56:03 +0530

    style: added button link and styles

--- apps/commudle-admin/src/app/feature-modules/student-non-profit-support/components/student-non-profit-community-support/student-non-profit-community-support.component.ts
+++ apps/commudle-admin/src/app/feature-modules/student-non-profit-support/components/student-non-profit-community-support/student-non-profit-community-support.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from 'apps/shared-services/seo.service';
 
 @Component({
   selector: 'commudle-student-non-profit-community-support',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-non-profit-community-support.component.scss'],
 })
 export class StudentNonProfitCommunitySupportComponent implements OnInit {
-  constructor() {}
+  constructor(private seoService: SeoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.seoService.setTags(
+      'Student & Non Profit Community Support',
+      'Building a community on Commudle is free for students and non profits. All features including events, video stage, QR code, channels, member management, projects, tutorials and more are free',
+      'https://commudle.com/assets/images/commudle-logo192.png',
+    );
+  }
 }
