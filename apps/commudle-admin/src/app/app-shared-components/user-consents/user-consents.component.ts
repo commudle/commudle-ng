7dd68968c2e83045def44cadd885162168df6e96
Prerna <rprerna2000@gmail.com>
2023-05-19 09:03:31 +0530

    refactor: moved consents component to user-consents

--- apps/commudle-admin/src/app/app-shared-components/user-consents/user-consents.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/user-consents/user-consents.component.ts
@@ -1,22 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbButtonModule, NbCardModule } from '@commudle/theme';
-import { UserFollowConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-follow-consent/user-follow-consent.component';
-import { JoinChannelConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/join-channel-consent/join-channel-consent.component';
-import { JoinCommunityConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/join-community-consent/join-community-consent.component';
-import { AcceptEventVolunteerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-event-volunteer-consent/accept-event-volunteer-consent.component';
-import { AcceptCommunityOrganizerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-community-organizer-consent/accept-community-organizer-consent.component';
-import { AcceptCommunityAdministratorConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-community-administrator-consent/accept-community-administrator-consent.component';
-import { AcceptBuildTeammateConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-build-teammate-consent/accept-build-teammate-consent.component';
-import { AcceptChannelTokenConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-channel-token-consent/accept-channel-token-consent.component';
-import { AcceptEventOneClickRegistrationConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-event-one-click-registration-consent/accept-event-one-click-registration-consent.component';
-import { AcceptEventFormRegistrationConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-event-form-registration-consent/accept-event-form-registration-consent.component';
-import { AcceptResumeConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-resume-consent/accept-resume-consent.component';
-import { AcceptSingleClickRegistrationSpeakerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-single-click-registration-speaker-consent/accept-single-click-registration-speaker-consent.component';
+import { UserFollowConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/user-follow-consent/user-follow-consent.component';
+import { JoinChannelConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/join-channel-consent/join-channel-consent.component';
+import { JoinCommunityConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/join-community-consent/join-community-consent.component';
+import { AcceptEventVolunteerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-event-volunteer-consent/accept-event-volunteer-consent.component';
+import { AcceptCommunityOrganizerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-community-organizer-consent/accept-community-organizer-consent.component';
+import { AcceptCommunityAdministratorConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-community-administrator-consent/accept-community-administrator-consent.component';
+import { AcceptBuildTeammateConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-build-teammate-consent/accept-build-teammate-consent.component';
+import { AcceptChannelTokenConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-channel-token-consent/accept-channel-token-consent.component';
+import { AcceptEventOneClickRegistrationConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-event-one-click-registration-consent/accept-event-one-click-registration-consent.component';
+import { AcceptEventFormRegistrationConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-event-form-registration-consent/accept-event-form-registration-consent.component';
+import { AcceptResumeConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-resume-consent/accept-resume-consent.component';
+import { AcceptSingleClickRegistrationSpeakerConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-single-click-registration-speaker-consent/accept-single-click-registration-speaker-consent.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faShieldHeart } from '@fortawesome/free-solid-svg-icons';
-import { AcceptJoinChannelEmailConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/accept-join-channel-email-consent/accept-join-channel-email-consent.component';
-
+import { AcceptJoinChannelEmailConsentComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-consents/accept-join-channel-email-consent/accept-join-channel-email-consent.component';
 @Component({
   selector: 'commudle-user-consents',
   standalone: true,
@@ -72,9 +71,24 @@ export class UserConsentsComponent implements OnInit {
   @Output() consentOutput = new EventEmitter<string>();
 
   faShieldHeart = faShieldHeart;
+
+  // @ViewChild('homepageAnimation', { static: false }) homepageAnimationContainer: ElementRef<HTMLDivElement>;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  // ngAfterViewInit(): void {
+  //   import('lottie-web').then((l) => {
+  //     l.default.loadAnimation({
+  //       container: this.homepageAnimationContainer.nativeElement,
+  //       renderer: 'svg',
+  //       loop: true,
+  //       autoplay: true,
+  //       path: 'https://assets7.lottiefiles.com/packages/lf20_3lol1shu/json files/json file.json',
+  //     });
+  //   });
+  // }
 
   accept() {
     this.consentOutput.emit('accepted');
