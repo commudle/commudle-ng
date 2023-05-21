9c16a18e396f6f14f2b521620d9a3f4c5e73248f
Prerna <rprerna2000@gmail.com>
2023-05-21 22:52:48 +0530

    refactor: added service

---
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/services/public-home-list-events.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PublicHomeListEventsService {
+  constructor() {}
+}
+// this.channelUsers = this.channelUsers.concat(data.user_roles_users);
