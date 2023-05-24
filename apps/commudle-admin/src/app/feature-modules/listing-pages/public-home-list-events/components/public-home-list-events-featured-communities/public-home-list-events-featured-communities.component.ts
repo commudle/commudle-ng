92ba447d949406d6268ec03fb74efb42c7cd3216
Prerna <rprerna2000@gmail.com>
2023-05-24 12:05:24 +0530

    refactor: featured communities

--- apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/public-home-list-events-featured-communities/public-home-list-events-featured-communities.component.ts
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/public-home-list-events-featured-communities/public-home-list-events-featured-communities.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FeaturedCommunitiesService } from 'apps/commudle-admin/src/app/services/featured-communities.service';
+import { IFeaturedCommunity } from 'apps/shared-models/featured-community.model';
 
 @Component({
   selector: 'commudle-public-home-list-events-featured-communities',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./public-home-list-events-featured-communities.component.scss'],
 })
 export class PublicHomeListEventsFeaturedCommunitiesComponent implements OnInit {
-  constructor() {}
+  featuredCommunities: IFeaturedCommunity[] = [];
 
-  ngOnInit(): void {}
+  constructor(private featuredCommunitiesService: FeaturedCommunitiesService) {}
+
+  ngOnInit(): void {
+    this.getFeaturedCommunities();
+  }
+
+  getFeaturedCommunities(): void {
+    this.featuredCommunitiesService.getLatestFeaturedCommunities().subscribe((value) => {
+      console.log(value);
+      this.featuredCommunities = value.featured_communities;
+    });
+  }
 }
