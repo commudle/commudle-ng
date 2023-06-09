6393bbaa08668510db7ce81a4c690eb289404da6
Prerna <rprerna2000@gmail.com>
2023-06-10 00:20:11 +0530

    style: fix-description-bug-user-profile

--- apps/commudle-admin/src/app/feature-modules/speaker-resources/components/speaker-resource/speaker-resource.component.ts
+++ apps/commudle-admin/src/app/feature-modules/speaker-resources/components/speaker-resource/speaker-resource.component.ts
@@ -71,9 +71,20 @@ export class SpeakerResourceComponent implements OnInit, OnDestroy {
     );
   }
 
+  // setMeta() {
+  //   this.seoService.setTags(
+  //     `${this.speakerResource.title} by ${this.speakerResource.user.name}`,
+  //     `Session by ${this.speakerResource.user.name} at ${this.speakerResource.event.name} on ${this.speakerResource.title}`,
+  //     this.speakerResource.user.avatar,
+  //   );
+  // }
+
   setMeta() {
+    const description = this.speakerResource.title
+      ? `${this.speakerResource.title} by ${this.speakerResource.user.name}`
+      : `${this.speakerResource.user.name}`;
     this.seoService.setTags(
-      `${this.speakerResource.title} by ${this.speakerResource.user.name}`,
+      description,
       `Session by ${this.speakerResource.user.name} at ${this.speakerResource.event.name} on ${this.speakerResource.title}`,
       this.speakerResource.user.avatar,
     );
