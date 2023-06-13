5597ed19c31a4df8a6b251de05e2f655216866bd
Prerna <rprerna2000@gmail.com>
2023-06-13 19:09:40 +0530

    style: fixed header style

---
+++ apps/commudle-admin/src/app/feature-modules/community-builds/components/community-builds/builds-header/builds-header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CmsService } from 'apps/shared-services/cms.service';
+import { IListingPageHeader } from 'apps/shared-models/listing-page-header.model';
+
+@Component({
+  selector: 'commudle-builds-header',
+  templateUrl: './builds-header.component.html',
+  styleUrls: ['./builds-header.component.scss'],
+})
+export class BuildsHeaderComponent implements OnInit {
+  buildsPageHeader: IListingPageHeader;
+  constructor(private cmsService: CmsService) {}
+
+  ngOnInit(): void {
+    this.getHeaderText();
+  }
+
+  imageUrl(source: any) {
+    return this.cmsService.getImageUrl(source);
+  }
+
+  getHeaderText() {
+    this.cmsService.getDataBySlug('builds').subscribe((data) => {
+      this.buildsPageHeader = data;
+    });
+  }
+}
