5b7d92527378dd581bc47a860404f7b3d15ae892
Prerna <rprerna2000@gmail.com>
2023-06-20 12:43:00 +0530

    style: resizing font-size

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -63,6 +63,7 @@ module.exports = {
       },
       gridColumn: {
         'span-16': 'span 16 / span 16',
+        'span-17': 'span 17 / span 17',
         'span-18': 'span 18 / span 18',
         'span-19': 'span 19 / span 19',
         'span-20': 'span 20 / span 20',
