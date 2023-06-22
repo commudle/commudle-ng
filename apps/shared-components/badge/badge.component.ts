36a3e7a884aca6baed6016a7e521816eed48114b
Prerna <rprerna2000@gmail.com>
2023-06-22 21:07:56 +0530

    style: fixed app-badge component

--- apps/shared-components/badge/badge.component.ts
+++ apps/shared-components/badge/badge.component.ts
@@ -41,7 +41,4 @@ export class BadgeComponent implements OnInit {
   backgroundColor() {
     return this.color || '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
   }
-  // backgroundColor() {
-  //   return '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
-  // }
 }
