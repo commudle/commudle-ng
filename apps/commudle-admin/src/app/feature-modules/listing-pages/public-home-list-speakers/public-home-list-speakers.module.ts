2687c9d335a7471f719841f40975b0a68cc6009e
Prerna <rprerna2000@gmail.com>
2023-07-03 23:50:55 +0530

    style: set the mobile view

--- apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-speakers/public-home-list-speakers.module.ts
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-speakers/public-home-list-speakers.module.ts
@@ -7,7 +7,7 @@ import { ListingPagesLayoutComponent } from 'apps/commudle-admin/src/app/app-sha
 import { SpeakersHeaderComponent } from './components/speakers-header/speakers-header.component';
 import { EventMiniCardComponent } from 'apps/commudle-admin/src/app/app-shared-components/event-mini-card/event-mini-card.component';
 import { PublicHomeListSpeakersUpcomingComponent } from './components/public-home-list-speakers-upcoming/public-home-list-speakers-upcoming.component';
-import { NbCardModule } from '@commudle/theme';
+import { NbButtonModule, NbCardModule } from '@commudle/theme';
 import { SkeletonCardsComponent } from 'apps/commudle-admin/src/app/feature-modules/skeleton-screens/components/skeleton-cards/skeleton-cards.component';
 import { PublicHomeListEventsModule } from 'apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/public-home-list-events.module';
 import { PublicHomeListSpeakersProfileComponent } from './components/public-home-list-speakers-profile/public-home-list-speakers-profile.component';
@@ -45,6 +45,7 @@ import { SkeletonVerticalCardsComponent } from 'apps/commudle-admin/src/app/feat
 
     //Nebular
     NbCardModule,
+    NbButtonModule,
   ],
 })
 export class PublicHomeListSpeakersModule {}
