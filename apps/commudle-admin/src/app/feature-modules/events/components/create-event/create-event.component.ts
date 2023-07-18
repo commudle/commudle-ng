8f6afd277f53a035304b822e76b27c4607c74c49
Arshdeep singh <arsh199820@gmail.com>
2023-07-18 22:23:04 +0530

    fix: event tags in create and edit details

--- apps/commudle-admin/src/app/feature-modules/events/components/create-event/create-event.component.ts
+++ apps/commudle-admin/src/app/feature-modules/events/components/create-event/create-event.component.ts
@@ -117,13 +117,7 @@ export class CreateEventComponent implements OnInit {
       }
     }
 
-    if (this.tags.length > 0) {
-      this.tags.forEach((value) => {
-        formValue['tags[]'] = value;
-      });
-    }
-
-    this.eventsService.createEvent(formValue, this.community).subscribe((data) => {
+    this.eventsService.createEvent(formValue, this.community, this.tags).subscribe((data) => {
       this.toastLogService.successDialog('Created!');
       this.router.navigate(['/admin/communities', this.community.slug, 'event-dashboard', data.slug]);
     });
