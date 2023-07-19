826a75cd048e709bbc7c466f9ec1dd387ec63b30
Prerna <rprerna2000@gmail.com>
2023-07-19 20:21:09 +0530

    style: removed comment

--- apps/commudle-admin/src/app/feature-modules/public-events/components/attended-members/attended-members.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-events/components/attended-members/attended-members.component.ts
@@ -25,7 +25,6 @@ export class AttendedMembersComponent implements OnInit, OnDestroy {
   page_info: IPageInfo;
   skeletonLoaderCard = true;
   searchForm;
-  // limit = 9;
   value = '';
   page = 1;
   count = 10;
@@ -47,32 +46,6 @@ export class AttendedMembersComponent implements OnInit, OnDestroy {
       name: [''],
     });
   }
-  // ngOnInit(): void {
-  //   this.subscriptions.push(
-  //     this.activatedRoute.parent.data.subscribe((data) => {
-  //       this.event = data.event;
-
-  //       if (this.activatedRoute.snapshot.queryParams['page']) {
-  //         this.page = Number(this.activatedRoute.snapshot.queryParams['page']);
-  //       }
-
-  //       this.seoService.setTags(
-  //         `Members who attended ${this.event.name}`,
-  //         `Connect with the community members who attended ${this.event.name} by ${this.activatedRoute.snapshot.params.community_id} with you`,
-  //         'https://commudle.com/assets/images/commudle-logo192.png',
-  //       );
-
-  //       this.getMembers();
-  //     }),
-  //   );
-
-  //   this.subscriptions.push(
-  //     this.queryChanged.pipe(debounceTime(800), distinctUntilChanged()).subscribe((q) => {
-  //       this.query = q;
-  //       this.search();
-  //     }),
-  //   );
-  // }
 
   ngOnInit(): void {
     this.search();
@@ -107,7 +80,6 @@ export class AttendedMembersComponent implements OnInit, OnDestroy {
       this.getMembers();
     }
 
-    // this.getMembers();
     this.seoService.setTags(
       `Members who attended ${this.event.name}`,
       `Connect with the community members who attended ${this.event.name} by ${this.activatedRoute.snapshot.params.community_id} with you`,
@@ -125,7 +97,6 @@ export class AttendedMembersComponent implements OnInit, OnDestroy {
       return;
     }
     this.canLoadMore = false;
-    // this.subscriptions.push(
     this.eventsService
       .getAttendedMembers(this.page, this.count, this.event.id, this.query, this.employer, this.employee)
       .subscribe((data) => {
@@ -135,86 +106,18 @@ export class AttendedMembersComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.canLoadMore = true;
       });
-    // );
   }
-  // getSpeakersList() {
-  //   this.loading = true;
-  //   if (this.loadingSpeakers) {
-  //     return;
-  //   }
-  //   this.loadingSpeakers = true;
-  //   if (!this.page_info?.end_cursor) {
-  //     this.speakers = [];
-  //   }
-
-  //   this.communitiesService
-  //     .getSpeakersList(
-  //       false,
-  //       this.page_info?.end_cursor,
-  //       this.limit,
-  //       this.query,
-  //       this.month,
-  //       this.year,
-  //       this.employer,
-  //       this.employee,
-  //     )
-  //     .subscribe((data) => {
-  //       this.speakers = this.speakers.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
-  //       this.total = data.total;
-  //       this.page_info = data.page_info;
-  //       this.skeletonLoaderCard = false;
-  //       this.loadingSpeakers = false;
-  //       this.loading = false;
-  //       this.isLoadingSearch = false;
-  //     });
-  // }
-
-  // search(): void {
-  //   this.subscriptions.push(
-  //     this.queryChanged.pipe(debounceTime(800), distinctUntilChanged()).subscribe((q) => {
-  //       this.query = q;
-  //       this.generateParams(this.employee, this.employer, this.query);
-  //     }),
-  //   );
-  //   this.page = 1;
-  // }
 
   search(): void {
     this.query = '';
     this.searchForm.valueChanges.pipe(debounceTime(800), distinctUntilChanged()).subscribe(() => {
-      // if (this.isLoadingSearch) {
-      //   return;
-      // }
       this.members = [];
       this.page_info = null;
-      // this.isLoadingSearch = true;
       this.query = this.searchForm.get('name').value;
-      // this.queryParamsString = this.query;
       this.generateParams(this.employee, this.employer, this.query);
     });
-    // this.subscriptions.push(
-    //   this.queryChanged.pipe(debounceTime(800), distinctUntilChanged()).subscribe((q) => {
-    //     this.query = q;
-    //     this.generateParams(this.employee, this.employer, this.query);
-    //   }),
-    // );
     this.page = 1;
   }
-
-  // search() {
-  //   this.query = '';
-  //   this.searchForm.valueChanges.pipe(debounceTime(800), distinctUntilChanged()).subscribe(() => {
-  //     if (this.isLoadingSearch) {
-  //       return;
-  //     }
-  //     this.speakers = [];
-  //     this.page_info = null;
-  //     this.isLoadingSearch = true;
-  //     this.query = this.searchForm.get('name').value;
-  //     this.queryParamsString = this.query;
-  //     this.generateParams(this.month, this.year, this.employee, this.employer, this.query);
-  //   });
-  // }
 
   updateFilter() {
     this.employee = this.value === 'employee' ? true : false;
