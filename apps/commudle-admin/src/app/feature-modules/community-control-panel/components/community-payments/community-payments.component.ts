6d62c70e7c151e3e2c235d4f3948260145bba70b
Arshdeep singh <arsh199820@gmail.com>
2023-07-20 22:26:32 +0530

    feat: retrieve stripe account

--- apps/commudle-admin/src/app/feature-modules/community-control-panel/components/community-payments/community-payments.component.ts
+++ apps/commudle-admin/src/app/feature-modules/community-control-panel/components/community-payments/community-payments.component.ts
@@ -11,7 +11,9 @@ export class CommunityPaymentsComponent implements OnInit {
   isLoading = false;
   constructor(private stripeHandlerService: StripeHandlerService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.retrieveStripeAccount();
+  }
 
   connectStripeAccount() {
     const currentUrl = this.router.url;
@@ -19,6 +21,16 @@ export class CommunityPaymentsComponent implements OnInit {
     this.stripeHandlerService.connectStripeAccount(currentUrl).subscribe((data) => {
       this.isLoading = false;
       window.open(data.url, '_blank');
+    });
+  }
+
+  retrieveStripeAccount() {
+    const accountId = 'bf1ea4fe-8f47-43b6-9846-31af0681e0f7';
+    this.stripeHandlerService.retrieveStripeAccount(accountId).subscribe((data) => {
+      console.log(
+        '🚀 ~ file: community-payments.component.ts:27 ~ CommunityPaymentsComponent ~ this.stripeHandlerService.retrieveStripeAccount ~ data:',
+        data,
+      );
     });
   }
 }
