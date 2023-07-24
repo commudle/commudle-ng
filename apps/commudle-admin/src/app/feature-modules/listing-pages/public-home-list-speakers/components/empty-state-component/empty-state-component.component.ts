1f6b296eff39d4122e78aa8a61f2dd90ae8ce550
Prerna <rprerna2000@gmail.com>
2023-07-24 11:49:09 +0530

    fix: fixed speakers api

--- apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-speakers/components/empty-state-component/empty-state-component.component.ts
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-speakers/components/empty-state-component/empty-state-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'commudle-empty-state-component',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empty-state-component.component.scss'],
 })
 export class EmptyStateComponentComponent implements OnInit {
+  @Input() parentType: string;
   constructor() {}
 
   ngOnInit(): void {}
