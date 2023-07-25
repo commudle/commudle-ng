4051fd03bb949b5cabc28560333174614d68fce7
Prerna <rprerna2000@gmail.com>
2023-07-25 23:32:56 +0530

    fix: basic dashboard-layout

---
+++ apps/commudle-admin/src/app/feature-modules/homepage/components/homepage-dashboard/homepage-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'commudle-homepage-dashboard',
+  templateUrl: './homepage-dashboard.component.html',
+  styleUrls: ['./homepage-dashboard.component.scss'],
+})
+export class HomepageDashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
