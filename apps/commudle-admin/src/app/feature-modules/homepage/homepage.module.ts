c4f6c69cc152c29c4bacf9fd855480eb4f90fc5d
Prerna <rprerna2000@gmail.com>
2023-07-25 23:33:16 +0530

    fix: basic dashboard-layout

--- apps/commudle-admin/src/app/feature-modules/homepage/homepage.module.ts
+++ apps/commudle-admin/src/app/feature-modules/homepage/homepage.module.ts
@@ -26,6 +26,10 @@ import { HomepageLabsComponent } from './components/homepage-labs/homepage-labs.
 import { HomepageTestimonialsComponent } from './components/homepage-testimonials/homepage-testimonials.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { HomepageRoutingModule } from './homepage-routing.module';
+import { HomepageDashboardComponent } from './components/homepage-dashboard/homepage-dashboard.component';
+import { UserprofileDetailsComponent } from './components/homepage-dashboard/userprofile-details/userprofile-details.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PublicHomeListEventsModule } from '../listing-pages/public-home-list-events/public-home-list-events.module';
 
 @NgModule({
   declarations: [
@@ -39,6 +43,8 @@ import { HomepageRoutingModule } from './homepage-routing.module';
     HomepageTestimonialsComponent,
     HomepageExpertsComponent,
     HomepageAboutComponent,
+    HomepageDashboardComponent,
+    UserprofileDetailsComponent,
   ],
   imports: [
     CommonModule,
@@ -51,12 +57,16 @@ import { HomepageRoutingModule } from './homepage-routing.module';
     SearchModule,
     MiniUserProfileModule,
 
+    //Nebular
     NbButtonModule,
     NbCardModule,
     NbIconModule,
     NbListModule,
     NbActionsModule,
     NbTooltipModule,
+    //FontAwesome
+    FontAwesomeModule,
+    PublicHomeListEventsModule,
   ],
 })
 export class HomepageModule {}
