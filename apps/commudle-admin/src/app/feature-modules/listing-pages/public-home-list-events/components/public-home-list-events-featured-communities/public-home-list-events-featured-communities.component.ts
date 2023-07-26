255951290cc5f81a789032b0e782fbc0af99b8f2
Prerna <rprerna2000@gmail.com>
2023-07-26 17:56:31 +0530

    style: featured communities scroll

--- apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/public-home-list-events-featured-communities/public-home-list-events-featured-communities.component.ts
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/public-home-list-events-featured-communities/public-home-list-events-featured-communities.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FeaturedCommunitiesService } from 'apps/commudle-admin/src/app/services/featured-communities.service';
 import { IFeaturedCommunity } from 'apps/shared-models/featured-community.model';
 
@@ -8,12 +8,15 @@ import { IFeaturedCommunity } from 'apps/shared-models/featured-community.model'
   styleUrls: ['./public-home-list-events-featured-communities.component.scss'],
 })
 export class PublicHomeListEventsFeaturedCommunitiesComponent implements OnInit {
+  @Input() showCardsHorizontal = false;
   featuredCommunities: IFeaturedCommunity[] = [];
   showSpinner = false;
+  isMobileView: boolean;
 
   constructor(private featuredCommunitiesService: FeaturedCommunitiesService) {}
 
   ngOnInit(): void {
+    this.isMobileView = window.innerWidth <= 1024;
     this.getFeaturedCommunities();
   }
 
