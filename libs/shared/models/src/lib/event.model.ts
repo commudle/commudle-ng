9b9169b37cc27143c91af839cd9c5d33cd2b9685
Arshdeep singh <arsh199820@gmail.com>
2023-07-28 19:08:04 +0530

    Update event.model.ts

--- libs/shared/models/src/lib/event.model.ts
+++ libs/shared/models/src/lib/event.model.ts
@@ -16,7 +16,6 @@ export interface IEvent {
   user: string;
   slug: string;
   event_status: IEventStatus;
-  status: string;
   seats: number;
   timezone: string;
   custom_registration: boolean;
