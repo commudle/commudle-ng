51a185d13da844cfc0e8efbd04dfe7f05121cde7
Arshdeep singh <arsh199820@gmail.com>
2023-07-31 16:40:47 +0530

    fix: event volunteer api

--- apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/events-volunteer-list/events-volunteer-list.component.ts
+++ apps/commudle-admin/src/app/feature-modules/listing-pages/public-home-list-events/components/events-volunteer-list/events-volunteer-list.component.ts
@@ -42,7 +42,7 @@ export class EventsVolunteerListComponent implements OnInit, OnDestroy {
     this.isLoadingVolunteers = true;
     this.subscriptions.push(
       this.eventsService
-        .pGetEventVolunteers(this.pageInfo?.end_cursor, this.limit, this.event.slug)
+        .pGetEventVolunteers(this.event.slug, this.limit, this.pageInfo?.end_cursor)
         .subscribe((data) => {
           this.volunteers = this.volunteers.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
           this.pageInfo = data.page_info;
