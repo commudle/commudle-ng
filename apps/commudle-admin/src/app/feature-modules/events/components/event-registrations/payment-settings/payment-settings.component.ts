116ed41e4dc024be2e0831c88818948c1456292d
Arshdeep singh <arsh199820@gmail.com>
2023-08-18 16:12:12 +0530

    feat: tax details form

--- apps/commudle-admin/src/app/feature-modules/events/components/event-registrations/payment-settings/payment-settings.component.ts
+++ apps/commudle-admin/src/app/feature-modules/events/components/event-registrations/payment-settings/payment-settings.component.ts
@@ -23,20 +23,46 @@ export class PaymentSettingsComponent implements OnInit {
   };
 
   dialogRef: NbDialogRef<any>;
+  bankAccountNo: string;
   constructor(
     private paymentSettingService: PaymentSettingService,
     private fb: FormBuilder,
     private dialogService: NbDialogService,
     private toastrService: ToastrService,
   ) {
-    this.paidTicketingForm = this.fb.group({
-      paid_ticket_setting: this.fb.group({
-        bank_ac_type: ['stripe', Validators.required],
-        bank_ac_id: ['', Validators.required],
-        price: ['', Validators.required],
-        currency: ['', Validators.required],
-      }),
-    });
+    this.paidTicketingForm = this.fb.group(
+      {
+        paid_ticket_setting: this.fb.group({
+          bank_ac_type: ['stripe', Validators.required],
+          bank_ac_id: ['', Validators.required],
+          price: ['', Validators.required],
+          currency: ['', Validators.required],
+          has_taxes: [false],
+          tax_name: [''],
+          tax_percentage: [''],
+          seller_tax_details: [''],
+          country: [''],
+          seller_name: [''],
+          seller_address: [''],
+        }),
+      },
+      {
+        validators: [
+          (fb) =>
+            fb.get('paid_ticket_setting').get('has_taxes').value === true &&
+            !fb.get('paid_ticket_setting').get('tax_name').value
+              ? {
+                  tax_name: true,
+                  tax_percentage: true,
+                  seller_tax_details: true,
+                  country: true,
+                  seller_name: true,
+                  seller_address: true,
+                }
+              : null,
+        ],
+      },
+    );
   }
 
   ngOnInit(): void {
@@ -53,6 +79,10 @@ export class PaymentSettingsComponent implements OnInit {
       this.paymentSettingService.indexPaymentSettings(this.edfeg.id).subscribe((data) => {
         if (data) {
           this.paymentData = data;
+          const selectedAccount = this.stripeAccounts.find(
+            (stripeAccount) => stripeAccount.uuid === this.paymentData.bank_ac_id,
+          );
+          this.bankAccountNo = selectedAccount.details.external_accounts.data[0].last4;
           this.paymentDetailsExist = true;
           this.updatePaidTicketingForm(this.paymentData);
         }
@@ -67,10 +97,11 @@ export class PaymentSettingsComponent implements OnInit {
       },
     });
     this.paymentSettingService.createPaymentSettings(this.paidTicketingForm.value, this.edfeg.id).subscribe((data) => {
-      this.toastrService.successDialog('Payment details has been updated');
+      this.toastrService.successDialog('Payment details has been created');
       this.paymentData = data;
+      this.paymentDetailsExist = true;
       this.updatePaidTicketingForm(this.paymentData);
-      this.close();
+      this.closeDialogBox();
     });
   }
 
@@ -86,20 +117,30 @@ export class PaymentSettingsComponent implements OnInit {
         this.paymentData = data;
         this.toastrService.successDialog('Payment details has been updated');
         this.updatePaidTicketingForm(this.paymentData);
-        this.close();
+        this.closeDialogBox();
       });
   }
 
   updatePaidTicketingForm(ticketDetails) {
-    this.paidTicketingForm.get('paid_ticket_setting').setValue({
+    const selectedAccount = this.stripeAccounts.find(
+      (stripeAccount) => stripeAccount.uuid === ticketDetails.bank_ac_id,
+    );
+    this.paidTicketingForm.get('paid_ticket_setting').patchValue({
       bank_ac_type: 'stripe',
       bank_ac_id: ticketDetails.bank_ac_id,
       price: ticketDetails.price / 100,
       currency: ticketDetails.currency,
+      has_taxes: ticketDetails.has_taxes,
+      tax_name: ticketDetails.tax_name,
+      tax_percentage: ticketDetails.tax_percentage,
+      seller_tax_details: ticketDetails.seller_tax_details,
+      country: ticketDetails.country,
+      seller_name: ticketDetails.seller_name,
+      seller_address: ticketDetails.seller_address,
     });
   }
 
-  close() {
+  closeDialogBox() {
     this.dialogRef.close();
   }
 }
