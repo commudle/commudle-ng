60a4af930962bd88882b50131088b11844263c2a
Prerna <rprerna2000@gmail.com>
2023-08-25 13:05:56 +0530

    style: seoBot service

--- apps/commudle-admin/src/app/feature-modules/public-events/components/agenda/event-location-tracks/event-location-tracks.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-events/components/agenda/event-location-tracks/event-location-tracks.component.ts
@@ -4,6 +4,7 @@ import { ICommunity } from 'apps/shared-models/community.model';
 import { IEventLocation } from 'apps/shared-models/event-location.model';
 import * as moment from 'moment';
 import { TrackSlotsService } from 'apps/commudle-admin/src/app/services/track_slots.service';
+import { SeoService } from 'apps/shared-services/seo.service';
 import * as _ from 'lodash';
 
 @Component({
@@ -25,14 +26,36 @@ export class EventLocationTracksComponent implements OnInit {
   totalSlotsCount: number;
   totalSlots: number;
   isMobileView: boolean;
+  visibility: boolean;
+  isBot: boolean;
 
-  constructor(private trackSlotsService: TrackSlotsService) {}
+  constructor(private trackSlotsService: TrackSlotsService, private seoService: SeoService) {}
+
+  // ngOnInit() {
+  //   this.isMobileView = window.innerWidth <= 640;
+  //   const visibility = this.eventLocation.event_location_tracks.length <= 2;
+  //   for (const event_location_track of this.eventLocation.event_location_tracks) {
+  //     this.trackSlotVisibility[event_location_track.id] = visibility;
+  //     this.sortedTrackSlots[event_location_track.id] = this.sortTrackSlots(event_location_track.track_slots);
+  //     this.totalSlotsCount = this.sortTrackSlots(event_location_track.track_slots).length;
+  //   }
+  //   this.isMobileView ? (this.totalSlots = 4) : (this.totalSlots = 8);
+  //   if (this.totalSlotsCount > this.totalSlots) {
+  //     this.footerText = `View More (${this.totalSlotsCount - this.totalSlots})`;
+  //   }
+  // }
 
   ngOnInit() {
     this.isMobileView = window.innerWidth <= 640;
-    const visibility = this.eventLocation.event_location_tracks.length <= 2;
+    if (this.seoService.isBot) {
+      this.visibility = true;
+      this.isBot = true;
+    } else {
+      this.visibility = this.eventLocation.event_location_tracks.length <= 2;
+      this.isBot = false;
+    }
     for (const event_location_track of this.eventLocation.event_location_tracks) {
-      this.trackSlotVisibility[event_location_track.id] = visibility;
+      this.trackSlotVisibility[event_location_track.id] = this.visibility;
       this.sortedTrackSlots[event_location_track.id] = this.sortTrackSlots(event_location_track.track_slots);
       this.totalSlotsCount = this.sortTrackSlots(event_location_track.track_slots).length;
     }
