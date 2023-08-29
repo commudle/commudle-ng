5fc0e5a01249dc3c84139f9b1e40200a3204475a
Arshdeep singh <arsh199820@gmail.com>
2023-08-29 11:20:21 +0530

    refactor: remove log

--- apps/commudle-admin/src/app/feature-modules/events/components/event-form-responses/user-payment-details/user-payment-details.component.ts
+++ apps/commudle-admin/src/app/feature-modules/events/components/event-form-responses/user-payment-details/user-payment-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { EventTicketOrderService } from '@commudle/shared-services';
 
@@ -7,27 +7,12 @@ import { EventTicketOrderService } from '@commudle/shared-services';
   templateUrl: './user-payment-details.component.html',
   styleUrls: ['./user-payment-details.component.scss'],
 })
-export class UserPaymentDetailsComponent implements OnInit {
+export class UserPaymentDetailsComponent {
   @Input() eto;
   subscription: Subscription[] = [];
   constructor(private eventTicketOrderService: EventTicketOrderService) {}
 
-  ngOnInit(): void {
-    this.eto;
-    console.log(
-      '🚀 ~ file: user-payment-details.component.ts:17 ~ UserPaymentDetailsComponent ~ ngOnInit ~ this.eto;:',
-      this.eto,
-    );
-  }
-
   refund(eventTicketOrderId) {
-    this.subscription.push(
-      this.eventTicketOrderService.checkRefundAmount(eventTicketOrderId).subscribe((data) => {
-        console.log(
-          '🚀 ~ file: user-payment-details.component.ts:25 ~ UserPaymentDetailsComponent ~ this.subscription.push ~ data:',
-          data,
-        );
-      }),
-    );
+    this.subscription.push(this.eventTicketOrderService.checkRefundAmount(eventTicketOrderId).subscribe((data) => {}));
   }
 }
