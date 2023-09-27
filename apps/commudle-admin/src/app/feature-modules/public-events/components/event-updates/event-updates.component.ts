26ce2cdd5f9531088f8d76b5c1c474014ce8a5ef
Arshdeep singh <arsh199820@gmail.com>
2023-09-27 19:12:17 +0530

    feat: click to open popup for image preview

--- apps/commudle-admin/src/app/feature-modules/public-events/components/event-updates/event-updates.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-events/components/event-updates/event-updates.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, TemplateRef } from '@angular/core';
 import { ICommunity } from 'apps/shared-models/community.model';
 import { IEvent } from 'apps/shared-models/event.model';
 import { EventUpdatesService } from 'apps/commudle-admin/src/app/services/event-updates.service';
 import { IEventUpdate } from 'apps/shared-models/event_update.model';
 import * as moment from 'moment';
 import { IPageInfo } from '@commudle/shared-models';
+import { NbDialogService } from '@commudle/theme';
 @Component({
   selector: 'app-event-updates',
   templateUrl: './event-updates.component.html',
@@ -20,7 +21,9 @@ export class EventUpdatesComponent implements OnInit {
   page_info: IPageInfo;
   limit = 5;
 
-  constructor(private eventUpdatesService: EventUpdatesService) {}
+  @ViewChild('imageTemplate') imageTemplate: TemplateRef<any>;
+
+  constructor(private eventUpdatesService: EventUpdatesService, private dialogService: NbDialogService) {}
 
   ngOnInit() {
     this.getEventUpdates();
@@ -36,5 +39,13 @@ export class EventUpdatesComponent implements OnInit {
           this.hasUpdates.emit(true);
         }
       });
+  }
+  openImage(image, eu) {
+    this.dialogService.open(this.imageTemplate, {
+      context: {
+        image: image,
+        eventUpdate: eu,
+      },
+    });
   }
 }
