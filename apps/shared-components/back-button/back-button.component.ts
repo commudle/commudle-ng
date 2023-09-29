3ed98cf503626d9774e8b4aa84afbdc1c48e3853
Arshdeep singh <arsh199820@gmail.com>
2023-09-29 14:54:15 +0530

    style: fix scanner height

--- apps/shared-components/back-button/back-button.component.ts
+++ apps/shared-components/back-button/back-button.component.ts
@@ -2,17 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'commudle-back-button',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
   templateUrl: './back-button.component.html',
   styleUrls: ['./back-button.component.scss'],
 })
 export class BackButtonComponent implements OnInit {
   @Input() text: string;
+  @Input() router: string;
   faChevronLeft = faChevronLeft;
+
   constructor(private location: Location) {}
 
   ngOnInit(): void {}
