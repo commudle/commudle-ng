f741162b25ea562dd73d101f2db33eda3d264df6
Prerna <rprerna2000@gmail.com>
2023-10-12 12:14:49 +0530

    style: notifications page

--- apps/commudle-admin/src/app/feature-modules/notifications/components/notifications-popover/notifications-popover.component.ts
+++ apps/commudle-admin/src/app/feature-modules/notifications/components/notifications-popover/notifications-popover.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ENotificationStatuses } from 'apps/shared-models/enums/notification_statuses.enum';
+import { INotification } from 'apps/shared-models/notification.model';
 
 @Component({
   selector: 'app-notifications-popover',
@@ -8,7 +10,18 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class NotificationsPopoverComponent implements OnInit {
   @Output() closePopover: EventEmitter<any> = new EventEmitter();
 
+  ENotificationStatuses = ENotificationStatuses;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  // changeStatus(status: ENotificationStatuses, notification: INotification) {
+  //   this.notificationsStore.changeStatus(status, notification);
+  //   if (status === ENotificationStatuses.INTERACTED) {
+  //     this.closePopover.emit();
+  //   } else {
+  //     this.gtmService();
+  //   }
+  // }
 }
