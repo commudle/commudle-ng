8d85a60dc854be2c6ffab8965f9203b024e6b38d
Prerna <rprerna2000@gmail.com>
2023-10-26 14:22:33 +0530

    style: basic layout

---
+++ apps/commudle-admin/src/app/feature-modules/public-pages/components/aggencies/aggencies.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'commudle-aggencies',
+  templateUrl: './aggencies.component.html',
+  styleUrls: ['./aggencies.component.scss'],
+})
+export class AggenciesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- apps/commudle-admin/src/app/feature-modules/public-pages/public-pages.module.ts
+++ apps/commudle-admin/src/app/feature-modules/public-pages/public-pages.module.ts
@@ -1,12 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NbCardModule } from '@commudle/theme';
-import { SharedPipesModule } from 'apps/shared-pipes/pipes.module';
-import { PublicPageGuidelinesComponent } from './components/public-page-guidelines/public-page-guidelines.component';
+import { CommonModule } from '@angular/common';
+
 import { PublicPagesRoutingModule } from './public-pages-routing.module';
+import { AggenciesComponent } from './components/aggencies/aggencies.component';
+import { AppSharedComponentsModule } from 'apps/commudle-admin/src/app/app-shared-components/app-shared-components.module';
+import { NbButtonModule } from '@commudle/theme';
 
 @NgModule({
-  declarations: [PublicPageGuidelinesComponent],
-  imports: [CommonModule, PublicPagesRoutingModule, NbCardModule, SharedPipesModule],
+  declarations: [AggenciesComponent],
+  imports: [CommonModule, PublicPagesRoutingModule, AppSharedComponentsModule, NbButtonModule],
 })
 export class PublicPagesModule {}
