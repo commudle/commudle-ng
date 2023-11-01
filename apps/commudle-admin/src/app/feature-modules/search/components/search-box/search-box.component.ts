d3ed5a1af12da45a9a766d38dc9b3302e44c719e
Prerna <rprerna2000@gmail.com>
2023-11-01 21:55:26 +0530

    style: search-box homepage

--- apps/commudle-admin/src/app/feature-modules/search/components/search-box/search-box.component.ts
+++ apps/commudle-admin/src/app/feature-modules/search/components/search-box/search-box.component.ts
@@ -1,16 +1,109 @@
+// import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import {
+//   getPicture,
+//   getRoute,
+//   groupResults,
+//   navigate,
+// } from 'apps/commudle-admin/src/app/feature-modules/search/components/utils/search.utils';
+// import { SearchStatusService } from 'apps/commudle-admin/src/app/feature-modules/search/services/search-status.service';
+// import { SearchService } from 'apps/commudle-admin/src/app/feature-modules/search/services/search.service';
+// import { ISearch, ISearchResult } from 'apps/shared-models/search.model';
+// import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-search-box',
+//   templateUrl: './search-box.component.html',
+//   styleUrls: ['./search-box.component.scss'],
+// })
+// export class SearchBoxComponent implements OnInit {
+//   @Input() overrideSearchStatus = false;
+//   @Input() showSuggestions = true;
+//   @Input() shape: 'round' | 'rectangle' | 'semi-round';
+
+//   inputFormControl: FormControl;
+//   total = -1;
+//   groupedResults = {};
+//   searchLoader = false;
+//   searchStatus = true;
+
+//   // use getRoute
+//   getRoute = getRoute;
+//   navigate = navigate;
+//   getPicture = getPicture;
+
+//   @ViewChild('searchInput', { static: false }) searchInputRef: ElementRef<HTMLInputElement>;
+
+//   constructor(
+//     private searchService: SearchService,
+//     public searchStatusService: SearchStatusService,
+//     private router: Router,
+//   ) {
+//     this.inputFormControl = new FormControl('');
+//     this.inputFormControl = this.fb.group({
+//       name: [''],
+//     });
+//   }
+
+//   ngOnInit() {
+//     this.observeInput();
+//     this.observeSearchStatus();
+//   }
+
+//   observeInput() {
+//     this.inputFormControl.valueChanges
+//       .pipe(
+//         filter((value) => typeof value === 'string'),
+//         map((value: string) => value.trim().toLowerCase()),
+//         filter(Boolean),
+//         distinctUntilChanged(),
+//         tap(() => {
+//           if (!this.showSuggestions) {
+//             this.onSubmit();
+//           }
+//         }),
+//         filter(() => this.showSuggestions),
+//         tap(() => {
+//           this.searchLoader = true;
+//           this.groupedResults = {};
+//         }),
+//         debounceTime(500),
+//         switchMap((value: string) => this.searchService.getSearchResults(value)),
+//       )
+//       .subscribe((value: ISearch) => {
+//         console.log('observe Input');
+//         this.groupedResults = groupResults(value.results);
+//         this.searchLoader = false;
+//         this.total = value.total;
+//       });
+//   }
+
+//   observeSearchStatus() {
+//     this.searchStatusService.searchStatus.subscribe((value: boolean) => {
+//       this.searchStatus = value;
+//     });
+//   }
+
+//   handleDisplay(result: ISearchResult | string) {
+//     return typeof result === 'string' ? result : result['query'] || result.name;
+//   }
+
+//   onSubmit() {
+//     this.router.navigate(['/search'], {
+//       queryParams: { q: this.inputFormControl.value?.name || this.inputFormControl.value },
+//     });
+//   }
+// }
+
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
-import {
-  getPicture,
-  getRoute,
-  groupResults,
-  navigate,
-} from 'apps/commudle-admin/src/app/feature-modules/search/components/utils/search.utils';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SearchStatusService } from 'apps/commudle-admin/src/app/feature-modules/search/services/search-status.service';
 import { SearchService } from 'apps/commudle-admin/src/app/feature-modules/search/services/search.service';
 import { ISearch, ISearchResult } from 'apps/shared-models/search.model';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-search-box',
@@ -22,58 +115,120 @@ export class SearchBoxComponent implements OnInit {
   @Input() showSuggestions = true;
   @Input() shape: 'round' | 'rectangle' | 'semi-round';
 
-  inputFormControl: FormControl;
+  @ViewChild('searchInput', { static: false }) searchInputRef: ElementRef<HTMLInputElement>;
+
+  inputFormControl;
+  query = '';
   total = -1;
-  groupedResults = {};
+  // isLoading = true;
   searchLoader = false;
   searchStatus = true;
-
-  // use getRoute
-  getRoute = getRoute;
-  navigate = navigate;
-  getPicture = getPicture;
-
-  @ViewChild('searchInput', { static: false }) searchInputRef: ElementRef<HTMLInputElement>;
+  searchResults = [];
+  Users = [];
+  Community = [];
+  Events = [];
+  Build = [];
+  Lab = [];
+  Event = [];
 
   constructor(
     private searchService: SearchService,
     public searchStatusService: SearchStatusService,
+    private fb: FormBuilder,
     private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private location: Location,
   ) {
-    this.inputFormControl = new FormControl('');
+    this.inputFormControl = this.fb.group({
+      name: [''],
+    });
   }
 
   ngOnInit() {
-    this.observeInput();
-    this.observeSearchStatus();
+    this.search();
+    const params = this.activatedRoute.snapshot.queryParams;
+    console.log(params, 'params');
+    if (Object.keys(params).length > 0) {
+      if (params.q) {
+        this.query = params.q;
+        this.inputFormControl.get('name').setValue(this.query);
+      }
+    }
+    if (!params.q) {
+      this.getNotifications();
+    }
   }
 
-  observeInput() {
-    this.inputFormControl.valueChanges
-      .pipe(
-        filter((value) => typeof value === 'string'),
-        map((value: string) => value.trim().toLowerCase()),
-        filter(Boolean),
-        distinctUntilChanged(),
-        tap(() => {
-          if (!this.showSuggestions) {
-            this.onSubmit();
-          }
-        }),
-        filter(() => this.showSuggestions),
-        tap(() => {
-          this.searchLoader = true;
-          this.groupedResults = {};
-        }),
-        debounceTime(500),
-        switchMap((value: string) => this.searchService.getSearchResults(value)),
-      )
-      .subscribe((value: ISearch) => {
-        console.log('observe Input');
-        this.groupedResults = groupResults(value.results);
+  search() {
+    this.query = '';
+    this.inputFormControl.valueChanges.pipe(debounceTime(800), distinctUntilChanged()).subscribe(() => {
+      this.query = this.inputFormControl.get('name').value;
+      if (!this.showSuggestions) {
+        this.onSubmit();
+      }
+      if (this.showSuggestions) {
+        this.searchLoader = true;
+      }
+      // if (this.isLoadingSearch) {
+      //   return;
+      // }
+      // this.isLoadingSearch = true;
+      // this.queryParamsString = this.query;
+      this.getNotifications();
+      // this.generateParams(this.query);
+    });
+  }
+
+  generateParams(query) {
+    // this.skeletonLoaderCard = true;
+    const queryParams: { [key: string]: any } = {};
+
+    if (query) {
+      queryParams.q = query;
+    }
+    // this.seoTitle = this.query;
+    // this.updateSeoTitle();
+    const urlSearchParams = new URLSearchParams(queryParams);
+    const queryParamsString = urlSearchParams.toString();
+    this.location.replaceState(location.pathname, queryParamsString);
+  }
+
+  getNotifications() {
+    console.log('fn called');
+    this.searchResults = [];
+    this.Users = [];
+    this.Community = [];
+    this.Events = [];
+    this.Build = [];
+    this.Lab = [];
+    this.Event = [];
+
+    if (this.query === '') {
+      console.log('empty query');
+      this.total = -1;
+      this.searchLoader = false;
+    }
+
+    this.searchService.getSearchResults(this.query).subscribe((value) => {
+      // this.isLoading = true;
+      this.searchResults = value.results.map((result) => {
+        if (result?.type === 'User') {
+          this.Users.push(result);
+        } else if (result?.type === 'Community') {
+          this.Community.push(result);
+        } else if (result?.type === 'Lab') {
+          this.Lab.push(result);
+        } else if (result?.type === 'Community Build') {
+          this.Build.push(result);
+        } else if (result?.type === 'Event') {
+          this.Event.push(result);
+        }
         this.searchLoader = false;
         this.total = value.total;
+        return result;
       });
+      // this.isLoading = false;
+    });
   }
 
   observeSearchStatus() {
@@ -82,13 +237,11 @@ export class SearchBoxComponent implements OnInit {
     });
   }
 
-  handleDisplay(result: ISearchResult | string) {
-    return typeof result === 'string' ? result : result['query'] || result.name;
-  }
-
   onSubmit() {
+    console.log('called onSubmit');
     this.router.navigate(['/search'], {
-      queryParams: { q: this.inputFormControl.value?.name || this.inputFormControl.value },
+      queryParams: { q: this.query },
+      // queryParams: { q: this.inputFormControl.value?.name || this.inputFormControl.value },
     });
   }
 }
