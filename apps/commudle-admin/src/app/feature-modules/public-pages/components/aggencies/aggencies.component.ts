7f73d5450db3bda7a9fbb7662e63db7a3b107372
Prerna <rprerna2000@gmail.com>
2023-11-07 23:54:11 +0530

    style: public page stats

--- apps/commudle-admin/src/app/feature-modules/public-pages/components/aggencies/aggencies.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-pages/components/aggencies/aggencies.component.ts
@@ -8,6 +8,81 @@ import { staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 })
 export class AggenciesComponent implements OnInit {
   staticAssets = staticAssets;
+  stats: any[] = [
+    {
+      number: 1,
+      name: 'name1',
+      description: 'description1',
+    },
+    {
+      number: 2,
+      name: 'name2',
+      description: 'description2',
+    },
+    {
+      number: 3,
+      name: 'name3',
+      description: 'description3',
+    },
+  ];
+
+  testimonials: any[] = [
+    {
+      name: 'name1',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name2',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name3',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name4',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name5',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name6',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name7',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name8',
+      username: 'username1',
+      testimonial: 'testimonial1',
+      date: 'date1',
+    },
+    {
+      name: 'name9',
+      username: 'username1',
+      testimonialiText: 'testimonial1',
+      date: 'date1',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
