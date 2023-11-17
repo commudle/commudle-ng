3cdc7954b504605ad72dae0c9e786a256c9b00c3
Prerna <rprerna2000@gmail.com>
2023-11-17 14:12:48 +0530

    style: add fragments on book public page

--- apps/commudle-admin/src/app/feature-modules/public-pages/components/book-page/book-page.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-pages/components/book-page/book-page.component.ts
@@ -93,6 +93,22 @@ export class BookPageComponent implements OnInit {
     },
   ];
 
+  questions = [
+    'Can I create multiple organizations or business pages which have communities under them?',
+    'What is the subscription / payment model?',
+    'Does Commudle have the option for custom dashboards and API’s?',
+    'How do you ensure that my communities rank up on search engines?',
+    'How do you ensure data privacy of the users and my clients?',
+  ];
+
+  answers = [
+    'Yes, Commudle has the features for creating an umbrella of communities for different businesses you manage and each can have a separate page and communities under it. You can run multiple technology based, geography or any other classification based global communities on Commudle.',
+    'You can purchase annual community subscriptions for individual chapters or in bulk exclusively for your business clients.',
+    'Yes, we can create custom dashboards or API’s for you to integrate in your existing dashboards as per requirements. The charges for these can be a part of your subscription plan.',
+    'All our pages with rich content are search engine optimized and we have dedicated experts who keep improving the strategy. Most of our existing active communities pages have top ranks on search engines.',
+    'Commudle is GDPR compliant and ISO 27001 certified. We take data privacy very seriously and understand its importance for businesses and have placed consents at multiple points on the platform so that the users know how their data will be processed and who will have access to it.',
+  ];
+
   ngOnInit(): void {
     console.log(this.feature1.heading);
   }
