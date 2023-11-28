1ac54fcf87cd7ebbae9db051b0bb03886f7d599f
Prerna <rprerna2000@gmail.com>
2023-11-28 21:42:00 +0530

    style: basic layout of left column reading-book

--- apps/commudle-admin/src/app/feature-modules/public-reading-book/components/reading-book-sidebar/reading-book-sidebar.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-reading-book/components/reading-book-sidebar/reading-book-sidebar.component.ts
@@ -6,16 +6,18 @@ import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./reading-book-sidebar.component.scss'],
 })
 export class ReadingBookSidebarComponent implements OnInit {
-  @Input() chapter; //lab
+  @Input() chapterIndexes;
   @Input() selectedChapterIndex: number; //labstep
 
   @Output() setChapter: EventEmitter<number> = new EventEmitter<number>();
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // console.log(this.chapterIndexes, 'index');
+  }
 
-  onSetStep(value: number) {
-    this.setChapter.emit(value);
+  onSetStep(index: number) {
+    this.setChapter.emit(index);
     // if (this.lab.lab_steps[value]?.id === this.lab.last_visited_step_id) {
     //   this.showContinue = false;
     // }
