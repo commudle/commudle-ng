7ffd2a4b49fcf343a75ce0241c136ad69a55a496
Prerna <rprerna2000@gmail.com>
2023-12-06 17:27:47 +0530

    style: download btn functionality

---
+++ apps/shared-models/reading_book.model.ts
@@ -0,0 +1,9 @@
+export interface IReadingBookModel {
+  chapter_name: string;
+  header_image?: string;
+  fun_facts?: string;
+  slug: any;
+  how_commudle_helps?: string;
+  link_to_previous_page?: string;
+  link_to_next_page?: string;
+}
