855d19b8d5478c8014be59064d7e411b4eb5f37d
Prerna <rprerna2000@gmail.com>
2023-12-21 13:46:29 +0530

    style: standard width and height

--- apps/commudle-admin/src/app/app-shared-components/featured-projects-card/featured-projects-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/featured-projects-card/featured-projects-card.component.ts
@@ -4,7 +4,6 @@ import { NbButtonModule, NbCardModule } from '@commudle/theme';
 import { MiniUserProfileModule } from 'apps/shared-modules/mini-user-profile/mini-user-profile.module';
 import * as moment from 'moment';
 import { SharedComponentsModule } from 'apps/shared-components/shared-components.module';
-import { IFeaturedItems } from 'apps/shared-models/featured-items.model';
 import { RouterModule } from '@angular/router';
 import { ICommunityBuild } from '@commudle/shared-models';
 
