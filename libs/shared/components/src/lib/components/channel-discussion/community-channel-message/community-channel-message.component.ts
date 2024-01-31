34a7eb5e2f67afadefe7a9d4792a19d8d33980b5
Arshdeep singh <arsh199820@gmail.com>
2024-01-31 12:12:33 +0530

    fix: username if name does not exist

--- libs/shared/components/src/lib/components/channel-discussion/community-channel-message/community-channel-message.component.ts
+++ libs/shared/components/src/lib/components/channel-discussion/community-channel-message/community-channel-message.component.ts
@@ -245,7 +245,7 @@ export class CommunityChannelMessageComponent implements OnInit, AfterViewInit {
       text: this.removeHtmlTags(this.message.content),
       author: {
         '@type': 'Person',
-        name: this.message.user.name,
+        name: this.message.user.name ? this.message.user.name : this.message.user.username,
         url: `https://www.commudle.com/users/${this.message.user.username}`,
       },
       datePublished: this.message.created_at,
@@ -263,7 +263,7 @@ export class CommunityChannelMessageComponent implements OnInit, AfterViewInit {
           text: this.removeHtmlTags(userMessage.content),
           author: {
             '@type': 'Person',
-            name: userMessage.user.name,
+            name: userMessage.user.name ? userMessage.user.name : userMessage.user.username,
             url: `https://www.commudle.com/users/${userMessage.user.username}`,
           },
           datePublished: userMessage.created_at,
