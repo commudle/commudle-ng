89e9b5b1a616274d8d63670ca640a8e0d1e5f57d
Prerna <rprerna2000@gmail.com>
2024-02-07 15:16:20 +0530

    style: featured experts

--- apps/commudle-admin/src/app/app-shared-components/featured-experts-card/featured-experts-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/featured-experts-card/featured-experts-card.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NbCardModule } from '@commudle/theme';
 import { staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 import { IUser } from '@commudle/shared-models';
+import { MiniUserProfileModule } from 'apps/shared-modules/mini-user-profile/mini-user-profile.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'commudle-featured-experts-card',
   standalone: true,
-  imports: [CommonModule, NbCardModule],
   templateUrl: './featured-experts-card.component.html',
   styleUrls: ['./featured-experts-card.component.scss'],
+  imports: [CommonModule, RouterModule, NbCardModule, MiniUserProfileModule],
 })
 export class FeaturedExpertsCardComponent implements OnInit {
   @Input() expert: IUser;
