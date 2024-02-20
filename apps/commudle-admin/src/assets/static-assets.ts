bf42ba9bb4fec4eacfecd09e9b3181fe916153ff
Prerna <rprerna2000@gmail.com>
2024-02-20 23:56:01 +0530

    style: pricing page image

--- apps/commudle-admin/src/assets/static-assets.ts
+++ apps/commudle-admin/src/assets/static-assets.ts
@@ -62,10 +62,10 @@ export const staticAssets = {
     'https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNUt0QVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--5b2ccc739fd77ba1a8608864352b8608733279f3/Content.svg',
 
   pricing_students:
-    'https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNkhVQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--cb454b49eb21754319f1822b8ce98b904d065a6b/Commudle%20for%20Students%20(Pricing%20Page).png',
+    'https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNlBVQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--239f0b40cbdaf2e55f8e5da07a7f767262f4770d/Commudle%20for%20Students.png',
 
   pricing_enterprise:
-    'https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNTdVQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--88ba7626fc70db4ea9259f734f01ca8733adf7ee/Commudle%20for%20Enterprise%20(Pricing%20Page).png',
+    'https://json.commudle.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNlRVQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--93d3ffeb355bd0a57e15c1c0eb2b0fea62e436f4/Commudle%20for%20Enterprise.png',
 
   pricing_usecase_cards_desktop:
     'https://json.commudle.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMnBnQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--19af23aeba1556ddef013ced2d20db12ab1b2c4e/Usecase%20Cards%20Desktop.svg',
