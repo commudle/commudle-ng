23fdac882b741b2c233c0cbab1d96b82a9a2aa3f
Arshdeep Singh <arsh199820@gmail.com>
2024-03-20 23:26:45 +0530

    feat: interested members

--- apps/commudle-admin/src/app/feature-modules/public-hackathon/components/public-hackathon-details-mini-card/public-hackathon-details-mini-card.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-hackathon/components/public-hackathon-details-mini-card/public-hackathon-details-mini-card.component.ts
@@ -4,6 +4,8 @@ import { IHackathonTeam } from 'apps/shared-models/hackathon-team.model';
 import { IHackathon, EHackathonLocationType } from 'apps/shared-models/hackathon.model';
 import { faGlobe, faAward } from '@fortawesome/free-solid-svg-icons';
 import { countries_details } from '@commudle/shared-services';
+import { HackathonService } from 'apps/commudle-admin/src/app/services/hackathon.service';
+import { IUser } from '@commudle/shared-models';
 @Component({
   selector: 'commudle-public-hackathon-details-mini-card',
   templateUrl: './public-hackathon-details-mini-card.component.html',
@@ -21,15 +23,25 @@ export class PublicHackathonDetailsMiniCardComponent implements OnInit {
   EHackathonLocationType = EHackathonLocationType;
   totalPrizesByCurrency: { currency: any; amount: number }[];
   countryDetails = countries_details;
+  users: IUser[];
+  totalUsers: number;
 
-  // constructor() {}
+  constructor(private hackathonService: HackathonService) {}
 
   ngOnInit() {
+    this.fetchInterestedMembers();
     if (this.hackathon.total_prize_amount) {
       this.totalPrizesByCurrency = Object.keys(this.hackathon.total_prize_amount).map((currency) => ({
         currency: this.countryDetails.find((detail) => detail.currency === currency),
         amount: this.hackathon.total_prize_amount[currency],
       }));
     }
+  }
+
+  fetchInterestedMembers() {
+    this.hackathonService.pInterestedUsers(this.hackathon.id).subscribe((data) => {
+      this.users = data.users;
+      this.totalUsers = data.total_count;
+    });
   }
 }
