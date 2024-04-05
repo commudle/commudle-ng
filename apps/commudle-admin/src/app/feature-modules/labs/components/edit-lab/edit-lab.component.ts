92668fbc6095d901568942503ecb9e60b646c9b7
Prerna <rprerna2000@gmail.com>
2024-04-05 23:18:59 +0530

    style: lab main column

--- apps/commudle-admin/src/app/feature-modules/labs/components/edit-lab/edit-lab.component.ts
+++ apps/commudle-admin/src/app/feature-modules/labs/components/edit-lab/edit-lab.component.ts
@@ -32,7 +32,7 @@ export class EditLabComponent implements OnInit, OnDestroy {
   faEdit = faEdit;
   tinyMCE: any = {
     placeholder:
-      'Add a description with some pictures to help the user get a brief of what you are going to teach them in this tutorial!*',
+      'Provide a brief overview of the tutorial with some images to help users understand what they will learn.',
     min_height: 500,
     menubar: false,
     convert_urls: false,
