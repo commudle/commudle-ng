27b360b34d798624db93ae8149304067ba06c9a2
Arshdeep Singh <arsh199820@gmail.com>
2024-04-13 10:07:18 +0530

    feat: razorpay key

--- libs/shared/environments/src/lib/environments.ts
+++ libs/shared/environments/src/lib/environments.ts
@@ -11,6 +11,7 @@ type Environment = {
   google_client_id: string;
   stripe: string;
   sentry_dsn: string;
+  razorpay_key: string;
 };
 
 const environments: { [type: string]: Environment } = {
@@ -28,6 +29,7 @@ const environments: { [type: string]: Environment } = {
     stripe:
       'pk_live_51NIQahSAaAm97Wzm6Nthh2SsbFH123ckuvoO9P4fEghH2IeC5laeiJmYSBHbOt9bFE1fHY1Hwig5lqiHko0bn7Yi00EWLt1AuB',
     sentry_dsn: 'https://008ea5f833883ac6e933856b26757b7e@o566989.ingest.sentry.io/4506098297405440',
+    razorpay_key: 'rzp_test_AQ8emxZcsJoKdl',
   },
   test: {
     production: false,
@@ -43,6 +45,7 @@ const environments: { [type: string]: Environment } = {
     stripe:
       'pk_test_51NIQahSAaAm97WzmtpZtqYAuI1cCfN7LAJPoy8SmBpJqXQ5c7gnmOXXS9VtXa1b6YvCa1Uc9bX3Ra9ZLjm4AQBSs00en3kVojH',
     sentry_dsn: 'https://008ea5f833883ac6e933856b26757b7e@o566989.ingest.sentry.io/4506098297405440',
+    razorpay_key: 'rzp_test_AQ8emxZcsJoKdl',
   },
   staging: {
     production: false,
@@ -58,6 +61,7 @@ const environments: { [type: string]: Environment } = {
     stripe:
       'pk_live_51NIQahSAaAm97Wzm6Nthh2SsbFH123ckuvoO9P4fEghH2IeC5laeiJmYSBHbOt9bFE1fHY1Hwig5lqiHko0bn7Yi00EWLt1AuB',
     sentry_dsn: 'https://008ea5f833883ac6e933856b26757b7e@o566989.ingest.sentry.io/4506098297405440',
+    razorpay_key: 'rzp_test_AQ8emxZcsJoKdl',
   },
   production: {
     production: true,
@@ -73,6 +77,7 @@ const environments: { [type: string]: Environment } = {
     stripe:
       'pk_live_51NIQahSAaAm97Wzm6Nthh2SsbFH123ckuvoO9P4fEghH2IeC5laeiJmYSBHbOt9bFE1fHY1Hwig5lqiHko0bn7Yi00EWLt1AuB',
     sentry_dsn: 'https://008ea5f833883ac6e933856b26757b7e@o566989.ingest.sentry.io/4506098297405440',
+    razorpay_key: 'rzp_test_AQ8emxZcsJoKdl',
   },
 };
 
