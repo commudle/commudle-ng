59ab2645adbde4218e20010ed220bc9ed3673abc
Ashwin <bsashwin9@gmail.com>
2024-05-05 20:22:19 -0700

    feat: dapp wallet

---
+++ libs/shared/models/src/lib/dapp-wallet.model.ts
@@ -0,0 +1,5 @@
+export interface IDappWallet {
+  id: number;
+  address: string;
+  walletType: 'metamask';
+}
---
+++ libs/shared/services/src/lib/metamask.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MetamaskService {
+  private ethereum: any;
+
+  constructor() {
+    const { ethereum } = <never>window;
+    this.ethereum = ethereum;
+  }
+
+  public async connectWallet(): Promise<any> {
+    if (!this.ethereum) {
+      return Promise.reject('Please install MetaMask!');
+    }
+
+    try {
+      return await this.ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (err) {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error.
+        // If this happens, the user rejected the connection request.
+        return Promise.reject('Please connect to MetaMask.');
+      } else {
+        return Promise.reject(err);
+      }
+    }
+  }
+
+  // revoke access to the dapp
+  public async disconnectWallet(): Promise<void> {
+    if (!this.ethereum) {
+      return Promise.reject('Please install MetaMask!');
+    }
+
+    return await this.ethereum.request({ method: 'wallet_revokePermissions', params: [{ eth_accounts: {} }] });
+  }
+}
