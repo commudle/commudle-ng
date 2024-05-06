2f12e3f3a550516c0f7b386649edac18e6bf3106
Prerna <rprerna2000@gmail.com>
2024-05-06 23:16:47 +0530

    refactor: update cookie functionality

--- apps/commudle-admin/src/app/app-shared-components/whats-new/whats-new.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/whats-new/whats-new.component.ts
@@ -5,7 +5,6 @@ import { faBullhorn, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SeoService } from 'apps/shared-services/seo.service';
 import { WhatsNewService } from 'apps/shared-services/whats-new.service';
-import moment from 'moment';
 import { WhatsNewCardComponent } from './whats-new-card/whats-new-card.component';
 import { IWhatsNew } from 'apps/shared-models/whats-new.model';
 
@@ -21,7 +20,6 @@ export class WhatsNewComponent implements OnInit {
   showDialogPopup = false;
   cookieCreationTime;
   lastUpdatedDate: string;
-  updates: IWhatsNew[] = [];
   newUpdates: IWhatsNew[];
   faBullhorn = faBullhorn;
   faXmark = faXmark;
@@ -35,24 +33,26 @@ export class WhatsNewComponent implements OnInit {
   ngOnInit(): void {
     if (!this.seoService.isBot) {
       setTimeout(() => {
-        this.updates = [];
-        this.whatsNewService.getNewUpdates().subscribe((data) => {
-          this.newUpdates = data;
-          const pastTime = new Date(moment().subtract(2, 'months').format());
-          this.cookieCreationTime = new Date(this.whatsNewService.getCookieByName('last_update_seen'));
-          const date = this.whatsNewService.getCookieByName('last_update_seen') ? this.cookieCreationTime : pastTime;
-          for (const newUpdate of this.newUpdates) {
-            if (new Date(newUpdate.date) > date) {
-              this.updates.push(newUpdate);
-            }
-          }
-          if (this.updates.length > 0) {
+        this.newUpdates = [];
+        const currentDate = new Date();
+        const formattedCurrentDate = new Date().toISOString();
+        currentDate.setMonth(currentDate.getMonth() - 2);
+        const formattedPastTime = currentDate.toISOString();
+        const date = this.whatsNewService.getCookieByName('com-last-whats-new-seen')
+          ? formattedCurrentDate
+          : formattedPastTime;
+        this.whatsNewService.getNewUpdates(date).subscribe((data) => {
+          if (data.length > 0) {
+            this.newUpdates = data;
             this.showPopup = true;
-            this.whatsNewService.setCookieCreationTime('last_update_seen');
           }
         });
       }, 5000);
     }
+  }
+
+  setCookie() {
+    this.whatsNewService.setCookieCreationTime('com-last-whats-new-seen');
   }
 
   openDialog(templateRef: TemplateRef<any>) {
