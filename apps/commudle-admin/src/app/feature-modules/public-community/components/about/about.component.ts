5a7d2da27dfc2cca72e75ffdd7d1a0276ba29fd3
Prerna <rprerna2000@gmail.com>
2024-05-14 16:01:45 +0530

    style: upcoming-card in about

--- apps/commudle-admin/src/app/feature-modules/public-community/components/about/about.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-community/components/about/about.component.ts
@@ -5,6 +5,9 @@ import { ICommunity } from 'apps/shared-models/community.model';
 import { EUserRoles } from 'apps/shared-models/enums/user_roles.enum';
 import { IUser } from 'apps/shared-models/user.model';
 import { SeoService } from 'apps/shared-services/seo.service';
+import { EventsService } from 'apps/commudle-admin/src/app/services/events.service';
+import { IEvent } from 'apps/shared-models/event.model';
+import moment from 'moment';
 
 @Component({
   selector: 'app-about',
@@ -15,6 +18,8 @@ export class AboutComponent implements OnInit {
   community: ICommunity = null;
   EUserRoles = EUserRoles;
   organizers: IUser[] = [];
+  events: IEvent[] = [];
+  upcomingEvents: IEvent[] = [];
 
   isLoading = false;
 
@@ -22,11 +27,13 @@ export class AboutComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private userRolesUsersService: UserRolesUsersService,
     private seoService: SeoService,
+    private eventsService: EventsService,
   ) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe((data) => {
       this.community = data.community;
+      this.getEvents();
       this.seoService.setTitle(this.community.name);
       this.getOrganizers([EUserRoles.ORGANIZER, EUserRoles.EVENT_VOLUNTEER]);
     });
@@ -40,6 +47,19 @@ export class AboutComponent implements OnInit {
         this.organizers = this.organizers.concat(data.users);
         this.isLoading = false;
       });
+    });
+  }
+
+  getEvents() {
+    this.isLoading = true;
+    this.eventsService.pGetCommunityEvents(this.community.id).subscribe((data) => {
+      this.events = data.events;
+      this.events.forEach((event) => {
+        if (moment(event.end_time) > moment() || event.end_time === null) {
+          this.upcomingEvents.push(event);
+        }
+      });
+      this.isLoading = false;
     });
   }
 }
