b7c15cc5d96e49d72f63aba504ae4de9d3dbedb4
Prerna <rprerna2000@gmail.com>
2024-05-16 22:50:20 +0530

    style: fetch the deatils of challenge, solution, testimonialss

--- apps/commudle-admin/src/app/feature-modules/case-study/components/case-study-taarangana/case-study-taarangana.component.ts
+++ apps/commudle-admin/src/app/feature-modules/case-study/components/case-study-taarangana/case-study-taarangana.component.ts
@@ -9,52 +9,64 @@ import { CmsService } from 'apps/shared-services/cms.service';
   styleUrls: ['./case-study-taarangana.component.scss'],
 })
 export class CaseStudyTaaranganaComponent implements OnInit {
-  caseStudyPageHeader: IListingPageHeader;
+  // caseStudyPageHeader: IListingPageHeader;
+  caseStudyPageHeader;
   richTextChallenges: string;
   richTextSolution: string;
-  testimonials: ITestimonial[];
+  richTextStats: any[] = [];
+  richTextTestimonials = [];
+  // richTextTestimonials: ITestimonial[] = [];
 
-  stats: any[] = [
-    {
-      number: '8000+',
-      name: 'Registrations',
-      description: 'Thousands of developers use Commudle to share knowledge, build recognition and find opportunities.',
-    },
-    {
-      number: '3000+',
-      name: 'Attendees',
-      description:
-        'From being an organizer at a developer community to participating as a member in multiple others, developers empower each other by sharing knowledge.',
-    },
-    {
-      number: '2',
-      name: 'Days',
-      description:
-        "When we say diverse, we don't just mean location though, our users have vibrant technology backgrounds!",
-    },
-  ];
+  // stats: any[] = [
+  //   {
+  //     number: '8000+',
+  //     name: 'Registrations',
+  //     description: 'Thousands of developers use Commudle to share knowledge, build recognition and find opportunities.',
+  //   },
+  //   {
+  //     number: '3000+',
+  //     name: 'Attendees',
+  //     description:
+  //       'From being an organizer at a developer community to participating as a member in multiple others, developers empower each other by sharing knowledge.',
+  //   },
+  //   {
+  //     number: '2',
+  //     name: 'Days',
+  //     description:
+  //       "When we say diverse, we don't just mean location though, our users have vibrant technology backgrounds!",
+  //   },
+  // ];
+
   constructor(private cmsService: CmsService) {}
 
   ngOnInit(): void {
     this.getHeaderText();
-    this.getTestimonials();
+    // this.getTestimonials();
   }
 
   getHeaderText() {
     this.cmsService.getDataBySlug('taarangana').subscribe((data) => {
       this.caseStudyPageHeader = data;
+      console.log(this.caseStudyPageHeader);
       this.richTextChallenges = this.cmsService.getHtmlFromBlock(data, 'challenge');
       this.richTextSolution = this.cmsService.getHtmlFromBlock(data, 'solution');
+      this.caseStudyPageHeader.stats.forEach((stat) => {
+        this.richTextStats.push(this.cmsService.getHtmlFromBlock(stat));
+      });
+      this.caseStudyPageHeader.testimonials.forEach((testimonial) => {
+        this.richTextTestimonials.push(this.cmsService.getHtmlFromBlock(testimonial));
+        console.log(this.richTextTestimonials);
+      });
     });
   }
 
-  getTestimonials() {
-    this.cmsService
-      .getDataByTypeWithFilter('publicTestimonials', 'testimonialType', 'Community_Leader', 10)
-      .subscribe((data) => {
-        if (data) {
-          this.testimonials = data;
-        }
-      });
-  }
+  // getTestimonials() {
+  //   this.cmsService
+  //     .getDataByTypeWithFilter('publicTestimonials', 'testimonialType', 'Community_Leader', 10)
+  //     .subscribe((data) => {
+  //       if (data) {
+  //         this.testimonials = data;
+  //       }
+  //     });
+  // }
 }
