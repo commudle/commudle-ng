2117a5d0544ae75018f5dd4898c1c78e903f3021
Prerna <prerna@pulkit-MBP.bbrouter>
2024-06-12 14:33:37 +0530

    style: create enum for type

--- apps/commudle-admin/src/app/app-shared-components/help-section/help-section.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/help-section/help-section.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { SidebarService } from 'apps/shared-components/sidebar/service/sidebar.service';
 import { help_dictionary } from '@commudle/shared-services';
-import { IHelpDictionary } from '@commudle/shared-models';
+import { IHelpDictionary, EHelpDictionaryType } from '@commudle/shared-models';
 import { HelpDictionaryService } from 'apps/commudle-admin/src/app/services/help-dictionary.service';
 
 @Component({
@@ -15,6 +15,7 @@ export class HelpSectionComponent implements OnInit {
   faCircleQuestion = faCircleQuestion;
   helpDictionary = help_dictionary;
   helpDictionaryData: IHelpDictionary;
+  EHelpDictionaryType = EHelpDictionaryType;
   constructor(private helpSidebarService: SidebarService, private helpDictionaryService: HelpDictionaryService) {}
 
   ngOnInit(): void {
@@ -27,7 +28,7 @@ export class HelpSectionComponent implements OnInit {
 
   getHelpSectionData() {
     this.helpDictionaryData = this.helpDictionary[this.helpDictionaryName];
-    if (this.helpDictionaryData.type === 'url') {
+    if (this.helpDictionaryData.type === EHelpDictionaryType.URL) {
       this.helpDictionaryService.getHelpDictionaryIframe(this.helpDictionaryData.url);
     }
   }
