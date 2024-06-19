c198db85a2dcd622284413ed6d00c483e6e83a70
Prerna <prerna@pulkit-MBP.bbrouter>
2024-06-19 16:29:50 +0530

    style: cross icon size and color

--- apps/commudle-admin/src/app/app-shared-components/help-dictionary-iframe/help-dictionary-iframe.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/help-dictionary-iframe/help-dictionary-iframe.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { faBars, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { HelpDictionaryService } from 'apps/commudle-admin/src/app/services/help-dictionary.service';
 import { SidebarService } from 'apps/shared-components/sidebar/service/sidebar.service';
 
@@ -11,20 +11,20 @@ import { SidebarService } from 'apps/shared-components/sidebar/service/sidebar.s
 export class HelpDictionaryIframeComponent implements OnInit {
   @Input() eventName: string;
   helpDictionaryUrl: string;
-  expandSidebar: boolean;
-  faCaretLeft = faCaretLeft;
+  faXmark = faXmark;
   faBars = faBars;
   constructor(private helpDictionaryService: HelpDictionaryService, private sidebarService: SidebarService) {}
 
   ngOnInit(): void {
     this.helpDictionaryService.helpDictionary$.subscribe((data) => {
-      this.helpDictionaryUrl = data;
-      this.expandSidebar = true;
+      if (data) {
+        this.helpDictionaryUrl = data;
+        this.sidebarService.openSidebar('helpSection');
+      }
     });
   }
 
   closeSidebar() {
-    this.expandSidebar = false;
     this.sidebarService.closeSidebar(this.eventName);
   }
 }
