94f279434420e96e04b1d65c2f3efaaf9317bc4f
Prerna <prerna@pulkit-MBP.bbrouter>
2024-07-30 00:31:04 +0530

    style: upcoming hackathons side card schema

--- apps/commudle-admin/src/app/app-shared-components/upcoming-hackathons/upcoming-hackathons.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/upcoming-hackathons/upcoming-hackathons.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@commudle/shared-environments';
 import { IHackathon } from '@commudle/shared-models';
+import { SeoService } from '@commudle/shared-services';
 import { HackathonService } from 'apps/commudle-admin/src/app/services/hackathon.service';
 import { IPageInfo } from 'apps/shared-models/page-info.model';
 
@@ -15,8 +17,10 @@ export class UpcomingHackathonsComponent implements OnInit {
   total: number;
   limit = 5;
   page_info: IPageInfo;
+  environment = environment;
+  schemaForHackathon = [];
 
-  constructor(private hackathonService: HackathonService) {}
+  constructor(private hackathonService: HackathonService, private seoService: SeoService) {}
 
   ngOnInit(): void {
     this.getUpcomingHackathons();
@@ -32,7 +36,53 @@ export class UpcomingHackathonsComponent implements OnInit {
         this.total = data.total;
         this.pageInfo = data.page_info;
         this.showSpinner = false;
+        this.setSchema();
       }
     });
+  }
+
+  setSchema() {
+    for (const upcomingHackathon of this.upcomingHackathons) {
+      if (upcomingHackathon.start_date) {
+        let location: object, hackathonStatus: string;
+        if (upcomingHackathon.hackathon_location_type === 'offline') {
+          location = {
+            '@type': 'Place',
+            name: upcomingHackathon.location_name,
+            address: upcomingHackathon.location_address,
+          };
+          hackathonStatus = 'OfflineEventAttendanceMode';
+        } else {
+          location = {
+            '@type': 'VirtualLocation',
+            url:
+              environment.app_url +
+              '/communities/' +
+              upcomingHackathon.kommunity_slug +
+              '/hackathons/' +
+              upcomingHackathon.slug,
+          };
+          hackathonStatus = 'OnlineEventAttendanceMode';
+        }
+        this.schemaForHackathon.push({
+          '@context': 'https://schema.org',
+          '@type': 'Event',
+          name: upcomingHackathon.name,
+          description: upcomingHackathon.description.replace(/<[^>]*>/g, '').substring(0, 200),
+          // image: upcomingHackathon.banner_image ? upcomingHackathon.banner_image.url : this.community.logo_path,
+          startDate: upcomingHackathon.start_date,
+          endDate: upcomingHackathon.end_date,
+          eventStatus: 'https://schema.org/EventScheduled',
+          eventAttendanceMode: 'https://schema.org/' + hackathonStatus,
+          location: location,
+          organizer: {
+            '@type': 'Organization',
+            name: upcomingHackathon.name,
+            url: environment.app_url + '/communities/' + upcomingHackathon.kommunity_slug,
+          },
+        });
+      }
+    }
+    this.seoService.setSchema(this.schemaForHackathon);
   }
 }
