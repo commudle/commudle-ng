45e73e06542f3ba2dddd0a5b9b7ccfa996c90c9c
Prerna <prerna@pulkit-MBP.bbrouter>
2024-08-15 23:46:51 +0530

    fix: routing in newsletter card

--- apps/shared-components/newsletter-card/newsletter-card.component.ts
+++ apps/shared-components/newsletter-card/newsletter-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { INewsletter } from 'apps/shared-models/newsletter.model';
 import { staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { ICommunity } from '@commudle/shared-models';
 
 @Component({
   selector: 'commudle-newsletter-card',
@@ -10,6 +11,9 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class NewsletterCardComponent implements OnInit {
   @Input() newsletter: INewsletter;
+  @Input() parentType = 'Newsletter';
+  @Input() community: ICommunity;
+
   staticAssets = staticAssets;
   icons = {
     faArrowRight,
