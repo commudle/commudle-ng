890d24449bda5727bca093aba326944c63057a1c
Prerna <prerna@pulkit-MBP.bbrouter>
2024-08-20 21:53:47 +0530

    fix: featured communities api

--- apps/commudle-admin/src/app/app-shared-components/featured-communities-card/featured-communities-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/featured-communities-card/featured-communities-card.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NbButtonModule, NbIconModule } from '@commudle/theme';
 import { faCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { IFeaturedCommunity } from 'apps/shared-models/featured-community.model';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SharedComponentsModule } from 'apps/shared-components/shared-components.module';
 import { PublicCommunityModule } from 'apps/commudle-admin/src/app/feature-modules/public-community/public-community.module';
+import { IFeaturedItems } from 'apps/shared-models/featured-items.model';
 @Component({
   selector: 'commudle-featured-communities-card',
   standalone: true,
@@ -23,7 +23,7 @@ import { PublicCommunityModule } from 'apps/commudle-admin/src/app/feature-modul
   styleUrls: ['./featured-communities-card.component.scss'],
 })
 export class FeaturedCommunitiesCardComponent implements OnInit {
-  @Input() featuredCommunity: IFeaturedCommunity;
+  @Input() featuredCommunity: IFeaturedItems;
   @Input() horizontalScroll = false;
   faCheck = faCheck;
   faPlus = faPlus;
