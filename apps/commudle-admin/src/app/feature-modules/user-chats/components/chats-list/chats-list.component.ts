d728e96ded00fba9be54e74b7ce000d37f971aa6
Prerna <prerna@pulkit-MBP.bbrouter>
2024-08-26 23:32:50 +0530

    style: remove commented code

--- apps/commudle-admin/src/app/feature-modules/user-chats/components/chats-list/chats-list.component.ts
+++ apps/commudle-admin/src/app/feature-modules/user-chats/components/chats-list/chats-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { UserChatNotificationsChannel } from 'apps/commudle-admin/src/app/feature-modules/user-chats/services/websockets/user-chat-notifications.channel';
 import { GoogleTagManagerService } from 'apps/commudle-admin/src/app/services/google-tag-manager.service';
-import { DiscussionChatChannel } from 'apps/shared-components/services/websockets/discussion-chat.channel';
 import { ICurrentUser } from 'apps/shared-models/current_user.model';
 import { IDiscussionFollower } from 'apps/shared-models/discussion-follower.model';
 import { LibAuthwatchService } from 'apps/shared-services/lib-authwatch.service';
@@ -12,7 +11,7 @@ import moment from 'moment';
   templateUrl: './chats-list.component.html',
   styleUrls: ['./chats-list.component.scss'],
 })
-export class ChatsListComponent implements OnInit, OnChanges {
+export class ChatsListComponent implements OnInit {
   @Input() currentUser: ICurrentUser;
   @Input() allPersonalChatUsers: IDiscussionFollower[];
   @Output() getChat: EventEmitter<IDiscussionFollower> = new EventEmitter<IDiscussionFollower>();
@@ -28,7 +27,6 @@ export class ChatsListComponent implements OnInit, OnChanges {
     private authWatchService: LibAuthwatchService,
     private userChatNotificationsChannel: UserChatNotificationsChannel,
     private gtm: GoogleTagManagerService,
-    private discussionChatChannel: DiscussionChatChannel,
   ) {}
 
   ngOnInit(): void {
@@ -39,41 +37,6 @@ export class ChatsListComponent implements OnInit, OnChanges {
     // Live update for new messages
     this.liveUpdates();
   }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    // if (changes.currentUser) {
-    //   this.allPersonalChatUsers.forEach((chatUser) => {
-    //     this.discussionChatChannel.subscribe(chatUser.discussion_id);
-    //   });
-    //   setTimeout(() => {
-    //     this.receiveData();
-    //   }, 2000);
-    // }
-  }
-
-  // receiveData() {
-  //   this.allPersonalChatUsers.forEach((chatUser) => {
-  //     console.log('1');
-  //     // eslint-disable-next-line no-prototype-builtins
-  //     if (this.discussionChatChannel.channelData$.hasOwnProperty(chatUser.discussion_id)) {
-  //       console.log('2');
-  //       this.discussionChatChannel.channelData$[chatUser.discussion_id].subscribe((data) => {
-  //         console.log('3');
-  //         if (data) {
-  //           console.log(data, '4');
-  //           switch (data.action) {
-  //             case this.discussionChatChannel.ACTIONS.ADD: {
-  //               const message = this.allPersonalChatUsers.find(
-  //                 (userMessage) => userMessage.discussion_id === data.user_message.discussion_id,
-  //               );
-  //               console.log(message);
-  //             }
-  //           }
-  //         }
-  //       });
-  //     }
-  //   });
-  // }
 
   openChat(chatUser) {
     this.selectedChatUser = chatUser;
