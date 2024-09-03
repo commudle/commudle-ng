7e517627a3f8ff1bcb9d5ade6722254c1a6d461d
Prerna <rprerna2000@gmail.com>
2024-09-03 12:54:05 +0530

    fix: embedded content without iframe embedded

--- apps/commudle-admin/src/app/app-shared-components/speaker-slides-card/speaker-slides-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/speaker-slides-card/speaker-slides-card.component.ts
@@ -14,20 +14,16 @@ export class SpeakerSlidesCardComponent implements OnInit {
   iframe: SafeHtml;
   EAttachmentType = EAttachmentType;
   staticAssets = staticAssets;
+  showDefaultImage = false;
 
   constructor(private domSanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    if (
-      this.item.attachment_type === EAttachmentType.EMBEDDED_LINK &&
-      this.item.embedded_content &&
-      this.item.embedded_content.length > 0
-    ) {
-      if (this.item.embedded_content.includes('<iframe')) {
+    if (this.item.attachment_type === EAttachmentType.EMBEDDED_LINK) {
+      if (this.item.embedded_content?.includes('<iframe')) {
         this.iframe = this.domSanitizer.bypassSecurityTrustHtml(this.item.embedded_content);
       } else {
-        const iframeHtml = `<iframe src="${this.item.embedded_content}" frameborder="0" width="100%" height="500" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`;
-        this.iframe = this.domSanitizer.bypassSecurityTrustHtml(iframeHtml);
+        this.showDefaultImage = true;
       }
     }
   }
