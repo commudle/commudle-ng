e6f37be7ab8f6f47de1e437bf23ee91592a6be1d
Prerna <rprerna2000@gmail.com>
2024-09-09 22:29:27 +0530

    style: left-column

--- apps/commudle-admin/src/app/feature-modules/dashboard/components/user-dashboard/user-dashboard.component.ts
+++ apps/commudle-admin/src/app/feature-modules/dashboard/components/user-dashboard/user-dashboard.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ICommunity, IEvent, IPageInfo } from '@commudle/shared-models';
+import { AppUsersService } from 'apps/commudle-admin/src/app/services/app-users.service';
+import { CommunitiesService } from 'apps/commudle-admin/src/app/services/communities.service';
+import { EventsService } from 'apps/commudle-admin/src/app/services/events.service';
 import { ICurrentUser } from 'apps/shared-models/current_user.model';
 import { LibAuthwatchService } from 'apps/shared-services/lib-authwatch.service';
+import { IUserStat } from 'libs/shared/models/src/lib/user-stats.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'commudle-user-dashboard',
@@ -9,11 +15,53 @@ import { LibAuthwatchService } from 'apps/shared-services/lib-authwatch.service'
 })
 export class UserDashboardComponent implements OnInit {
   currentUser: ICurrentUser;
-  constructor(private authWatchService: LibAuthwatchService) {}
+  userProfileDetails: IUserStat;
+  managedCommunities: ICommunity[] = [];
+  subscriptions: Subscription[] = [];
+  page_info: IPageInfo;
+  total: number;
+  limit = 4;
+  upcomingEvents: IEvent[] = [];
+
+  constructor(
+    private authWatchService: LibAuthwatchService,
+    private appUsersService: AppUsersService,
+    private communitiesService: CommunitiesService,
+    private eventsService: EventsService,
+  ) {}
 
   ngOnInit(): void {
+    this.getUserDetails();
+    this.getCommunitiesData();
+    this.getUpcomingEvents();
+  }
+
+  getUserDetails() {
     this.authWatchService.currentUser$.subscribe((data) => {
       this.currentUser = data;
+      if (this.currentUser) {
+        this.appUsersService.getProfileStats().subscribe((data) => {
+          this.userProfileDetails = data;
+        });
+      }
+    });
+  }
+
+  getCommunitiesData() {
+    this.subscriptions.push(
+      this.communitiesService.userManagedCommunities$.subscribe((data: ICommunity[]) => {
+        this.managedCommunities = data;
+      }),
+    );
+  }
+
+  getUpcomingEvents() {
+    this.eventsService.getEventsList('future', this.limit, this.page_info?.end_cursor).subscribe((data) => {
+      if (data) {
+        this.upcomingEvents = this.upcomingEvents.concat(data.page.reduce((acc, value) => [...acc, value.data], []));
+        this.total = data.total;
+        this.page_info = data.page_info;
+      }
     });
   }
 }
--- apps/commudle-admin/src/app/feature-modules/dashboard/dashboard.module.ts
+++ apps/commudle-admin/src/app/feature-modules/dashboard/dashboard.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { NbCardModule } from '@commudle/theme';
+import { UserProfileComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-profile/user-profile.component';
+import { UserprofileDetailsComponent } from 'apps/commudle-admin/src/app/feature-modules/homepage/components/homepage-dashboard/userprofile-details/userprofile-details.component';
+import { EventHorizontalCardComponent } from 'apps/commudle-admin/src/app/app-shared-components/event-horizontal-card/event-horizontal-card.component';
 
 @NgModule({
   declarations: [UserDashboardComponent],
@@ -13,6 +16,11 @@ import { NbCardModule } from '@commudle/theme';
 
     //Nebular
     NbCardModule,
+
+    //Standalone
+    UserProfileComponent,
+    UserprofileDetailsComponent,
+    EventHorizontalCardComponent,
   ],
 })
 export class DashboardModule {}
