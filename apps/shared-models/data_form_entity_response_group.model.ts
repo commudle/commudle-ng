b96714e43120ff4d7ae118571c76ad9f25ec3957
Prerna <rprerna2000@gmail.com>
2024-10-11 02:04:28 +0530

    style: qr-code popup

--- apps/shared-models/data_form_entity_response_group.model.ts
+++ apps/shared-models/data_form_entity_response_group.model.ts
@@ -4,6 +4,7 @@ import { IUser } from './user.model';
 import { IDataFormEntityResponseValue } from './data_form_entity_response_value.model';
 import { ITrackSlot } from 'apps/shared-models/track-slot.model';
 import { EDbModels, IEvent, IHackathon, IHackathonTeam } from '@commudle/shared-models';
+import { IEventDataFormEntityGroup } from 'apps/shared-models/event_data_form_enity_group.model';
 
 export interface IDataFormEntityResponseGroup {
   id: number;
@@ -17,4 +18,5 @@ export interface IDataFormEntityResponseGroup {
   event?: IEvent;
   hackathon?: IHackathon;
   hackathon_team?: IHackathonTeam;
+  event_data_form_entity_group?: IEventDataFormEntityGroup;
 }
