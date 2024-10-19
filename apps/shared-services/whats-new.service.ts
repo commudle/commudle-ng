5e93a294dd2876bbf4558ccc8bab37d01f886a52
Arshdeep Singh <arsh199820@gmail.com>
2024-10-19 19:38:33 +0530

    fix: hide what's new popup in yt beam

--- apps/shared-services/whats-new.service.ts
+++ apps/shared-services/whats-new.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { CmsService } from 'apps/shared-services/cms.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WhatsNewService {
+  showWhatsNewPopup: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  showWhatsNewPopup$ = this.showWhatsNewPopup.asObservable();
   constructor(private cookieService: CookieService, private cmsService: CmsService) {}
 
   getCookieByName(cookieName) {
@@ -19,5 +22,9 @@ export class WhatsNewService {
 
   getNewUpdates(date) {
     return this.cmsService.getDataByTypeFilterWithDate('whatNew', date, 'date desc');
+  }
+
+  hideWhatsNewPopup() {
+    this.showWhatsNewPopup.next(false);
   }
 }
